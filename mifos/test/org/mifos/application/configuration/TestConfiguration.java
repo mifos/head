51e37b07c62263bb3448eb99a8d73fdcca559591
aditi_mohammedn <aditi_mohammedn@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-07-11 08:20:46 +0000

    Implementation : Test case modified to remove initialization of configuration in setup.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10203 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/test/org/mifos/application/configuration/TestConfiguration.java
+++ mifos/test/org/mifos/application/configuration/TestConfiguration.java
@@ -2,43 +2,42 @@ package org.mifos.application.configuration;
 
 import java.util.Map;
 
-import org.mifos.framework.MifosTestCase;
-
 import org.mifos.application.configuration.business.MifosConfiguration;
 import org.mifos.application.configuration.util.helpers.ConfigurationConstants;
 import org.mifos.application.configuration.util.helpers.LabelKey;
+import org.mifos.framework.MifosTestCase;
 
 public class TestConfiguration extends MifosTestCase {
-	
-	MifosConfiguration configuration ;
-	
-	
+
+	MifosConfiguration configuration;
+
 	@Override
 	protected void setUp() throws Exception {
-		configuration= MifosConfiguration.getInstance();
-		configuration.init();
+		configuration = MifosConfiguration.getInstance();
+	}
+
+	public void testInitializeLabelCahe() {
+
+		Map<LabelKey, String> labelCache = configuration.getLabelCache();
+		assertEquals(true, labelCache.size() > 10);
 
 	}
-	public void testInitializeLabelCahe(){
-		
-		Map<LabelKey, String> labelCache = configuration.getLabelCache();
-		assertEquals(true ,labelCache.size()>10);
-		
+
+	public void testGetLabelValueEnglish() {
+		assertEquals("Bulk entry", MifosConfiguration.getInstance()
+				.getLabelValue(ConfigurationConstants.BULKENTRY, (short) 1));
 	}
-	
-	public void testGetLabelValueEnglish(){
-		assertEquals("Bulk entry",MifosConfiguration.getInstance().getLabelValue(ConfigurationConstants.BULKENTRY,(short)1)) ;
-	}
-	
-	/* Will be uncommented when spanish values will be entered in master data.
-	public void testGetLabelValueSpanish(){
-		assertEquals("Entrada a granel",MifosConfiguration.getInstance().getLabelValue(ConfigurationConstants.BULKENTRY,(short)2)) ;
-	}*/
-	
-	public void testInitializeLabelLocaleIdCache(){
-		Map< String,Short> loacleIdCache = configuration.getLoacleIdCache();
-		assertEquals(true ,loacleIdCache.size()>0);
-		
+
+	/*
+	 * Will be uncommented when spanish values will be entered in master data.
+	 * public void testGetLabelValueSpanish(){ assertEquals("Entrada a
+	 * granel",MifosConfiguration.getInstance().getLabelValue(ConfigurationConstants.BULKENTRY,(short)2)) ; }
+	 */
+
+	public void testInitializeLabelLocaleIdCache() {
+		Map<String, Short> loacleIdCache = configuration.getLoacleIdCache();
+		assertEquals(true, loacleIdCache.size() > 0);
+
 	}
 
 }
