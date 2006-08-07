f1817b99e39db91532ab52b55f445219c0673a22
jkingdon <jkingdon@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-08-07 14:48:41 +0000

    Don't require database for MoneyTest. Insert missing fail() calls.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10378 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/test/org/mifos/framework/util/helpers/MoneyTest.java
+++ mifos/test/org/mifos/framework/util/helpers/MoneyTest.java
@@ -39,154 +39,141 @@ package org.mifos.framework.util.helpers;
 
 import java.math.BigDecimal;
 
-import org.mifos.framework.MifosTestCase;
+import org.mifos.application.master.business.MifosCurrency;
+
+import junit.framework.TestCase;
 
 /**
  * This class is used to test Money class.
  */
-public class MoneyTest extends MifosTestCase {
+public class MoneyTest extends TestCase {
 	
+	private static final MifosCurrency RUPEE =
+		new MifosCurrency((short)2, "RUPEE", "Rs", (short)1, 1.0f, (short)1, (short)1);
 	
+	private static final MifosCurrency EURO =
+		new MifosCurrency((short)3, "EURO", "", (short)2, 0.5f, (short)0, (short)1);
+
 	public MoneyTest(){
 		
 	}
-	private Money createMoneyWithMFICurrency(String amnt){
-		return new Money(TestObjectFactory.getMFICurrency(),amnt);
-	}
-	
-	private Money createMoneyObj(Short currencyId,String amnt){
-		return new Money(TestObjectFactory.getCurrency(currencyId),amnt);
-	}
 	
 	public void testAdd(){
-		Money money = createMoneyWithMFICurrency("100.0");
-		Money addendend = createMoneyWithMFICurrency("200.0");
-		assertEquals("testing Add, should succeed", createMoneyWithMFICurrency("300.0"),money.add(addendend));
-		
+		Money money = new Money(RUPEE, "100.0");
+		Money addendend = new Money(RUPEE, "200.0");
+		assertEquals("testing Add, should succeed", new Money(RUPEE, "300.0"),money.add(addendend));
 	}
 	
 	public void testAddMultipleDecimalAmounts(){
-		Money money = createMoneyWithMFICurrency("0.1");
-		Money money1 = money.add(createMoneyWithMFICurrency("0.1"));
-		Money money2 = money1.add(createMoneyWithMFICurrency("0.1"));
-		Money money3 = money2.add(createMoneyWithMFICurrency("0.1"));
-		Money money4 = money3.add(createMoneyWithMFICurrency("0.1"));
-		assertEquals("testing addMultipleDecimalAmounts, should succeed", createMoneyWithMFICurrency("0.5"),money4);
-		
+		Money money = new Money(RUPEE, "0.1");
+		Money money1 = money.add(new Money(RUPEE, "0.1"));
+		Money money2 = money1.add(new Money(RUPEE, "0.1"));
+		Money money3 = money2.add(new Money(RUPEE, "0.1"));
+		Money money4 = money3.add(new Money(RUPEE, "0.1"));
+		assertEquals("testing addMultipleDecimalAmounts, should succeed", new Money(RUPEE, "0.5"),money4);
 	}
 	
 	public void testAddWithDiffCurrencies(){
-		Money money = createMoneyWithMFICurrency("100.0");
+		Money money = new Money(RUPEE, "100.0");
 		try{
-			Money addendendWithDiffCurrecny= createMoneyObj(Short.valueOf("2"),"200");
+			Money addendendWithDiffCurrecny= new Money(EURO, "200");
 			money.add(addendendWithDiffCurrecny);
-		}catch(Exception e){
-			assertEquals("testing Add with different currencies should throw an exception.",e.getClass(), IllegalArgumentException.class);
+			fail("testing Add with different currencies should throw an exception.");
+		}catch(IllegalArgumentException e){
 		}
 	}
 	
-	
-	
 	public void testSubtract(){
-		Money subtrahend = createMoneyWithMFICurrency("100.0");
-		Money money = createMoneyWithMFICurrency("200.0");
-		assertEquals("testing subtract, should succeed",createMoneyWithMFICurrency("100.0"),money.subtract(subtrahend));
-	
+		Money subtrahend = new Money(RUPEE, "100.0");
+		Money money = new Money(RUPEE, "200.0");
+		assertEquals("testing subtract, should succeed",new Money(RUPEE, "100.0"),money.subtract(subtrahend));
 	}
 	
 	public void testSubtractWithDiffCurrencies(){
-		Money money = createMoneyWithMFICurrency("100.0");
+		Money money = new Money(RUPEE, "100.0");
 		try{
-			Money subtrahendWithDiffCurrecny= createMoneyObj(Short.valueOf("2"),"100");
+			Money subtrahendWithDiffCurrecny= new Money(EURO, "100");
 			money.subtract(subtrahendWithDiffCurrecny);
-		}catch(Exception e){
-			assertEquals("testing subtract with different currencies should throw an exception.",e.getClass(), IllegalArgumentException.class);
+			fail("testing subtract with different currencies should throw an exception.");
+		}catch(IllegalArgumentException e){
 		}
 	}
 	
 	public void testMultiply(){
-		Money multiplicand = createMoneyWithMFICurrency("10.0");
-		Money money = createMoneyWithMFICurrency("20.0");
-		assertEquals("testing multiply, should succeed",createMoneyWithMFICurrency("200.0"),money.multiply(multiplicand));
-	
+		Money multiplicand = new Money(RUPEE, "10.0");
+		Money money = new Money(RUPEE, "20.0");
+		assertEquals("testing multiply, should succeed",new Money(RUPEE, "200.0"),money.multiply(multiplicand));
 	}
 	
 	public void testFactorMultiply(){
-		Money money = createMoneyWithMFICurrency("100.0");
+		Money money = new Money(RUPEE, "100.0");
 		Double factor = new Double(1+(24/100.0));
-		assertEquals("testing multiply with a factor, should succeed",createMoneyWithMFICurrency("124.0"),money.multiply(factor));
-	
+		assertEquals("testing multiply with a factor, should succeed",new Money(RUPEE, "124.0"),money.multiply(factor));
 	}
+
 	public void testMultiplyWithDiffCurrencies(){
-		Money money = createMoneyWithMFICurrency("20.0");
+		Money money = new Money(RUPEE, "20.0");
 		try{
-			Money multiplicandWithDiffCurrecny= createMoneyObj(Short.valueOf("2"),"10");
+			Money multiplicandWithDiffCurrecny= new Money(EURO, "10");
 			money.multiply(multiplicandWithDiffCurrecny);
-		}catch(Exception e){
-			assertEquals("testing multiply with different currencies should throw an exception.",e.getClass(), IllegalArgumentException.class);
+			fail("testing multiply with different currencies should throw an exception.");
+		}catch(IllegalArgumentException e){
 		}
 	}
 	
 	public void testDivide(){
-		Money dividend = createMoneyWithMFICurrency("10.0");
-		Money money = createMoneyWithMFICurrency("20.0");
-		assertEquals("testing divide, should succeed",createMoneyWithMFICurrency("2.0"),money.divide(dividend));
-	
+		Money dividend = new Money(RUPEE, "10.0");
+		Money money = new Money(RUPEE, "20.0");
+		assertEquals("testing divide, should succeed",new Money(RUPEE, "2.0"),money.divide(dividend));
 	}
 	
 	public void testDivideWithDiffCurrencies(){
-		Money money = createMoneyWithMFICurrency("20.0");
+		Money money = new Money(RUPEE, "20.0");
 		try{
-			Money dividendWithDiffCurrecny= createMoneyObj(Short.valueOf("2"),"10");
+			Money dividendWithDiffCurrecny= new Money(EURO, "10");
 			money.divide(dividendWithDiffCurrecny);
-		}catch(Exception e){
-			assertEquals("testing divide with different currencies should throw an exception.",e.getClass(), IllegalArgumentException.class);
+			fail("testing divide with different currencies should throw an exception.");
+		}catch(IllegalArgumentException e){
 		}
 	}
 	
 	public void testNegate(){
-		
-		Money money = createMoneyWithMFICurrency("20.0");
-		assertEquals(createMoneyWithMFICurrency("-20.0"),money.negate());
+		Money money = new Money(RUPEE, "20.0");
+		assertEquals(new Money(RUPEE, "-20.0"),money.negate());
 	}
 	
 	public void testRoundUp(){
-		
-		Money money = createMoneyWithMFICurrency("142.34");
-		assertEquals(createMoneyWithMFICurrency("143.00"),Money.round(money));
-		
+		Money money = new Money(RUPEE, "142.34");
+		assertEquals(new Money(RUPEE, "143.00"),Money.round(money));
 	}
 	
 	public void testRoundDown(){
-		Money money = createMoneyObj(Short.valueOf("3"),"142.34");
-		assertEquals(createMoneyObj(Short.valueOf("3"),"142.00"),Money.round(money));
+		Money money = new Money(EURO, "142.34");
+		assertEquals(new Money(EURO, "142.00"),Money.round(money));
 	}
 	
 	public void testHashCode(){
-		Money money = createMoneyWithMFICurrency("142.34");
+		Money money = new Money(RUPEE, "142.34");
 		BigDecimal amnt = new BigDecimal(142.34);
 		assertEquals((money.getCurrency().getCurrencyId()*100 + amnt.intValue()), money.hashCode());
-		
-		
 	}
 	
 	public void testHashCodeForNullAmnt(){
-		Money money = new Money(TestObjectFactory.getMFICurrency(),"");
+		Money money = new Money(RUPEE, "");
 		assertEquals(0, money.hashCode());
 	}
 	
 	public void testSetScale(){
 		Money money=null;
-		money = createMoneyWithMFICurrency("142.344");
+		money = new Money(RUPEE, "142.344");
 		assertEquals(142.3,money.getAmountDoubleValue());
-		money = createMoneyWithMFICurrency("142.356");
+		money = new Money(RUPEE, "142.356");
 		assertEquals(142.4,money.getAmountDoubleValue());
 	}
 	
 	public void testToString() {
-		Money money = createMoneyWithMFICurrency("4456456456.6");
+		Money money = new Money(RUPEE, "4456456456.6");
 		assertEquals("The toString of money returns : ","4456456456.6",money.toString());
 	}
-	
 	
 }
