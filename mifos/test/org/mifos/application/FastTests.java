3408e2179402c1347d6ab34acca4caf6b16eaf54
jkingdon <jkingdon@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-08-17 21:06:58 +0000

    Add suite for those tests which can be run quickly.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10457 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/test/org/mifos/application/FastTests.java
@@ -0,0 +1,58 @@
+/**
+
+ * Copyright (c) 2005-2006 Grameen Foundation USA
+
+ * 1029 Vermont Avenue, NW, Suite 400, Washington DC 20005
+
+ * All rights reserved.
+
+
+
+ * Apache License
+ * Copyright (c) 2005-2006 Grameen Foundation USA
+ *
+
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the
+
+ * License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an explanation of the license
+
+ * and how it is applied.
+
+ *
+
+ */
+
+package org.mifos.application;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.mifos.framework.MifosTestSuite;
+import org.mifos.framework.components.interestcalculator.FlatInterestCalculatorTest;
+import org.mifos.framework.util.helpers.MoneyTest;
+
+/**
+ * Tests which run quickly (say, <10ms per test, or some such,
+ * so that the whole run can be done in seconds or at most a
+ * minute or two).
+ */
+public class FastTests extends MifosTestSuite {
+
+	public static Test suite() throws Exception {
+		TestSuite suite = new FastTests();
+		suite.addTestSuite(FlatInterestCalculatorTest.class);
+		suite.addTestSuite(MoneyTest.class);
+		return suite;
+	}
+
+}
