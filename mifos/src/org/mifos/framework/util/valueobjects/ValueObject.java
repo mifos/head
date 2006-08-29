6465af3328c199bc0c5e2d48422d6c38c86694f3
jkingdon <jkingdon@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-08-29 16:45:45 +0000

    Explain what the ValueObject class is for.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10550 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/framework/util/valueobjects/ValueObject.java
+++ mifos/src/org/mifos/framework/util/valueobjects/ValueObject.java
@@ -3,8 +3,11 @@ package org.mifos.framework.util.valueobjects;
 import java.io.Serializable;
 
 /**
- * This is the interface which would be implemented by all the ValueObjects and
- * this interface will inturn extend serializable interface
+	Value Objects were used in the M1 code. These value objects held state
+	with business handler classes having the respective business logic. In
+	the M2 style code we are not using these value objects and business
+	handlers. Instead the business objects hold both the state and
+	behaviour. We will remove all these un-used classes after the migration.
  */
 public abstract class ValueObject implements Serializable, ReturnType {
 
