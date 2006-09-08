c4619e5bd1806f5f33e43b816223bc33e6b1605f
aditi_sreedhark <aditi_sreedhark@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-09-08 15:36:09 +0000

    adding test.bat file to call the watir scripts in sequence
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10693 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/test/testscripts/Watir/test.bat
@@ -0,0 +1,17 @@
+ruby -I ./modules -S modules/Office/Office.rb
+ruby -I ./modules -S modules/User/User_Main.rb
+ruby -I ./modules -S modules/Fee/fees_man.rb
+ruby -I ./modules -S modules/check_list/Check_List.rb
+ruby -I ./modules -S modules/Funds/Funds_Main.rb
+ruby -I ./modules -S modules/ProductCategory/ProductCategory_main.rb
+ruby -I ./modules -S modules/LoanProduct/LoanProduct_Main.rb
+ruby -I ./modules -S modules/Centers/CenterTest.rb
+ruby -I ./modules -S modules/Groups/GroupTest.rb
+rem ruby -I ./modules -S modules/GroupOutofCenter/GroupOutOfCenterTest.rb
+ruby -I ./modules -S modules/Clients/ClientTest.rb
+ruby -I ./modules -S modules/LoanAccount/LoanAccountTest.rb
+ruby -I ./modules -S modules/Loans/FeeP.rb
+ruby -I ./modules -S modules/FeeOnetime/Feeone.rb
+ruby -I ./modules -S modules/SavingProduct/SavingProduct_Main.rb
+ruby -I ./modules -S modules/savings/main.rb
+ruby -I ./modules -S modules/Bulkentry/BulkEntryTestCase.rb
