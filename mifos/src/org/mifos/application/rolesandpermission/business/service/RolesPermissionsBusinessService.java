cba6fb7e8a993008416082d7e819045a4317772d
aditi_krishankg <aditi_krishankg@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-09-18 14:36:40 +0000

    Wrong service exception import fixed.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10806 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/application/rolesandpermission/business/service/RolesPermissionsBusinessService.java
+++ mifos/src/org/mifos/application/rolesandpermission/business/service/RolesPermissionsBusinessService.java
@@ -2,15 +2,13 @@ package org.mifos.application.rolesandpermission.business.service;
 
 import java.util.List;
 
-import javax.xml.rpc.ServiceException;
-
-import org.mifos.application.NamedQueryConstants;
 import org.mifos.application.rolesandpermission.business.RoleBO;
 import org.mifos.application.rolesandpermission.persistence.RolesPermissionsPersistence;
 import org.mifos.framework.business.BusinessObject;
-import org.mifos.framework.security.util.UserContext;
 import org.mifos.framework.business.service.BusinessService;
 import org.mifos.framework.exceptions.PersistenceException;
+import org.mifos.framework.exceptions.ServiceException;
+import org.mifos.framework.security.util.UserContext;
 
 
 public class RolesPermissionsBusinessService extends BusinessService {
