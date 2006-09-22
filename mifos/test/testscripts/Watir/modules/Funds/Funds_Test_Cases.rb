25463684a7abe5959e88670463a4d2727f876120
aditi_sreedharkr <aditi_sreedharkr@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-09-22 16:10:36 +0000

    Moved the Funds_main class to the Funds_test_cases.rb file
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10861 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/test/testscripts/Watir/modules/Funds/Funds_Test_Cases.rb
+++ mifos/test/testscripts/Watir/modules/Funds/Funds_Test_Cases.rb
@@ -1,7 +1,6 @@
 # This file contains all the automation test case scenario for testing the funds module
 require 'watir'
 require 'English'
-include Watir
 require 'modules/common/inputs'
 require 'win32ole'
 require 'modules/common/TestClass'
@@ -10,6 +9,8 @@ require 'mysql'
 require 'test/unit/ui/console/testrunner'
 require 'test/unit/assertions'
 require 'watir/WindowHelper'
+
+include Watir
 
 class Funds_Test_Cases < TestClass
 
@@ -285,3 +286,46 @@ class Funds_Test_Cases < TestClass
    end 
   end  
  end  
+ 
+ 
+ class Funds_Main
+  funds_obj = Funds_Test_Cases.new
+  funds_obj.mifos_login
+  funds_obj.Click_Admin_page
+  funds_obj.Check_Funds_Links
+  funds_obj.Check_New_Funds_cancel
+    
+  filename = File.expand_path(File.dirname($PROGRAM_NAME))+"/data/Funds_Data.xls"  
+
+#This section deals with the all validation message and error handling
+
+  funds_obj.open(filename, 2)
+  count = 0
+  rowid = -1
+    
+  while(rowid < $maxrow * $maxcol - 1)
+    funds_obj.read_fund_values(rowid)
+    funds_obj.Click_Admin_page
+    funds_obj.check_validation_error(funds_obj.Fund_name,funds_obj.Gl_code)
+    rowid+=$maxcol
+  end
+  funds_obj.open(filename, 1)  
+   rowid = -1
+  while(rowid < $maxrow * $maxcol - 1)
+    funds_obj.read_fund_values(rowid)
+    funds_obj.Click_Admin_page
+    $ie.link(:text, $new_funds_link).click
+    funds_obj.Check_New_Funds
+    funds_obj.Create_New_Funds(funds_obj.Fund_name,funds_obj.Gl_code)
+    $ie.link(:text, $new_funds_link).click
+    funds_obj.check_editdata_onpreview(funds_obj.Fund_name,funds_obj.Gl_code)
+    funds_obj.check_view_Funds(funds_obj.Fund_name,funds_obj.Gl_code)
+   
+    #funds_obj.create_Funds_duplicate_name(fund_name, glcode)
+    #end
+    funds_obj.edit_Funds_name(funds_obj.Fund_name,funds_obj.Fund_name + "123",funds_obj.Gl_code)
+  
+    rowid+=$maxcol
+  end 
+funds_obj.mifos_logout()
+end
