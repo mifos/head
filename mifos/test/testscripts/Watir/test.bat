5bafb94ebd1659da01067b99bb29711e54825db2
aditi_sreedharkr <aditi_sreedharkr@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-09-26 18:29:55 +0000

    Updated the test.bat with the correct file names
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10942 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/test/testscripts/Watir/test.bat
+++ mifos/test/testscripts/Watir/test.bat
@@ -3,8 +3,8 @@ ruby -I ./modules -S modules/User/User_Test_Cases.rb
 ruby -I ./modules -S modules/Fee/fees_man.rb
 ruby -I ./modules -S modules/check_list/Check_List.rb
 ruby -I ./modules -S modules/Funds/Funds_Main.rb
-ruby -I ./modules -S modules/ProductCategory/ProductCategory_main.rb
-ruby -I ./modules -S modules/LoanProduct/LoanProduct_Main.rb
+ruby -I ./modules -S modules/ProductCategory/ProductCategory_Test_cases.rb
+ruby -I ./modules -S modules/LoanProduct/LoanProduct_Test_Cases.rb
 ruby -I ./modules -S modules/Centers/CenterTest.rb
 ruby -I ./modules -S modules/Groups/GroupTest.rb
 rem ruby -I ./modules -S modules/GroupOutofCenter/GroupOutOfCenterTest.rb
@@ -14,4 +14,4 @@ ruby -I ./modules -S modules/Loans/FeeP.rb
 ruby -I ./modules -S modules/FeeOnetime/Feeone.rb
 ruby -I ./modules -S modules/SavingProduct/SavingProduct_Test_Cases.rb
 ruby -I ./modules -S modules/savings/SavingsAccountTest.rb
-ruby -I ./modules -S modules/Bulkentry/BulkEntryTestCase.rb
+rem ruby -I ./modules -S modules/Bulkentry/BulkEntryTestCase.rb
