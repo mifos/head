548e0912c4cc47f49f6b9c54e5a5f1231456cd34
twong <twong@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2006-09-27 03:15:41 +0000

    Fixes to set the working directory (AKA MIFOS_HOME), and to include the MySQL 5 client libs.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@10946 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/api/TestClient.sh
+++ mifos/src/org/mifos/api/TestClient.sh
@@ -3,4 +3,10 @@
 # This should be run from the root of the mifos source tree:
 #   $ ./src/org/mifos/api/TestClient.sh <LoanId>
 
-java -cp conf:lib/hibernate3.jar:lib/dom4j-1.4.jar:lib/commons-logging.jar:lib/commons-collections.jar:lib/log4j-1.2.11.jar:dist/mifos-lib.jar:lib/cglib-full-2.0.2.jar:lib/mysql-connector-java-3.1.7-bin.jar:lib/jta.jar:lib/antlr-2.7.4.jar:dist/mifos-x.jar org.mifos.api.TestClient $*
+if [ -z $MIFOS_HOME ]
+  then MIFOS_HOME=`pwd` 
+fi
+
+echo $MIFOS_HOME
+
+java -cp $MIFOS_HOME/conf:$MIFOS_HOME/lib/hibernate3.jar:$MIFOS_HOME/lib/dom4j-1.4.jar:$MIFOS_HOME/lib/commons-logging.jar:$MIFOS_HOME/lib/commons-collections.jar:$MIFOS_HOME/lib/log4j-1.2.11.jar:$MIFOS_HOME/dist/mifos-lib.jar:$MIFOS_HOME/lib/cglib-full-2.0.2.jar:$MIFOS_HOME/lib/mysql-connector-java-5.0.3-bin.jar:$MIFOS_HOME/lib/jta.jar:$MIFOS_HOME/lib/antlr-2.7.4.jar:$MIFOS_HOME/dist/mifos-x.jar org.mifos.api.TestClient $*
