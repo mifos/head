091c3fdd1dc69a6d5ecbc5541d270caaaa59433a
jkingdon <jkingdon@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2007-01-12 23:22:20 +0000

    Dispatcher now can handle multiple pages.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@11485 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/application/ui/Dispatcher.java
+++ mifos/src/org/mifos/application/ui/Dispatcher.java
@@ -1,6 +1,8 @@
 package org.mifos.application.ui;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,19 +20,84 @@ public class Dispatcher extends HttpServlet {
 		HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		XmlBuilder html = new XmlBuilder();
+		String pathInfo = request.getPathInfo();
+		if (pathInfo == null) {
+			redirectTo(response, urlOf(request) + "/");
+			return;
+		}
+		else if (pathInfo.equals("/")) {
+			developerPage(html);
+		}
+		else if (pathInfo.equals("/reports/create")) {
+			createPage(html);
+		}
+		else {
+			errorPage(html, pathInfo);
+			response.setStatus(404);
+			return;
+		}
+		response.getWriter().write(html.getOutput());
+	}
+
+	private String urlOf(HttpServletRequest request) throws ServletException {
+		try {
+			URL url = new URL(request.getScheme(), 
+				request.getServerName(), request.getServerPort(),
+				request.getContextPath() + request.getServletPath());
+			return url.toExternalForm();
+		}
+		catch (MalformedURLException e) {
+			throw new ServletException(e);
+		}
+	}
+
+	private void redirectTo(HttpServletResponse response, String url) {
+		response.setStatus(303);
+		response.setHeader("Location", url);
+	}
+
+	private void errorPage(XmlBuilder html, String pathInfo) {
 		html.startTag("html");
-		html.startTag("head");
-		html.startTag("title");
-		html.text("Developer page");
-		html.endTag("title");
-		html.endTag("head");
+		head(html, "Create report");
+
+		html.startTag("body");
+		html.text("Did not find page for " + pathInfo);
+		html.endTag("body");
+
+		html.endTag("html");
+	}
+
+	private void createPage(XmlBuilder html) {
+		html.startTag("html");
+		head(html, "Create report");
+
+		html.startTag("body");
+		createReportBody(html);
+		html.endTag("body");
+
+		html.endTag("html");
+	}
+
+	private void createReportBody(XmlBuilder html) {
+	}
+
+	private void developerPage(XmlBuilder html) {
+		html.startTag("html");
+		head(html, "Developer page");
 
 		html.startTag("body");
 		body(html);
 		html.endTag("body");
 
 		html.endTag("html");
-		response.getWriter().write(html.getOutput());
+	}
+
+	private void head(XmlBuilder html, String title) {
+		html.startTag("head");
+		html.startTag("title");
+		html.text(title);
+		html.endTag("title");
+		html.endTag("head");
 	}
 
 	private void body(XmlBuilder html) {
@@ -49,6 +116,12 @@ public class Dispatcher extends HttpServlet {
 		html.startTag("p");
 		html.text("Database version = " + 
 			DatabaseVersionPersistence.APPLICATION_VERSION);
+		html.endTag("p");
+
+		html.startTag("p");
+		html.startTag("a", "href", "reports/create");
+		html.text("Create report");
+		html.endTag("a");
 		html.endTag("p");
 	}
 
--- mifos/test/org/mifos/application/ui/DispatcherTest.java
+++ mifos/test/org/mifos/application/ui/DispatcherTest.java
@@ -1,11 +1,13 @@
 package org.mifos.application.ui;
 
 import static junitx.framework.StringAssert.assertContains;
+import static junitx.framework.StringAssert.assertNotContains;
 
 import javax.servlet.ServletContext;
 
 import junit.framework.TestCase;
 
+import org.dom4j.DocumentException;
 import org.mifos.framework.TestUtils;
 import org.mifos.framework.persistence.DatabaseVersionPersistence;
 
@@ -15,26 +17,60 @@ import servletunit.ServletContextSimulator;
 
 public class DispatcherTest extends TestCase {
 	
-	public void testMainPage() throws Exception {
-		HttpServletResponseSimulator response = dispatch();
-		assertEquals(200, response.getStatusCode());
-		String out = response.getWriterBuffer().toString();
-		TestUtils.assertWellFormedDocument(out);
+	public void testMainPageNoTrailingSlash() throws Exception {
+		HttpServletResponseSimulator response = dispatch(null);
+		assertEquals(303, response.getStatusCode());
+		assertEquals("https://test-server:123/context/developer/",
+			response.getHeader("Location"));
+	}
+
+	public void testMainPageWithTrailingSlash() throws Exception {
+		HttpServletResponseSimulator response = dispatch("/");
+		String out = getSuccessfulDocument(response);
 		assertContains("page is for features", out);
 		assertContains("Database version = " + 
 			DatabaseVersionPersistence.APPLICATION_VERSION, out);
 		assertContains(
 			"<a href=\"custSearchAction.do?method=getHomePage\">Home</a>", 
 			out);
+		assertContains(
+			"<a href=\"reports/create\">Create report</a>",
+			out);
 	}
 
-	private HttpServletResponseSimulator dispatch() throws Exception {
+	public void testCreateReportPage() throws Exception {
+		HttpServletResponseSimulator response = dispatch("/reports/create");
+		String out = getSuccessfulDocument(response);
+		assertNotContains("page is for features", out);
+	}
+	
+	public void testNotFound() throws Exception {
+		HttpServletResponseSimulator response = dispatch("/foo/bar");
+		assertEquals(404, response.getStatusCode());
+	}
+
+	private String getSuccessfulDocument(HttpServletResponseSimulator response) 
+	throws DocumentException {
+		assertEquals(200, response.getStatusCode());
+		String out = response.getWriterBuffer().toString();
+		TestUtils.assertWellFormedDocument(out);
+		return out;
+	}
+	
+	private HttpServletResponseSimulator dispatch(String path) 
+	throws Exception {
 		ServletContext context = new ServletContextSimulator();
 		HttpServletRequestSimulator request = 
 			new HttpServletRequestSimulator(context);
 		HttpServletResponseSimulator response = 
 			new HttpServletResponseSimulator();
 		request.setMethod(HttpServletRequestSimulator.GET);
+		request.setScheme("https");
+		request.setServerName("test-server");
+		request.setServerPort(123);
+		request.setContextPath("/context");
+		request.setServletPath("/developer");
+		request.setPathInfo(path);
 		new Dispatcher().service(request, response);
 		return response;
 	}
