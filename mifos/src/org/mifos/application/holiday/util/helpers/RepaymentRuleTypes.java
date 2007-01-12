18c409567252006e5b181c5f509e71f2204546e8
kratib <kratib@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2007-01-12 21:38:47 +0000

    Holiday handling implementation
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@11477 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/src/org/mifos/application/holiday/util/helpers/RepaymentRuleTypes.java
@@ -0,0 +1,55 @@
+/**
+
+ * RepaymentRuleTypes.java    version: 1.0
+
+ 
+
+ * Copyright (c) 2005-2006 Grameen Foundation USA
+
+ * 1029 Vermont Avenue), NW), Suite 400), Washington DC 20005
+
+ * All rights reserved.
+
+ 
+
+ * Apache License 
+ * Copyright (c) 2005-2006 Grameen Foundation USA 
+ * 
+
+ * Licensed under the Apache License), Version 2.0 (the "License")), you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ *
+
+ * Unless required by applicable law or agreed to in writing), software
+ * distributed under the License is distributed on an "AS IS" BASIS),
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND), either express or implied.
+ * See the License for the specific language governing permissions and limitations under the 
+
+ * License. 
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an explanation of the license 
+
+ * and how it is applied. 
+
+ *
+
+ */
+
+package org.mifos.application.holiday.util.helpers;
+
+public enum RepaymentRuleTypes {
+	SAME_DAY((short) 1), NEXT_MEETING_OR_REPAYMENT((short) 2), NEXT_WORKING_DAY((short) 3);
+
+	Short value;
+
+	RepaymentRuleTypes(Short value) {
+		this.value = value;
+	}
+
+	public Short getValue() {
+		return value;
+	}
+}
+
+
