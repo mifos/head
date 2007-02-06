d9b766fccd0c8b3b203718883e1e83ff0c13eeea
vanmh <vanmh@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2007-02-06 17:59:43 +0000

    add TestKey.java
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@11534 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/test/org/mifos/framework/components/configuration/cache/TestKey.java
@@ -0,0 +1,19 @@
+package org.mifos.framework.components.configuration.cache;
+
+import org.mifos.framework.TestUtils;
+import junit.framework.TestCase;
+
+
+public class TestKey extends TestCase {
+
+	public void testEquals() throws Exception {
+		Key equal1 = new Key((short)1,"a");
+		Key equal2 = new Key((short)1,"a");
+		Key equal3 = new Key((short)1,"A");
+		Key notEqual1 = new Key((short)1,"b");
+		Key notEqual2 = new Key((short)2,"a");
+		
+		TestUtils.verifyBasicEqualsContract(new Key[]{equal1, equal2, equal3}, 
+				new Key[]{notEqual1, notEqual2}, new Key((short)1,"a"){});
+	}
+}
