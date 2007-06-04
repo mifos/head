4e9641f180e4bb75314ffb95bcd063372f98b6ec
gsteff <gsteff@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2007-06-04 21:23:56 +0000

    Fixing dumb error caused by improper patch application
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@11774 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/framework/struts/action/PersistenceAction.java
+++ mifos/src/org/mifos/framework/struts/action/PersistenceAction.java
@@ -28,33 +28,3 @@ public class PersistenceAction extends BaseAction {
 	}
 
 }
-package org.mifos.framework.struts.action;
-
-import org.mifos.framework.business.service.BusinessService;
-import org.mifos.framework.exceptions.ServiceException;
-import org.mifos.framework.persistence.SessionOpener;
-import org.mifos.framework.persistence.ThreadLocalOpener;
-
-public class PersistenceAction extends BaseAction {
-	
-	private static SessionOpener defaultSessionOpener = new ThreadLocalOpener();
-	protected SessionOpener opener;
-
-	@Override
-	protected BusinessService getService() throws ServiceException {
-		throw new RuntimeException("not implemented");
-	}
-
-	public static SessionOpener getDefaultSessionOpener() {
-		return defaultSessionOpener;
-	}
-
-	public static void setDefaultSessionOpener(SessionOpener defaultSessionOpener) {
-		PersistenceAction.defaultSessionOpener = defaultSessionOpener;
-	}
-	
-	public PersistenceAction() {
-		opener = PersistenceAction.defaultSessionOpener;
-	}
-
-}
