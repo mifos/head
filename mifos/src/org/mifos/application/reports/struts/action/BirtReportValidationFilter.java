1796a37dc6d86223f55b30d3976e41c7b33c338f
erid_du <erid_du@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2007-08-03 09:56:31 +0000

    Refactor BirtReportValidationFilter
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@11987 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/application/reports/struts/action/BirtReportValidationFilter.java
+++ mifos/src/org/mifos/application/reports/struts/action/BirtReportValidationFilter.java
@@ -11,35 +11,44 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 public class BirtReportValidationFilter implements Filter {
+	
+	private String errorMessages;
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		String branchId = request.getParameter("branchId");
-		String report = request.getParameter("__report");
-		if (report.equals("report/ActiveLoansByLoanOfficer.rptdesign") || report.equals("report/DetailedAgingPortfolioAtRisk.rptdesign")) {
-			if (branchId == null || !branchId.equals("-2")) {
-				chain.doFilter(request, response);
-			}
-			else {
-				showError(request, response);
-			}
-		}
-		else {
+		errorMessages = "";
+		filter(request.getParameter("__report"), "report/ActiveLoansByLoanOfficer.rptdesign", request.getParameter("branchId"), "Please select a Branch Office.");
+		filter(request.getParameter("__report"), "report/DetailedAgingPortfolioAtRisk.rptdesign", request.getParameter("branchId"), "Please select a Branch Office.");
+		if(errorMessages.length() == 0) {
 			chain.doFilter(request, response);
 		}
+		else {
+			showError(request, response);
+		}
+	}
+
+	private void filter(String actualReport, String expectedReport, String requireData,String errorMessage) {
+		if(actualReport.equals(expectedReport)) {
+			if(requireData !=null && requireData.equals("-2")){
+				addErrorMessage(errorMessage);
+			}
+		}
+	}
+	
+	private void addErrorMessage(String errorMessage) {
+		errorMessages += errorMessage;
 	}
 
 	private void showError(ServletRequest request, ServletResponse response)
 			throws IOException, ServletException {
 		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-		String errorMessage = "Please Select the required items";
 		httpServletResponse
 				.sendRedirect("/mifos/run?__report="
 						+ request.getParameter("__report") + "&reportName="
 						+ request.getParameter("reportName") + "&userId="
 						+ request.getParameter("userId") + "&__format="
 						+ request.getParameter("__format") + "&message="
-						+ errorMessage);
+						+ errorMessages);
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
