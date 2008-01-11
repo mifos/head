c339d21223229946f351c38e5b64f34d78bdc149
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-01-11 21:25:05 +0000

    Fixed Javadoc for LocalizedTextLookup. No code changes.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@12250 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/config/LocalizedTextLookup.java
+++ mifos/src/org/mifos/config/LocalizedTextLookup.java
@@ -1,7 +1,7 @@
 
 package org.mifos.config;
 
-/*
+/**
  * A class which implements this interface can 
  * be used in conjunction with {@link MessageLookup}
  * to lookup a localized text value associated with
@@ -11,10 +11,10 @@ package org.mifos.config;
  * enumerated type to associated a localized text
  * value with each of its entries.  
  * 
- * Examples include {@link Weekday}, {@link CustomFieldType}
+ * Examples include {@link WeekDay}, {@link CustomFieldType}
  */
 public interface LocalizedTextLookup {
-	/*
+	/**
 	 * Return the key to use for looking up the
 	 * localized text value associated with an object.
 	 */
