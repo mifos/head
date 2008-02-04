ca935ca2d584a0bcc40c9bfce367e7454453f294
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-02-04 23:35:43 +0000

    Javadoc-to-normal-Java-comment change.
    This class will be the example for future Mifos license edits to Java source
    code as outlined in story #189.
    
    More info: http://mifos.org/developers/wiki/MifosLicensing
    
    
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@12283 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/config/ProcessFlowRules.java
+++ mifos/src/org/mifos/config/ProcessFlowRules.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2005-2008 Grameen Foundation USA
  * 1029 Vermont Avenue, NW, Suite 400, Washington DC 20005
  * All rights reserved.
@@ -11,9 +11,9 @@
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  * 
  * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
  * explanation of the license and how it is applied.
