3595869648b9f0e96386f151fceb6a110ce1e9a5
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-09-15 16:56:36 +0000

    testing pootle/.properties .po file maintenance
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@13739 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/src/org/mifos/config/localizedResources/locale_sync.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -o errexit
+
+# NAME
+#     push_prep
+#
+# SYNOPSIS
+#     ./push_prep.sh LANG
+# 
+# DESCRIPTION
+#     push_prep converts Java .properties-based resource bundles into .po
+#     (portable object) format in preparation for use with an external
+#     translation tool.
+#
+# OPTIONS
+#     LANG must be a language code like 'fr', 'es', or 'en_GB'.
+#
+# WARNING
+#     This script is brittle and dangerous! Notice how the first argument is a
+#     directory that is promptly nuked. Use at your own risk.
+
+locale=$1
+mkdir -p $locale
+defaults=`\ls *.properties | grep -v _`
+
+for bundle in $defaults
+do
+    bundleBase=`basename $bundle .properties`
+    echo $bundleBase
+    prop2po $bundle --pot $bundleBase.pot
+    if [ ! -e $locale/${bundleBase}_$locale.po ]
+    then
+        pot2po $bundleBase.pot $locale/${bundleBase}_$locale.po
+    else
+        pot2po -t $locale/${bundleBase}_$locale.po $bundleBase.pot \
+            ${bundleBase}_$locale.po.TEMP
+        mv ${bundleBase}_$locale.po.TEMP $locale/${bundleBase}_$locale.po
+    fi
+    rm $bundleBase.pot
+    # do instead at build time to generate language-specific .properties
+    # NOTE: initial locale-specific .po files for Rhino should be generated
+    #       using something like this.
+    #prop2po -t $bundle \
+    #  -i $locale/${bundleBase}_$locale.properties > $locale/$bundleBase.po
+done
