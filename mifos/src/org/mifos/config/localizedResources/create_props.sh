bfeca2673204bf69c58c16f4c28c872baa19533f
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-09-15 16:57:16 +0000

    documenting TODOs, etc.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@13742 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/config/localizedResources/create_props.sh
+++ mifos/src/org/mifos/config/localizedResources/create_props.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# TODO:
+# TODO, for the pseudolocale (is_IS):
+#   specific Ant task(?) to autogenerate *_is_IS.properties
+
+# TODO, for the "real" locales (like French):
+#   Ant task
 #   example: po2prop.py -t message.properties message_is.properties.po message_is.properties
 
 # NOTE: This may also be implemented as a build step (say, in Java)
