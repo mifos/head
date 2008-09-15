85695ba79f588c761209c4a9a1b4df8abcd3d337
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-09-15 16:57:32 +0000

    locale_sync.sh: set interpreter to "bash" rather than "sh" in case I     accidentally added something Bash-specific
    update_defaults.sh: implement logic to update default translations from 'en'
        .po files
    
    
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@13745 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/config/localizedResources/update_defaults.sh
+++ mifos/src/org/mifos/config/localizedResources/update_defaults.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # NAME
 #     update_defaults
@@ -7,9 +7,18 @@
 #     ./update_defaults.sh
 # 
 # DESCRIPTION
-#     TBD
+#     Uses *_en.po translations to rebuild default translations in Java
+#     .properties format resource bundles.
 #
 # OPTIONS
 #     None.
 
-po2prop -t messages.properties en/messages_en.po > messages.properties
+# NOTE: leave the "grep -v" until existing locale-specific bundles are
+# converted into .po files in their respective directories
+defaults=`\ls *.properties | grep -v _`
+
+for bundle in $defaults
+do
+    bundleBase=`basename $bundle .properties`
+    po2prop -t $bundle en/${bundleBase}_en.po > $bundle
+done
