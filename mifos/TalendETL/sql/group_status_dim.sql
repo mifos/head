48a5ceb9ddeb65d7ca66eeaa430df7f2a6d0293c
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-12-03 21:14:42 +0000

    Moving the TalendETL spike to a new home.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14179 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/TalendETL/sql/group_status_dim.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `group_status_dim` (
-  `status_id` smallint(4) NOT NULL auto_increment,
-  `status_description` varchar(50) NOT NULL default '',
-  PRIMARY KEY  (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- mifos/TalendETL/sql/group_status_fact.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE  `group_status_fact` (
-  `group_id` int(15) NOT NULL,
-  `status_id` smallint(4) NOT NULL,
-  `time_id` smallint(4) default NULL,
-  CONSTRAINT `group_status_fk1` FOREIGN KEY (`group_id`) REFERENCES `group_dim` (`group_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `group_status_fk2` FOREIGN KEY (`status_id`) REFERENCES `group_status_dim` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `group_status_fk3` FOREIGN KEY (`time_id`) REFERENCES `time_dim` (`time_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
