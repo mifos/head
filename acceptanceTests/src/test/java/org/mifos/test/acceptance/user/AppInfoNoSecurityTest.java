77dd60af0a3a7520a956d7e6feb222ea8706409f
adamfeuer <adamfeuer@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-12-04 23:10:05 +0000

    changes to allow acceptance tests to run on hudson (story 1117): added spring test context files; updated pom and contexts to change selenium and cargo ports; improved eclipse build system; optimized imports for AppInfoNoSecurityTest
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14188 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/user/AppInfoNoSecurityTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/user/AppInfoNoSecurityTest.java
@@ -20,11 +20,8 @@ package org.mifos.test.acceptance.user;
  */
 
 import org.mifos.test.acceptance.framework.AppInfoNoSecurityPage;
-import org.mifos.test.acceptance.framework.AppLauncher;
-import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.UiTestCaseBase;
 import org.springframework.test.context.ContextConfiguration;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
