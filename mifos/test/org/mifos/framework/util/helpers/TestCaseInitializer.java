f5d07d6114b4e8884f828e995c8abd6ec332b5ec
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-12-10 20:00:08 +0000

    unit tests require ActivityMapper to be initialized prior to unit tests that use struts
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14205 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/test/org/mifos/framework/util/helpers/TestCaseInitializer.java
+++ mifos/test/org/mifos/framework/util/helpers/TestCaseInitializer.java
@@ -1,40 +1,23 @@
-/**
-
- * TestCaseInitializer.java    version: xxx
-
-
-
- * Copyright (c) 2005-2006 Grameen Foundation USA
-
- * 1029 Vermont Avenue, NW, Suite 400, Washington DC 20005
-
+/*
+ * Copyright (c) 2005-2008 Grameen Foundation USA
  * All rights reserved.
-
-
-
- * Apache License
- * Copyright (c) 2005-2006 Grameen Foundation USA
- *
-
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
-
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the
-
- * License.
- *
- * See also http://www.apache.org/licenses/LICENSE-2.0.html for an explanation of the license
-
- * and how it is applied.
-
- *
-
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
  */
+
 package org.mifos.framework.util.helpers;
 
 import org.mifos.application.accounts.financial.util.helpers.FinancialInitializer;
@@ -45,6 +28,7 @@ import org.mifos.framework.TestUtils;
 import org.mifos.framework.components.audit.util.helpers.AuditConfigurtion;
 import org.mifos.framework.security.authorization.AuthorizationManager;
 import org.mifos.framework.security.authorization.HierarchyManager;
+import org.mifos.framework.security.util.ActivityMapper;
 
 /**
  * Many tests initialize themselves via this class.
@@ -74,6 +58,7 @@ public class TestCaseInitializer {
 			TestUtils.initializeSpring();
 			// Spring must be initialized before FinancialInitializer
 			FinancialInitializer.initialize();
+			ActivityMapper.getInstance().init();
 			AuthorizationManager.getInstance().init();
 			HierarchyManager.getInstance().init();
 			
