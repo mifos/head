5ceba3f8a73a6b42276c5b42eb96fe9a47d9f1e1
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2008-12-15 15:20:23 +0000

    Issue 2144: closer to completion.
    create_props.sh
    * fixed discovery of .po files
    * fixed regex to isolate base name of default Java .properties-based resource
      bundle (without extension)
    
    
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14224 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/org/mifos/config/localizedResources/create_props.sh
+++ mifos/src/org/mifos/config/localizedResources/create_props.sh
@@ -8,13 +8,13 @@ set -o errexit
 
 for locale in `find . -regex '[^/]*/[a-zA-Z_]*$' -type d | cut -c3-`
 do
-    for translated in `find $locale -type f -name "*$locale.po"`
+    for translated in `find $locale -type f -name "*.po"`
     do
         # remove directory part of path
         translatedBase=`basename $translated`
 
         # remove locale and file extension
-        bundleBase=`echo $translatedBase | sed -e 's/_[a-zA-Z]*\.po//'`
+        bundleBase=`echo $translatedBase | sed -e 's/\.po$//'`
 
         # create locale-specific .properties files for use by Mifos web app
         po2prop -t $bundleBase.properties \
