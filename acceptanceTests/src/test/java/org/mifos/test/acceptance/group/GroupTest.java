54be9b7ffa88b70f90ce1a82900d47a5288fbd29
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-02-18 22:28:17 +0000

    In-progress work for story 1373, task 1376. New acceptance test (GroupTest). This is a simple one... all it had to do was hit the group dashboard to reproduce the bug found in issue 2366.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14433 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/group/GroupTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2005-2009 Grameen Foundation USA
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.test.acceptance.group;
+
+import org.mifos.test.acceptance.framework.AppLauncher;
+import org.mifos.test.acceptance.framework.DbUnitUtilities;
+import org.mifos.test.acceptance.framework.HomePage;
+import org.mifos.test.acceptance.framework.LoginPage;
+import org.mifos.test.acceptance.framework.MifosPage;
+import org.mifos.test.acceptance.framework.UiTestCaseBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(locations={"classpath:ui-test-context.xml"})
+@Test(sequential=true, groups={"GroupTest","acceptance","ui"})
+public class GroupTest extends UiTestCaseBase {
+
+    @Autowired
+    private DriverManagerDataSource dataSource;
+    @Autowired
+    private DbUnitUtilities dbUnitUtilities;
+    private AppLauncher appLauncher;
+    
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
+    @BeforeMethod
+    public void setUp() throws Exception {
+        super.setUp();
+        appLauncher = new AppLauncher(selenium);
+    }
+
+    @AfterMethod
+    public void logOut() {
+        (new MifosPage(selenium)).logout();
+    }
+
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
+    public void testHitGroupDashboard() throws Exception {
+        dbUnitUtilities.loadDataFromFile("acceptance_small_001_dbunit.xml.zip", dataSource);
+        LoginPage loginPage = appLauncher.launchMifos();
+        HomePage homePage = loginPage.loginSuccessfullyUsingDefaultCredentials();
+        homePage.verifyPage();
+    }
+}
