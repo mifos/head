1b26d2bbb59edae02b2f6cf8f9a69c8ff314635d
vanmh <vanmh@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-02-26 19:58:03 +0000

    Introduced ConfigurationLocator to encapsulate where to find configuration files and file overrides. Story 1404, task 1414.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@14532 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ mifos/src/main/java/org/mifos/framework/util/ConfigurationLocator.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2005-2009 Grameen Foundation USA
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.framework.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Encapsulates logic for determining which directory 
+ * to look in for configuration files.
+ */
+public class ConfigurationLocator {
+    private static final String LOCATOR_SYSTEM_PROPERTY_NAME = "mifos.conf";
+    private static final String LOCATOR_ENVIRONMENT_PROPERTY_NAME = "MIFOS_CONF";
+    private static final String HOME_PROPERTY_NAME = "user.home";
+    private static final String MIFOS_USER_CONFIG_DIRECTORY_NAME = ".mifos";
+    private static final String DEFAULT_CONFIGURATION_PATH = "org/mifos/config/resources/";
+    
+    public File getFileHandle(String filename) throws IOException {
+        String systemPropertyDirectory = System.getProperty(LOCATOR_SYSTEM_PROPERTY_NAME);
+        String envPropertyDirectory = System.getenv(LOCATOR_ENVIRONMENT_PROPERTY_NAME);
+        String homeDirectory = System.getProperty(HOME_PROPERTY_NAME);
+
+        File fileToReturn = null;
+        
+        if (StringUtils.isNotBlank(systemPropertyDirectory)) {
+            File file = new File(systemPropertyDirectory,filename);
+            System.out.println("Checking system property path: " + file.getAbsolutePath());
+            if (file.exists()) {
+                fileToReturn = file;
+            }
+        } else if (StringUtils.isNotBlank(envPropertyDirectory)) {
+            File file = new File(envPropertyDirectory,filename);
+            System.out.println("Checking environment path: " + file.getAbsolutePath());
+            if (file.exists()) {
+                fileToReturn = file;
+            }
+        } else if (new File(homeDirectory,MIFOS_USER_CONFIG_DIRECTORY_NAME).exists()) {
+            File file = new File(homeDirectory + "/" + MIFOS_USER_CONFIG_DIRECTORY_NAME,filename);
+            System.out.println("Checking home directory path: " + file.getAbsolutePath());
+            if (file.exists()) {
+                fileToReturn = file;
+            }
+        } 
+        
+        if (fileToReturn == null) {
+            filename = DEFAULT_CONFIGURATION_PATH + filename;
+            System.out.println(new ClassPathResource(filename).getPath());
+            fileToReturn = new ClassPathResource(filename).getFile();
+        }
+        
+        return fileToReturn;
+        
+    }
+}
---
+++ mifos/src/test/java/org/mifos/framework/util/ConfigurationLocatorTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2005-2009 Grameen Foundation USA
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.framework.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.JUnit4TestAdapter;
+
+public class ConfigurationLocatorTest {
+
+    @Test
+    public void testGetFileHandle() throws IOException {
+        ConfigurationLocator locator = new ConfigurationLocator();
+        Assert.assertNotNull(locator.getFileHandle("mifosChartOfAccounts.xml"));
+    }
+
+    @Test (expected = FileNotFoundException.class)
+    public void testGetFileHandleFailure() throws IOException {
+        ConfigurationLocator locator = new ConfigurationLocator();
+        locator.getFileHandle("x.xml");
+    }    
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(ConfigurationLocatorTest.class);
+    }
+
+}
