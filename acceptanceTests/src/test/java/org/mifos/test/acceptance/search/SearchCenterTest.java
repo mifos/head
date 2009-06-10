0a398d22d19172e7c6e6b90171075e6b11efb871
jbrewster <jbrewster@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-06-10 18:40:55 +0000

    Add new test for searching Centers.  Patch provided by Goldman Sachs as issue tracker task 2419.  Only adding new class - SearchCenterTest - since patch for task 2420 already provided other helper methods.  Also included license and removed argument to countSearchResults.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15022 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/search/SearchCenterTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2005-2009 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+package org.mifos.test.acceptance.search;
+
+import org.mifos.test.acceptance.framework.AppLauncher;
+import org.mifos.test.acceptance.framework.DbUnitUtilities;
+import org.mifos.test.acceptance.framework.MifosPage;
+import org.mifos.test.acceptance.framework.search.SearchResultsPage;
+import org.mifos.test.acceptance.remote.InitializeApplicationRemoteTestingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(locations = { "classpath:ui-test-context.xml" })
+@Test(sequential = true, groups = {"acceptance","ui"})
+public class SearchCenterTest extends SearchTestBase {
+    private AppLauncher appLauncher;
+
+    @Autowired
+    private DriverManagerDataSource dataSource;
+    @Autowired
+    private DbUnitUtilities dbUnitUtilities;
+    @Autowired
+    private InitializeApplicationRemoteTestingService initRemote;
+    
+    
+    private static String dataFileName = "acceptance_small_003_dbunit.xml.zip";
+    
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    // one of the dependent methods throws Exception
+    @BeforeMethod
+    public void setUp() throws Exception {
+        super.setUp();
+        appLauncher = new AppLauncher(selenium);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        (new MifosPage(selenium)).logout();
+    }
+    
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    public void searchCentersTest() throws Exception {
+        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, 
+                dataFileName,
+                dataSource, selenium);    
+        
+        SearchResultsPage page = searchFor( appLauncher, "MyCenter"); 
+        int count = page.countSearchResults();
+        Assert.assertEquals( count, 5 );
+    }
+    
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    public void searchCenterSpecificTest()  throws Exception {
+        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, 
+                dataFileName,
+                dataSource, selenium);    
+        
+        SearchResultsPage page = searchFor( appLauncher, "MyCenter1232993841778");
+        int count = page.countSearchResults();
+        Assert.assertEquals( count, 1 );
+    }    
+}
