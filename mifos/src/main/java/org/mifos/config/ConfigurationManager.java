695ba18409251f1e24d61f0ca9d898082ff10e5c
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-06-11 17:57:17 +0000

    Reformatted using 4 spaces per indentation level, no tabs.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15037 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- mifos/src/main/java/org/mifos/config/ConfigurationManager.java
+++ mifos/src/main/java/org/mifos/config/ConfigurationManager.java
@@ -17,7 +17,7 @@
  * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
  * explanation of the license and how it is applied.
  */
- 
+
 package org.mifos.config;
 
 import java.io.BufferedInputStream;
@@ -39,278 +39,263 @@ import org.mifos.framework.util.StandardTestingService;
 import org.mifos.service.test.TestMode;
 import org.springframework.core.io.ClassPathResource;
 
-
 /**
- * This is a quick initial sketch of a class for managing configuration
- * values that come from various sources.  The intent is to use file
- * based configuration values for some configuration data and 
- * database based configuration for other configuration data.  
+ * This is a quick initial sketch of a class for managing configuration values
+ * that come from various sources. The intent is to use file based configuration
+ * values for some configuration data and database based configuration for other
+ * configuration data.
  * <p>
- * The general idea is that configuration that does not change often
- * (or should not be changed often) would go into configuration files
- * while more frequently changed configuration would be stored in the
- * database and exposed via the UI.  In particular, configuration 
- * values that should be set once at install time or not be changed 
- * after being set are likely to go into configuration files.
+ * The general idea is that configuration that does not change often (or should
+ * not be changed often) would go into configuration files while more frequently
+ * changed configuration would be stored in the database and exposed via the UI.
+ * In particular, configuration values that should be set once at install time
+ * or not be changed after being set are likely to go into configuration files.
  * <p>
- * This class is currently under active development, so it is likely
- * to be changed significantly as iterative development proceeds.
+ * This class is currently under active development, so it is likely to be
+ * changed significantly as iterative development proceeds.
  */
 public class ConfigurationManager implements Configuration {
 
-	/**
-	 * Filename where default application-wide configuration values are stored.
-	 * This file should never be hand-edited, edit values in the custom config
-	 * file instead.
-	 * 
-	 * @see #CUSTOM_CONFIG_PROPS_FILENAME
-	 */
-	public static final String DEFAULT_CONFIG_PROPS_FILENAME = "applicationConfiguration.default.properties";
+    /**
+     * Filename where default application-wide configuration values are stored.
+     * This file should never be hand-edited, edit values in the custom config
+     * file instead.
+     * 
+     * @see #CUSTOM_CONFIG_PROPS_FILENAME
+     */
+    public static final String DEFAULT_CONFIG_PROPS_FILENAME = "applicationConfiguration.default.properties";
 
-	/**
-	 * Filename where custom overrides for application-wide configuration values
-	 * are stored. Keys in this file must exist in the default config file. This
-	 * file may be hand-edited.
-	 * 
-	 * @see #DEFAULT_CONFIG_PROPS_FILENAME
-	 */
-	public static final String CUSTOM_CONFIG_PROPS_FILENAME = "applicationConfiguration.custom.properties";
-	
-	/** Custom application-wide configuration file for acceptance testing only. */
-	public static final String ACCEPTANCE_CONFIG_PROPS_FILENAME = "applicationConfiguration.acceptance.properties";
-	
-	private static ConfigurationManager configurationManagerInstance = new ConfigurationManager();
-		
-	private Configuration configuration;
-	
-	public static final ConfigurationManager getInstance() {
-		return configurationManagerInstance;	
-	}
+    /**
+     * Filename where custom overrides for application-wide configuration values
+     * are stored. Keys in this file must exist in the default config file. This
+     * file may be hand-edited.
+     * 
+     * @see #DEFAULT_CONFIG_PROPS_FILENAME
+     */
+    public static final String CUSTOM_CONFIG_PROPS_FILENAME = "applicationConfiguration.custom.properties";
 
-	private ConfigurationManager() {
-	    ConfigurationLocator configurationLocator = new ConfigurationLocator();
-	    Properties props = new Properties();
+    /** Custom application-wide configuration file for acceptance testing only. */
+    public static final String ACCEPTANCE_CONFIG_PROPS_FILENAME = "applicationConfiguration.acceptance.properties";
 
-		try {
-		    File defaults = configurationLocator.getFile(DEFAULT_CONFIG_PROPS_FILENAME);
-		    props.load(new BufferedInputStream(new FileInputStream(defaults)));
-		}
-        catch (IOException e) {
+    private static ConfigurationManager configurationManagerInstance = new ConfigurationManager();
+
+    private Configuration configuration;
+
+    public static final ConfigurationManager getInstance() {
+        return configurationManagerInstance;
+    }
+
+    private ConfigurationManager() {
+        ConfigurationLocator configurationLocator = new ConfigurationLocator();
+        Properties props = new Properties();
+
+        try {
+            File defaults = configurationLocator.getFile(DEFAULT_CONFIG_PROPS_FILENAME);
+            props.load(new BufferedInputStream(new FileInputStream(defaults)));
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        
+
         TestMode currentTestMode = new StandardTestingService().getTestMode();
         File customConfigFile = null;
-        
+
         try {
             if (TestMode.MAIN == currentTestMode) {
                 customConfigFile = configurationLocator.getFile(CUSTOM_CONFIG_PROPS_FILENAME);
             } else if (TestMode.ACCEPTANCE == currentTestMode) {
                 customConfigFile = configurationLocator.getFile(ACCEPTANCE_CONFIG_PROPS_FILENAME);
             } else if (TestMode.INTEGRATION == currentTestMode) {
-                customConfigFile = new ClassPathResource("org/mifos/config/resources/" + CUSTOM_CONFIG_PROPS_FILENAME).getFile();
+                customConfigFile = new ClassPathResource("org/mifos/config/resources/" + CUSTOM_CONFIG_PROPS_FILENAME)
+                        .getFile();
             }
             props.load(new BufferedInputStream(new FileInputStream(customConfigFile)));
         } catch (FileNotFoundException e) {
-            /* An FileNotFoundException will be thrown if a file is not found
-             * by getFile(); for normal runtime and acceptance testing modes,
-             * the custom config file is optional, hence, ignore the exception.
+            /*
+             * An FileNotFoundException will be thrown if a file is not found by
+             * getFile(); for normal runtime and acceptance testing modes, the
+             * custom config file is optional, hence, ignore the exception.
              */
         } catch (IOException e) {
-            /* An IOException is thrown by ClassPathResource if the file is not
-             * found. Integration tests require a (dummy) custom config file
-             * and we should fail if the file is missing.
+            /*
+             * An IOException is thrown by ClassPathResource if the file is not
+             * found. Integration tests require a (dummy) custom config file and
+             * we should fail if the file is missing.
              */
             throw new SystemException(e);
         }
-        
-		configuration = ConfigurationConverter.getConfiguration(props);
-	}
 
-	public Configuration getConfiguration() {
-		return configuration;
-	}
-	
-	public Short getShort(String key, Short defaultValue)
-	{
-		return configuration.getShort(key, defaultValue);
-	}
-	
-	public short getShort(String key, short defaultValue)
-	{
-		return configuration.getShort(key, defaultValue);
-	}
-	
-	public  short getShort(String key)
-	{
-		return configuration.getShort(key);
-	}
-	
-	public float getFloat(String key)
-	{
-		return configuration.getFloat(key);
-	}
-	
-	
-	public String getString(String key, String defaultValue)
-	{
-		return configuration.getString(key, defaultValue);
-	}
-	
-	
-	public  int getInt(String key, int defaultValue)
-	{
-		return configuration.getInt(key, defaultValue);
-	}
-	
-	public int getInt(String key)
-	{
-		return configuration.getInt(key);
-	}
-	
-	public double getDouble(String key, double defaultValue)
-	{
-		return configuration.getDouble(key, defaultValue);
-	}
-	
-	public  double getDouble(String key)
-	{
-		return configuration.getDouble(key);
-	}
-	
-	public  boolean containsKey(String key)
-	{
-		return configuration.containsKey(key);
-	}
+        configuration = ConfigurationConverter.getConfiguration(props);
+    }
 
-	public void addProperty(String propertyName, Object propertyValue) {
-		configuration.addProperty(propertyName, propertyValue);
-	}
-	
-	public void clearProperty(String propertyName) {
-		configuration.clearProperty(propertyName);
-	}
+    public Configuration getConfiguration() {
+        return configuration;
+    }
 
-	public void clear() {
-		configuration.clear();
-	}
+    public Short getShort(String key, Short defaultValue) {
+        return configuration.getShort(key, defaultValue);
+    }
 
-	public BigDecimal getBigDecimal(String key) {
-		return configuration.getBigDecimal(key);
-	}
+    public short getShort(String key, short defaultValue) {
+        return configuration.getShort(key, defaultValue);
+    }
 
-	public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
-		return configuration.getBigDecimal(key, defaultValue);
-	}
+    public short getShort(String key) {
+        return configuration.getShort(key);
+    }
 
-	public BigInteger getBigInteger(String key) {
-		return configuration.getBigInteger(key);
-	}
+    public float getFloat(String key) {
+        return configuration.getFloat(key);
+    }
 
-	public BigInteger getBigInteger(String key, BigInteger defaultValue) {
-		return configuration.getBigInteger(key, defaultValue);
-	}
+    public String getString(String key, String defaultValue) {
+        return configuration.getString(key, defaultValue);
+    }
 
-	public boolean getBoolean(String key) {
-		return configuration.getBoolean(key);
-	}
+    public int getInt(String key, int defaultValue) {
+        return configuration.getInt(key, defaultValue);
+    }
 
-	public boolean getBoolean(String key, boolean defaultValue) {
-		return configuration.getBoolean(key, defaultValue);
-	}
+    public int getInt(String key) {
+        return configuration.getInt(key);
+    }
 
-	public Boolean getBoolean(String key, Boolean defaultValue) {
-		return configuration.getBoolean(key, defaultValue);
-	}
+    public double getDouble(String key, double defaultValue) {
+        return configuration.getDouble(key, defaultValue);
+    }
 
-	public byte getByte(String key) {
-		return configuration.getByte(key);
-	}
+    public double getDouble(String key) {
+        return configuration.getDouble(key);
+    }
 
-	public byte getByte(String key, byte defaultValue) {
-		return configuration.getByte(key, defaultValue);
-	}
+    public boolean containsKey(String key) {
+        return configuration.containsKey(key);
+    }
 
-	public Byte getByte(String key, Byte defaultValue) {
-		return configuration.getByte(key, defaultValue);
-	}
+    public void addProperty(String propertyName, Object propertyValue) {
+        configuration.addProperty(propertyName, propertyValue);
+    }
 
-	
+    public void clearProperty(String propertyName) {
+        configuration.clearProperty(propertyName);
+    }
 
-	public Double getDouble(String key, Double defaultValue) {
-		return configuration.getDouble(key, defaultValue);
-	}
+    public void clear() {
+        configuration.clear();
+    }
 
-	public float getFloat(String key, float defaultValue) {
-		return configuration.getFloat(key, defaultValue);
-	}
+    public BigDecimal getBigDecimal(String key) {
+        return configuration.getBigDecimal(key);
+    }
 
-	public Float getFloat(String key, Float defaultValue) {
-		return configuration.getFloat(key, defaultValue);
-	}
+    public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
+        return configuration.getBigDecimal(key, defaultValue);
+    }
 
+    public BigInteger getBigInteger(String key) {
+        return configuration.getBigInteger(key);
+    }
 
-	public Integer getInteger(String key, Integer defaultValue) {
-		return configuration.getInteger(key, defaultValue);
-	}
+    public BigInteger getBigInteger(String key, BigInteger defaultValue) {
+        return configuration.getBigInteger(key, defaultValue);
+    }
 
-	public Iterator getKeys() {
-		return configuration.getKeys();
-	}
+    public boolean getBoolean(String key) {
+        return configuration.getBoolean(key);
+    }
 
-	public Iterator getKeys(String key) {
-		return configuration.getKeys(key);
-	}
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return configuration.getBoolean(key, defaultValue);
+    }
 
-	public List getList(String key) {
-		return configuration.getList(key);
-	}
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        return configuration.getBoolean(key, defaultValue);
+    }
 
-	public List getList(String key, List defaultValue) {
-		return configuration.getList(key, defaultValue);
-	}
+    public byte getByte(String key) {
+        return configuration.getByte(key);
+    }
 
-	public long getLong(String key) {
-		return configuration.getLong(key);
-	}
+    public byte getByte(String key, byte defaultValue) {
+        return configuration.getByte(key, defaultValue);
+    }
 
-	public long getLong(String key, long defaultValue) {
-		return configuration.getLong(key, defaultValue);
-	}
+    public Byte getByte(String key, Byte defaultValue) {
+        return configuration.getByte(key, defaultValue);
+    }
 
-	public Long getLong(String key, Long defaultValue) {
-		return configuration.getLong(key, defaultValue);
-	}
+    public Double getDouble(String key, Double defaultValue) {
+        return configuration.getDouble(key, defaultValue);
+    }
 
-	public Properties getProperties(String key) {
-		return configuration.getProperties(key);
-	}
+    public float getFloat(String key, float defaultValue) {
+        return configuration.getFloat(key, defaultValue);
+    }
 
-	public Object getProperty(String key) {
-		return configuration.getProperty(key);
-	}
+    public Float getFloat(String key, Float defaultValue) {
+        return configuration.getFloat(key, defaultValue);
+    }
 
+    public Integer getInteger(String key, Integer defaultValue) {
+        return configuration.getInteger(key, defaultValue);
+    }
 
-	public String getString(String key) {
-		return configuration.getString(key);
-	}
+    public Iterator getKeys() {
+        return configuration.getKeys();
+    }
 
-	public String[] getStringArray(String key) {
-		return configuration.getStringArray(key);
-	}
+    public Iterator getKeys(String key) {
+        return configuration.getKeys(key);
+    }
 
-	public boolean isEmpty() {
-		return configuration.isEmpty();
-	}
+    public List getList(String key) {
+        return configuration.getList(key);
+    }
 
-	public void setProperty(String propertyName, Object propertyValue) {
-		configuration.setProperty(propertyName, propertyValue);
-	}
-	
-    // Return a decorator Configuration containing every key from the current Configuration 
-	// that starts with the specified prefix
-	public Configuration subset(String prefix) {
-		return configuration.subset(prefix);
-	}
+    public List getList(String key, List defaultValue) {
+        return configuration.getList(key, defaultValue);
+    }
+
+    public long getLong(String key) {
+        return configuration.getLong(key);
+    }
+
+    public long getLong(String key, long defaultValue) {
+        return configuration.getLong(key, defaultValue);
+    }
+
+    public Long getLong(String key, Long defaultValue) {
+        return configuration.getLong(key, defaultValue);
+    }
+
+    public Properties getProperties(String key) {
+        return configuration.getProperties(key);
+    }
+
+    public Object getProperty(String key) {
+        return configuration.getProperty(key);
+    }
+
+    public String getString(String key) {
+        return configuration.getString(key);
+    }
+
+    public String[] getStringArray(String key) {
+        return configuration.getStringArray(key);
+    }
+
+    public boolean isEmpty() {
+        return configuration.isEmpty();
+    }
+
+    public void setProperty(String propertyName, Object propertyValue) {
+        configuration.setProperty(propertyName, propertyValue);
+    }
+
+    // Return a decorator Configuration containing every key from the current
+    // Configuration
+    // that starts with the specified prefix
+    public Configuration subset(String prefix) {
+        return configuration.subset(prefix);
+    }
 
 }
