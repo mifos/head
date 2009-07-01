59406d4b7e462bfb3cca112e07ac4d3510a98b06
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-07-01 17:11:00 +0000

    Commit more of hudson build to version control.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15217 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ resources/continuous-integration/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -o errexit
+
+# used by redeploy_war-pom.xml and rename_latest_war.sh
+export WAR_ARCHIVE=/var/www/gazelle/war/trunk
+
+cd trunk
+
+# clean up because we're using update rather than a clean checkout
+svn status | grep ^? | awk '{print $2}' | xargs --no-run-if-empty mv --force --target-directory=/tmp
+cd mifos/src/main/resources/org/mifos/config/localizedResources
+svn revert --recursive .
+
+# generate localized strings from .po files
+./create_props.sh
+
+# back to "trunk"
+cd $OLDPWD
+
+mvn clean install -Pcontinuous-integration,tomcat6x,download-container
+
+# copy the WAR to where the test server can automatically fetch it
+mvn -f resources/continuous-integration/redeploy_war-pom.xml dependency:copy
+resources/continuous-integration/rename_latest_war.sh
