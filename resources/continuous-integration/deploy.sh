62505c5c56fb198bdede4bc507389bcaf92cd47a
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-07-10 03:29:33 +0000

    $WORKSPACE/../lastStable isn't updated until *after* the build, so instead grab the freshly built WAR right out of the Maven "target" folder.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15284 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- resources/continuous-integration/deploy.sh
+++ resources/continuous-integration/deploy.sh
@@ -5,7 +5,7 @@ set -o errexit
 # JOB_NAME environment variable must be set. We count on Hudson for this.
 
 controlScript=$WORKSPACE/working_copy/resources/continuous-integration/deploy/tomcat/control.sh
-lastStableWAR=$WORKSPACE/../lastStable/archive/working_copy/application/target/mifos-webapp.war
+lastStableWAR=$WORKSPACE/working_copy/application/target/mifos-webapp.war
 deployRoot=$HOME/mifos-$JOB_NAME-deploy
 targetWARlocation=$deployRoot/tomcat6/webapps/mifos.war
 
