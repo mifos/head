791af6480d0a6194e854fcf1bcefc8d00f1589c9
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-07-10 03:22:33 +0000

    Automate creation of /home/hudson/mifos-*-deploy
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15283 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ resources/continuous-integration/new_deploy_root.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -o errexit
+
+# Create new deployment root for a Mifos Hudson build job
+
+deployNickname=$1
+if [ -z "$deployNickname" ]
+then
+    echo "ERROR: Must provide a deployment nickname like 'trunk' or 'v1.3.x' to proceed"
+    echo "Usage: $0 NICKNAME"
+    exit 1
+fi
+
+deployRoot=$HOME/mifos-$deployNickname-deploy
+if [ -e $deployRoot ]
+then
+    echo ERROR: $deployRoot already exists.
+    exit 1
+fi
+
+ciResources=$HOME/hudson-home/jobs/$deployNickname/workspace/working_copy/resources/continuous-integration
+
+set -x
+mkdir $deployRoot
+cd $deployRoot
+tar -xzf $HOME/arc/apache-tomcat-6.*.tar.gz
+ln -s apache-tomcat-* tomcat6
+ln -s $ciResources/deploy/mifos_conf
+cd tomcat6/conf
+ln -fs $ciResources/deploy/tomcat/server.xml
+ln -fs $ciResources/deploy/tomcat/context.xml
+cd ../lib
+ln -s $ciResources/deploy/tomcat/c3p0.properties
