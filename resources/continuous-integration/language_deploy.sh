c275ff465c23ad077f4e9fecbf6b4e343b0434e8
celustig <celustig@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-09-01 11:14:02 +0000

    Added support for Arabic, French, and English rtl test deployments.  Created new_language_deploy.sh and language_deploy.sh for creating and using new language deployments.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@15686 a8845c50-7012-0410-95d3-8e1449b9b1e4

---
+++ resources/continuous-integration/language_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x
+set -o errexit
+
+# JOB_NAME environment variable must be set. We count on Hudson for this.
+
+controlScript=$WORKSPACE/working_copy/resources/continuous-integration/$JOB_NAME-deploy/tomcat/control.sh
+lastStableWAR=$WORKSPACE/working_copy/application/target/mifos-webapp.war
+deployRoot=$HOME/deploys/mifos-$JOB_NAME-deploy
+targetWARlocation=$deployRoot/tomcat6/webapps/mifos.war
+
+$controlScript stop
+rm -f $deployRoot/tomcat6/logs/*
+cp $lastStableWAR $targetWARlocation
+$controlScript start
---
+++ resources/continuous-integration/rtl-deploy/tomcat/control.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Hudson Tomcat control script
+#
+# description: Provides easy control of Hudson-deployed tomcat instance(s)
+
+# Variables for use within this script
+DEPLOY_ROOT=$HOME/deploys/mifos-$JOB_NAME-deploy
+JVM_TMPDIR=/tmp/hudson-$JOB_NAME-tomcat-tmp
+# Variables for use by children/successors of this script
+export CATALINA_HOME=$DEPLOY_ROOT/tomcat6
+export CATALINA_OPTS="-Xmx512m -Djava.io.tmpdir=$JVM_TMPDIR -Djava.awt.headless=true"
+export CATALINA_PID=$DEPLOY_ROOT/tomcat.pid
+export MIFOS_CONF=$DEPLOY_ROOT/mifos_conf
+
+[ -f $CATALINA_HOME/bin/catalina.sh ] || exit 0
+
+[ -d $JVM_TMPDIR ] || mkdir -p $JVM_TMPDIR || exit 1
+
+case $1 in
+start)
+        $CATALINA_HOME/bin/startup.sh
+        ;;
+stop)  
+        $CATALINA_HOME/bin/shutdown.sh
+        rm -f $CATALINA_PID
+        ;;
+status)  
+        if [ -e $CATALINA_PID ]
+        then
+            echo "Tomcat appears to be running as process id `cat $CATALINA_PID`"
+        else
+            echo "$CATALINA_PID does not exist, Tomcat probably is not running."
+        fi
+        ;;
+restart)
+        $CATALINA_HOME/bin/shutdown.sh
+        sleep 1
+        $CATALINA_HOME/bin/startup.sh
+        ;;
+*)
+        echo "Usage: $0 {start|stop|status}"
+        exit 1
+        ;;
+esac   
+exit 0
