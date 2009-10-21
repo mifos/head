f4890089e02722df6846f631eea6ede05c45bf90
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2009-10-21 20:29:52 +0000

    * reformatted * fixed warnings by adding @Override annotations
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@16007 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- application/src/test/java/org/mifos/framework/util/TestingServiceTest.java
+++ application/src/test/java/org/mifos/framework/util/TestingServiceTest.java
@@ -30,11 +30,12 @@ import org.mifos.framework.components.logger.MifosLogManager;
 import org.mifos.service.test.TestMode;
 import org.testng.annotations.Test;
 
-@Test(groups={"unit", "fastTestsSuite"},  dependsOnGroups={"productMixTestSuite"})
+@Test(groups = { "unit", "fastTestsSuite" }, dependsOnGroups = { "productMixTestSuite" })
 public class TestingServiceTest extends TestCase {
     static StandardTestingService standardTestingService = null;
     static TestMode savedTestMode = null;
 
+    @Override
     public void setUp() {
         MifosLogManager.configureLogging();
         standardTestingService = new StandardTestingService();
@@ -42,6 +43,7 @@ public class TestingServiceTest extends TestCase {
         standardTestingService.setTestMode(TestMode.ACCEPTANCE);
     }
 
+    @Override
     public void tearDown() {
         standardTestingService.setTestMode(savedTestMode);
     }
