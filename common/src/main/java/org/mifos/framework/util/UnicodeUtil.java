e4dc68e6c56df272f5d1ddeb8d5df9da7058a48b
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-01-20 14:04:05 +0000

    use boilerplate Mifos licensing text from http://mifos.org/developers/wiki/MifosLicensing
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@16587 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- common/src/main/java/org/mifos/framework/util/UnicodeUtil.java
+++ common/src/main/java/org/mifos/framework/util/UnicodeUtil.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2005-2009 Grameen Foundation USA
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
 package org.mifos.framework.util;
 
 import java.io.BufferedReader;
@@ -14,13 +34,9 @@ import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 /**
- * Original code from <a href=
+ * Original public domain code from <a href=
  * "http://tripoverit.blogspot.com/2007/04/javas-utf-8-and-unicode-writing-is.html"
- * >Trip over IT</a>. No license specified.
- * <p>
- * This implementation is much less than ideal (see WARNING in
- * {@link UnicodeInputStream#UnicodeInputStream(InputStream, String)}), but it's
- * the best we've got for now.
+ * >Trip over IT</a>. 
  */
 public class UnicodeUtil {
 
