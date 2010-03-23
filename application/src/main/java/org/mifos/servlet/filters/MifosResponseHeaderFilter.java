2be8d8283878d652b29963da3bc2047297349037
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-03-23 22:18:14 +0000

    web.xml * enabled header modification: add cache-control to responses
    MifosResponseHeaderFilter
    * reformatted, added boilerplate license text
    
    
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@17020 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- application/src/main/java/org/mifos/servlet/filters/MifosResponseHeaderFilter.java
+++ application/src/main/java/org/mifos/servlet/filters/MifosResponseHeaderFilter.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2005-2010 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
 package org.mifos.servlet.filters;
 
 import java.io.IOException;
@@ -15,45 +35,39 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet Filter implementation class MifosResponseHeaderFilter
  */
 public class MifosResponseHeaderFilter implements Filter {
-	FilterConfig fc;
-	
+    FilterConfig fc;
+
     /**
-     * Default constructor. 
+     * @see Filter#destroy()
      */
-    public MifosResponseHeaderFilter() {
-        // TODO Auto-generated constructor stub
+    public void destroy() {
+        this.fc = null;
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		this.fc = null;
-	}
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @SuppressWarnings("unchecked")
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	@SuppressWarnings("unchecked")
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response =
+            (HttpServletResponse) res;
+        // set the provided HTTP response parameters
+        for (Enumeration e=fc.getInitParameterNames();
+                e.hasMoreElements();) {
+            String headerName = (String)e.nextElement();
+            response.addHeader(headerName,
+                    fc.getInitParameter(headerName));
+                }
+        // pass the request/response on
+        chain.doFilter(req, response);
+    }
 
-	    HttpServletResponse response =
-	      (HttpServletResponse) res;
-	    // set the provided HTTP response parameters
-	    for (Enumeration e=fc.getInitParameterNames();
-	        e.hasMoreElements();) {
-	      String headerName = (String)e.nextElement();
-	      response.addHeader(headerName,
-	                 fc.getInitParameter(headerName));
-	    }
-	    // pass the request/response on
-	    chain.doFilter(req, response);	}
-
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		this.fc = fConfig;
-	}
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        this.fc = fConfig;
+    }
 
 }
