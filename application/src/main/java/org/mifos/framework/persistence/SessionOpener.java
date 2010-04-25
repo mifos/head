8c402e34a62f61a9295bacbb0096788f2ec717c7
ugupta <ugupta@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-04-25 12:19:20 +0000

    Removing unused code
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@17263 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- application/src/main/java/org/mifos/framework/persistence/SessionOpener.java
+++ application/src/main/java/org/mifos/framework/persistence/SessionOpener.java
@@ -20,10 +20,5 @@
 
 package org.mifos.framework.persistence;
 
-import org.mifos.framework.hibernate.helper.SessionHolder;
-
 public interface SessionOpener {
-
-    public SessionHolder open();
-
 }
