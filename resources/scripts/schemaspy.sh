b3be6dec6310b6b4e19d3c6944b79e31b302140c
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-04-28 04:44:04 +0000

    * increase our chances that the output directory only exists if schemaSpy succeeded by writing output to a temp dir, then moving to the final location iff the "java" process returned a successful exit code ...but it looks like schemaSpy always returns a successful exit code, so this won't really work. Oh well. If I can convince the schemaSpy folks to use proper error codes, this will work. * improve output
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@17295 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- resources/scripts/schemaspy.sh
+++ resources/scripts/schemaspy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -o errexit
 
-TRUNK_DB_VERSION=`echo 'select database_version from database_version' | mysql --skip-column-names -u hudson -phudson hudson_mifos_gazelle_trunk`
-outputDir=/var/www/schema/trunk/$TRUNK_DB_VERSION
+DATABASE_VERSION=`echo 'select database_version from database_version' | mysql --skip-column-names -u hudson -phudson hudson_mifos_gazelle_trunk`
+tempDir=`mktemp -d`
+outputDir=/var/www/schema/trunk/$DATABASE_VERSION
 
 if [ -d $outputDir ]
 then
-    echo $outputDir exists, assuming schema has not changed. Exiting gracefully.
+    echo "$outputDir exists--looks like the schema has not changed. Exiting gracefully."
     exit 0
 fi
 
-mkdir $outputDir
+echo "Generating schema documentation for database version $DATABASE_VERSION"
 
-java -jar $HOME/arc/schemaSpy_4.1.1.jar -t mysql -host localhost -u hudson -p hudson -db hudson_mifos_gazelle_trunk -dp $HOME/arc/mysql-connector-java-5.1.12-bin.jar -hq -o $outputDir
+mkdir $tempDir
+
+java -jar $HOME/arc/schemaSpy_4.1.1.jar -t mysql -host localhost -u hudson -p hudson -db hudson_mifos_gazelle_trunk -dp $HOME/arc/mysql-connector-java-5.1.12-bin.jar -hq -o $tempDir
+
+# this mv increases the probability that $outputDir will not exist unless schemaSpy succeeded
+# FIXME: this won't work unless schemaSpy is modified to return exit codes that make sense... currently it appears to always return "success"
+mv $tempDir $outputDir
