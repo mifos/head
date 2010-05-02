19a6ad6bc5c7fa757c4bb16bc0fe6bcdefb103bd
meonkeys <meonkeys@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-05-02 03:29:29 +0000

    Merge in a change to the schema spy script I meant to commit to /trunk.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@17342 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- resources/scripts/schemaspy.sh
+++ resources/scripts/schemaspy.sh
@@ -8,12 +8,11 @@ outputDir=/var/www/schema/trunk/$DATABASE_VERSION
 if [ -d $outputDir ]
 then
     echo "$outputDir exists--looks like the schema has not changed. Exiting gracefully."
+    rm -rf $tempDir
     exit 0
 fi
 
 echo "Generating schema documentation for database version $DATABASE_VERSION"
-
-mkdir $tempDir
 
 java -jar $HOME/arc/schemaSpy_4.1.1.jar -t mysql -host localhost -u hudson -p hudson -db hudson_mifos_gazelle_trunk -dp $HOME/arc/mysql-connector-java-5.1.12-bin.jar -hq -o $tempDir
 
