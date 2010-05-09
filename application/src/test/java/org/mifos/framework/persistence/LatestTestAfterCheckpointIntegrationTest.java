343bb3346af840a964c3210339c384abf4315ac3
keithwoodlock <keithwoodlock@a8845c50-7012-0410-95d3-8e1449b9b1e4>
2010-05-09 19:42:30 +0000

    Turn back on temporarily commented out tests on latestTestAfterCheckpointIntegrationTest. Removing did not show any major difference in build time on hudson yet on local development machine causes build to be 4-5 minutes longer.
    git-svn-id: https://mifos.svn.sourceforge.net/svnroot/mifos/trunk@17412 a8845c50-7012-0410-95d3-8e1449b9b1e4

--- application/src/test/java/org/mifos/framework/persistence/LatestTestAfterCheckpointIntegrationTest.java
+++ application/src/test/java/org/mifos/framework/persistence/LatestTestAfterCheckpointIntegrationTest.java
@@ -67,14 +67,14 @@ public class LatestTestAfterCheckpointIntegrationTest {
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-//        StaticHibernateUtil.initialize();
-//        connection = StaticHibernateUtil.getSessionTL().connection();
-//        connection.setAutoCommit(false);
+        StaticHibernateUtil.initialize();
+        connection = StaticHibernateUtil.getSessionTL().connection();
+        connection.setAutoCommit(false);
     }
 
     @Before
     public void setUp() throws Exception {
-//        dropLatestDatabase();
+        dropLatestDatabase();
     }
 
     @AfterClass
@@ -82,190 +82,183 @@ public class LatestTestAfterCheckpointIntegrationTest {
         // Cleaning the database using FK check disabled connection
         // If any one of the test fails or throws error it could lead to
         // multiple failures in other tests during test build
-//        TestDatabase.createMySQLTestDatabase();
-//        FinancialInitializer.initialize();
-//        StaticHibernateUtil.flushAndCloseSession();
+        TestDatabase.createMySQLTestDatabase();
+        FinancialInitializer.initialize();
+        StaticHibernateUtil.flushAndCloseSession();
     }
 
     @Test
-    public void doNothing() {
+    public void testSimple() throws Exception {
+        connection.createStatement().execute("drop table if exists foo");
+        connection.commit();
+        loadLatest();
+        IDataSet latestDump = new DatabaseConnection(connection).createDataSet();
+        connection.createStatement().execute("drop table if exists foo");
+        connection.commit();
+        applyUpgrades();
+        IDataSet upgradeDump = new DatabaseConnection(connection).createDataSet();
+        Assertion.assertEquals(latestDump, upgradeDump);
+    }
+
+    @Test
+    public void testRealSchemaFromCheckpoint() throws Exception {
+        createLatestDatabaseWithLatestData();
+        Assert.assertEquals(DatabaseVersionPersistence.APPLICATION_VERSION, new DatabaseVersionPersistence(connection)
+                .read());
+        IDataSet latestDataDump = new DatabaseConnection(connection).createDataSet();
+
+        // FIXME for some reason the comparison of DatabaseDataSet (IDataSet) doesn't expose the difference at assert in
+        // datasets FlatXmlDataSet seems to work here.
+        FlatXmlDataSet.write(latestDataDump, new FileOutputStream(System.getProperty("java.io.tmpdir")
+                + "/latestDataDump.xml"));
+        latestDataDump = new FlatXmlDataSet(new File(System.getProperty("java.io.tmpdir") + "/latestDataDump.xml"));
+
+        String latestDump = TestDatabase.getAllTablesStructureDump();
+        dropLatestDatabase();
+        createLatestCheckPointDatabaseWithLatestData();
+        TestDatabase.runUpgradeScripts(LATEST_CHECKPOINT_VERSION, connection);
+        String upgradeDump = TestDatabase.getAllTablesStructureDump();
+
+        IDataSet upgradeDataDump = new DatabaseConnection(connection).createDataSet();
+
+        FlatXmlDataSet.write(upgradeDataDump, new FileOutputStream(System.getProperty("java.io.tmpdir")
+                + "/upgradeDataDump.xml"));
+        upgradeDataDump = new FlatXmlDataSet(new File(System.getProperty("java.io.tmpdir") + "/upgradeDataDump.xml"));
+
+        Assert.assertEquals(latestDump, upgradeDump);
+        Assertion.assertEquals(latestDataDump, upgradeDataDump);
+    }
+
+    @Test
+    public void testDropTables() throws Exception {
+        String blankDB = TestDatabase.getAllTablesStructureDump();
+        createLatestDatabase();
+        dropLatestDatabase();
+        String cleanedDB = TestDatabase.getAllTablesStructureDump();
+        Assert.assertEquals(blankDB, cleanedDB);
+    }
+
+    /**
+     * The idea here is to figure out whether we are dropping tables in the
+     * right order to deal with foreign keys. I'm not sure we fully succeed,
+     * however.
+     */
+    @Test
+    public void testDropTablesWithData() throws Exception {
+        String blankDB = TestDatabase.getAllTablesStructureDump();
+        createLatestDatabaseWithLatestData();
+        dropLatestDatabase();
+        String cleanedDB = TestDatabase.getAllTablesStructureDump();
+        Assert.assertEquals(blankDB, cleanedDB);
+    }
+
+    @Test
+    public void testAfterLookupValuesAfterCheckpoint() throws Exception {
+        createLatestCheckPointDatabaseWithLatestData();
+        int nextLookupId = largestLookupId() + 1;
+        connection.createStatement().execute(
+                "insert into LOOKUP_VALUE(LOOKUP_ID, ENTITY_ID, LOOKUP_NAME) " + "VALUES(" + nextLookupId
+                        + ", 19,'TestLookUpName')");
+        connection.createStatement().execute(
+                "insert into LOOKUP_VALUE_LOCALE(LOCALE_ID, LOOKUP_ID, LOOKUP_VALUE) " + "VALUES(1," + nextLookupId
+                        + ",'Martian')");
+
+        upgradeAllFromVersion(LATEST_CHECKPOINT_VERSION);
+        connection.commit();
+
+        // Assert that custom values have been retained
+        ResultSet rs = connection.createStatement().executeQuery(
+                "select * from lookup_value where lookup_id=" + nextLookupId);
+        rs.next();
+        Assert.assertEquals(19, rs.getInt("entity_id"));
+        Assert.assertEquals("TestLookUpName", rs.getString("lookup_name"));
+
+        rs = connection.createStatement().executeQuery(
+                "select * from lookup_value_locale where lookup_id=" + nextLookupId);
+        rs.next();
+        Assert.assertEquals(1, rs.getInt("locale_id"));
+        Assert.assertEquals("Martian", rs.getString("lookup_value"));
+        rs.close();
 
     }
 
-//    @Test
-//    public void testSimple() throws Exception {
-//        connection.createStatement().execute("drop table if exists foo");
-//        connection.commit();
-//        loadLatest();
-//        IDataSet latestDump = new DatabaseConnection(connection).createDataSet();
-//        connection.createStatement().execute("drop table if exists foo");
-//        connection.commit();
-//        applyUpgrades();
-//        IDataSet upgradeDump = new DatabaseConnection(connection).createDataSet();
-//        Assertion.assertEquals(latestDump, upgradeDump);
-//    }
-//
-//    @Test
-//    public void testRealSchemaFromCheckpoint() throws Exception {
-//        createLatestDatabaseWithLatestData();
-//        Assert.assertEquals(DatabaseVersionPersistence.APPLICATION_VERSION, new DatabaseVersionPersistence(connection)
-//                .read());
-//        IDataSet latestDataDump = new DatabaseConnection(connection).createDataSet();
-//
-//        // FIXME for some reason the comparison of DatabaseDataSet (IDataSet) doesn't expose the difference at assert in
-//        // datasets FlatXmlDataSet seems to work here.
-//        FlatXmlDataSet.write(latestDataDump, new FileOutputStream(System.getProperty("java.io.tmpdir")
-//                + "/latestDataDump.xml"));
-//        latestDataDump = new FlatXmlDataSet(new File(System.getProperty("java.io.tmpdir") + "/latestDataDump.xml"));
-//
-//        String latestDump = TestDatabase.getAllTablesStructureDump();
-//        dropLatestDatabase();
-//        createLatestCheckPointDatabaseWithLatestData();
-//        TestDatabase.runUpgradeScripts(LATEST_CHECKPOINT_VERSION, connection);
-//        String upgradeDump = TestDatabase.getAllTablesStructureDump();
-//
-//        IDataSet upgradeDataDump = new DatabaseConnection(connection).createDataSet();
-//
-//        FlatXmlDataSet.write(upgradeDataDump, new FileOutputStream(System.getProperty("java.io.tmpdir")
-//                + "/upgradeDataDump.xml"));
-//        upgradeDataDump = new FlatXmlDataSet(new File(System.getProperty("java.io.tmpdir") + "/upgradeDataDump.xml"));
-//
-//        Assert.assertEquals(latestDump, upgradeDump);
-//        Assertion.assertEquals(latestDataDump, upgradeDataDump);
-//    }
-//
-//    @Test
-//    public void testDropTables() throws Exception {
-//        String blankDB = TestDatabase.getAllTablesStructureDump();
-//        createLatestDatabase();
-//        dropLatestDatabase();
-//        String cleanedDB = TestDatabase.getAllTablesStructureDump();
-//        Assert.assertEquals(blankDB, cleanedDB);
-//    }
-//
-//    /**
-//     * The idea here is to figure out whether we are dropping tables in the
-//     * right order to deal with foreign keys. I'm not sure we fully succeed,
-//     * however.
-//     */
-//    @Test
-//    public void testDropTablesWithData() throws Exception {
-//        String blankDB = TestDatabase.getAllTablesStructureDump();
-//        createLatestDatabaseWithLatestData();
-//        dropLatestDatabase();
-//        String cleanedDB = TestDatabase.getAllTablesStructureDump();
-//        Assert.assertEquals(blankDB, cleanedDB);
-//    }
-//
-//    @Test
-//    public void testAfterLookupValuesAfterCheckpoint() throws Exception {
-//        createLatestCheckPointDatabaseWithLatestData();
-//        int nextLookupId = largestLookupId() + 1;
-//        connection.createStatement().execute(
-//                "insert into LOOKUP_VALUE(LOOKUP_ID, ENTITY_ID, LOOKUP_NAME) " + "VALUES(" + nextLookupId
-//                        + ", 19,'TestLookUpName')");
-//        connection.createStatement().execute(
-//                "insert into LOOKUP_VALUE_LOCALE(LOCALE_ID, LOOKUP_ID, LOOKUP_VALUE) " + "VALUES(1," + nextLookupId
-//                        + ",'Martian')");
-//
-//        upgradeAllFromVersion(LATEST_CHECKPOINT_VERSION);
-//        connection.commit();
-//
-//        // Assert that custom values have been retained
-//        ResultSet rs = connection.createStatement().executeQuery(
-//                "select * from lookup_value where lookup_id=" + nextLookupId);
-//        rs.next();
-//        Assert.assertEquals(19, rs.getInt("entity_id"));
-//        Assert.assertEquals("TestLookUpName", rs.getString("lookup_name"));
-//
-//        rs = connection.createStatement().executeQuery(
-//                "select * from lookup_value_locale where lookup_id=" + nextLookupId);
-//        rs.next();
-//        Assert.assertEquals(1, rs.getInt("locale_id"));
-//        Assert.assertEquals("Martian", rs.getString("lookup_value"));
-//        rs.close();
-//
-//    }
-//
-//    private void upgradeAllFromVersion(int fromVersion) throws Exception {
-//        for (int currentVersion = fromVersion; currentVersion < APPLICATION_VERSION; ++currentVersion) {
-//            int higherVersion = currentVersion + 1;
-//            try {
-//                upgradeNextVersion(higherVersion);
-//            } catch (Exception failure) {
-//                throw new Exception("Cannot upgrade to " + higherVersion, failure);
-//            }
-//        }
-//    }
-//
-//    private void upgradeNextVersion(int nextVersion) throws Exception {
-//        DatabaseVersionPersistence persistence = new DatabaseVersionPersistence(connection);
-//        Upgrade upgrade = persistence.findUpgrade(nextVersion);
-//        if (upgrade instanceof SqlUpgrade) {
-//            assertNoHardcodedValues((SqlUpgrade) upgrade, nextVersion);
-//        }
-//
-//        upgrade.upgrade(connection);
-//    }
-//
-//    private void assertNoHardcodedValues(SqlUpgrade upgrade, int version) throws Exception {
-//        String[] sqlStatements = SqlExecutor.readFile((InputStream) upgrade.sql().getContent());
-//        for (String sqlStatement : sqlStatements) {
-//            Assert.assertTrue("Upgrade " + version + " contains hard-coded lookup values", HardcodedValues
-//                    .checkLookupValue(sqlStatement));
-//            Assert.assertTrue("Upgrade " + version + " contains hard-coded lookup value locales", HardcodedValues
-//                    .checkLookupValueLocale(sqlStatement));
-//        }
-//    }
-//
-//    private int largestLookupId() throws SQLException {
-//        Statement statement = connection.createStatement();
-//        ResultSet results = statement.executeQuery("select max(lookup_id) from LOOKUP_VALUE");
-//        if (!results.next()) {
-//            throw new SystemException(SystemException.DEFAULT_KEY,
-//                    "Did not find an existing lookup_id in lookup_value table");
-//        }
-//        int largestLookupId = results.getInt(1);
-//        results.close();
-//        statement.close();
-//        return largestLookupId;
-//    }
-//
-//    private void dropLatestDatabase() throws Exception {
-//        TestDatabase.dropMySQLDatabase();
-//    }
-//
-//    private void applyUpgrades() throws Exception {
-//        connection.createStatement().execute("create table foo(x integer)");
-//        connection.createStatement().execute("insert into foo(x) values(5)");
-//        connection.createStatement().execute("alter table foo add column y integer default 7");
-//        connection.commit();
-//    }
-//
-//    private void loadLatest() throws Exception {
-//        connection.createStatement().execute("create table foo(x integer, y integer default 7)");
-//        connection.createStatement().execute("insert into foo(x, y) values(5,7)");
-//        connection.commit();
-//    }
-//
-//    private void createLatestDatabaseWithLatestData() throws Exception {
-//        executeScript("mifosdroptables.sql", connection);
-//        executeScript("latest-schema.sql", connection);
-//        executeScript("latest-data.sql", connection);
-//        connection.commit();
-//    }
-//
-//    private void createLatestDatabase() throws Exception {
-//        executeScript("latest-schema.sql", connection);
-//        connection.commit();
-//    }
-//
-//    private void createLatestCheckPointDatabaseWithLatestData() throws Exception {
-//        executeScript("mifosdroptables-checkpoint.sql", connection);
-//        executeScript("latest-schema-checkpoint.sql", connection);
-//        executeScript("latest-data-checkpoint.sql", connection);
-//        connection.commit();
-//    }
+    private void upgradeAllFromVersion(int fromVersion) throws Exception {
+        for (int currentVersion = fromVersion; currentVersion < APPLICATION_VERSION; ++currentVersion) {
+            int higherVersion = currentVersion + 1;
+            try {
+                upgradeNextVersion(higherVersion);
+            } catch (Exception failure) {
+                throw new Exception("Cannot upgrade to " + higherVersion, failure);
+            }
+        }
+    }
 
+    private void upgradeNextVersion(int nextVersion) throws Exception {
+        DatabaseVersionPersistence persistence = new DatabaseVersionPersistence(connection);
+        Upgrade upgrade = persistence.findUpgrade(nextVersion);
+        if (upgrade instanceof SqlUpgrade) {
+            assertNoHardcodedValues((SqlUpgrade) upgrade, nextVersion);
+        }
 
+        upgrade.upgrade(connection);
+    }
+
+    private void assertNoHardcodedValues(SqlUpgrade upgrade, int version) throws Exception {
+        String[] sqlStatements = SqlExecutor.readFile((InputStream) upgrade.sql().getContent());
+        for (String sqlStatement : sqlStatements) {
+            Assert.assertTrue("Upgrade " + version + " contains hard-coded lookup values", HardcodedValues
+                    .checkLookupValue(sqlStatement));
+            Assert.assertTrue("Upgrade " + version + " contains hard-coded lookup value locales", HardcodedValues
+                    .checkLookupValueLocale(sqlStatement));
+        }
+    }
+
+    private int largestLookupId() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery("select max(lookup_id) from LOOKUP_VALUE");
+        if (!results.next()) {
+            throw new SystemException(SystemException.DEFAULT_KEY,
+                    "Did not find an existing lookup_id in lookup_value table");
+        }
+        int largestLookupId = results.getInt(1);
+        results.close();
+        statement.close();
+        return largestLookupId;
+    }
+
+    private void dropLatestDatabase() throws Exception {
+        TestDatabase.dropMySQLDatabase();
+    }
+
+    private void applyUpgrades() throws Exception {
+        connection.createStatement().execute("create table foo(x integer)");
+        connection.createStatement().execute("insert into foo(x) values(5)");
+        connection.createStatement().execute("alter table foo add column y integer default 7");
+        connection.commit();
+    }
+
+    private void loadLatest() throws Exception {
+        connection.createStatement().execute("create table foo(x integer, y integer default 7)");
+        connection.createStatement().execute("insert into foo(x, y) values(5,7)");
+        connection.commit();
+    }
+
+    private void createLatestDatabaseWithLatestData() throws Exception {
+        executeScript("mifosdroptables.sql", connection);
+        executeScript("latest-schema.sql", connection);
+        executeScript("latest-data.sql", connection);
+        connection.commit();
+    }
+
+    private void createLatestDatabase() throws Exception {
+        executeScript("latest-schema.sql", connection);
+        connection.commit();
+    }
+
+    private void createLatestCheckPointDatabaseWithLatestData() throws Exception {
+        executeScript("mifosdroptables-checkpoint.sql", connection);
+        executeScript("latest-schema-checkpoint.sql", connection);
+        executeScript("latest-data-checkpoint.sql", connection);
+        connection.commit();
+    }
 }
