1c388295e6eb9e5931e7848023cb4d03cfce46eb
angshus <angshuonline@gmail.com>
2010-05-24 16:54:38 +0530

    [MIFOS-2968] updating feecontroller

--- application/src/main/java/org/mifos/accounts/fees/servicefacade/FeeController.java
+++ application/src/main/java/org/mifos/accounts/fees/servicefacade/FeeController.java
@@ -2,15 +2,24 @@ package org.mifos.accounts.fees.servicefacade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FeeController {
 
-    private FeeServiceFacade feeServiceFacade = null;
-
     @Autowired
+    private FeeServiceFacade feeServiceFacade;
+
     public FeeController(FeeServiceFacade feeServiceFacade) {
         this.feeServiceFacade = feeServiceFacade;
     }
+
+    @RequestMapping("/viewAllFees.html")
+    public void viewAllFees() {
+
+
+    }
+
+
 
 }
