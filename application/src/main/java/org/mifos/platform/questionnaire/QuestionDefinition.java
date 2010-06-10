5d63927b77c5b6e01917d5c42e76265de643299c
Vinod John <vinodcjohn@gmail.com>
2010-06-11 00:02:20 +0530

    MIFOS-3019 Create Question with TEXT response

---
+++ application/src/main/java/org/mifos/platform/questionnaire/QuestionDefinition.java
@@ -0,0 +1,13 @@
+package org.mifos.platform.questionnaire;
+
+public class QuestionDefinition {
+    private String title;
+
+    public QuestionDefinition(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ application/src/main/java/org/mifos/platform/questionnaire/QuestionDetail.java
@@ -0,0 +1,29 @@
+package org.mifos.platform.questionnaire;
+
+public class QuestionDetail {
+    private Integer questionId;
+
+    private String questionText;
+
+    private String shortName;
+
+    public QuestionDetail(Integer questionId, String questionText, String shortName) {
+        this.questionId = questionId;
+        this.questionText = questionText;
+        this.shortName = shortName;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+
+}
