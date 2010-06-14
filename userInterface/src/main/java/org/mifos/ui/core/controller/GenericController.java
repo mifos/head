1c886db15478664561c16b2e1269b48f6faba92e
Kalyan Akella <kalyan.akella@gmail.com>
2010-06-14 16:36:52 +0530

    Fixing acceptance tests due to FTL integration

--- userInterface/src/main/java/org/mifos/ui/core/controller/GenericController.java
+++ userInterface/src/main/java/org/mifos/ui/core/controller/GenericController.java
@@ -20,17 +20,18 @@
 
 package org.mifos.ui.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
+@Controller
 public class GenericController extends AbstractController {
 
 	@Override
