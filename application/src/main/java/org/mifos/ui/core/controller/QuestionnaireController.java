598bca9b90e49888a612d7b226c7d67e1f67627e
Angshuman Sarkar <angshuonline@gmail.com>
2010-07-09 14:38:51 +0530

    [MIFOS-3347] Maven Module for Questionnaire

--- application/src/main/java/org/mifos/ui/core/controller/QuestionnaireController.java
+++ application/src/main/java/org/mifos/ui/core/controller/QuestionnaireController.java
@@ -36,6 +36,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.webflow.execution.RequestContext;
+import org.mifos.ui.core.controller.Question; 
+
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
