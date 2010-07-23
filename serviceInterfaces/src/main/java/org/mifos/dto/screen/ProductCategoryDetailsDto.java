7ec82402689afb0a2c9d7f61620e40ddbd99b6ab
Mohammad Shahiduzzaman <shahid21st@gmail.com>
2010-07-23 14:32:49 +1000

    Addig missing files in previous commit

---
+++ serviceInterfaces/src/main/java/org/mifos/dto/screen/ProductCategoryDetailsDto.java
@@ -0,0 +1,35 @@
+package org.mifos.dto.screen;
+
+public class ProductCategoryDetailsDto {
+
+    private final String productCategoryName;
+    private final Short productCategoryStatusId;
+    private final Short productTypeId;
+    private final String productCategoryDesc;
+    private final String productTypeName;
+
+    public ProductCategoryDetailsDto(String productCategoryName, Short productCategoryStatusId, Short productTypeId,
+            String productCategoryDesc, String productTypeName) {
+        this.productCategoryName = productCategoryName;
+        this.productCategoryStatusId = productCategoryStatusId;
+        this.productTypeId = productTypeId;
+        this.productCategoryDesc = productCategoryDesc;
+        this.productTypeName = productTypeName;
+    }
+
+    public String getProductCategoryName() {
+        return this.productCategoryName;
+    }
+    public Short getProductCategoryStatusId() {
+        return this.productCategoryStatusId;
+    }
+    public Short getProductTypeId() {
+        return this.productTypeId;
+    }
+    public String getProductCategoryDesc() {
+        return this.productCategoryDesc;
+    }
+    public String getProductTypeName() {
+        return this.productTypeName;
+    }
+}
