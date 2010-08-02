37385224a200ec113c9cf782e0ba9608f3729002
Vinod John <vinodcjohn@gmail.com>
2010-08-02 11:05:08 +0530

    [MIFOS-3170] Edit question group

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/questionnaire/QuestionGroupResponsePage.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/questionnaire/QuestionGroupResponsePage.java
@@ -25,7 +25,7 @@ import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.client.ClientViewDetailsPage;
 
 public class QuestionGroupResponsePage extends MifosPage {
-    private static final String ANSWER_JS = "window.$(\"#displayResponseForm label:contains(\\\"%s\\\")\").parent().find(\"label\")[1].getElementsByTagName(\"span\")[0].innerHTML.trim()";
+    private static final String ANSWER_JS = "window.$(\"#displayResponseForm label:contains(\\\"%s\\\")\").parent().text().trim()";
 
     public QuestionGroupResponsePage(Selenium selenium) {
         super(selenium);
@@ -35,12 +35,12 @@ public class QuestionGroupResponsePage extends MifosPage {
         super.verifyPage("display_question_group_reponse");
     }
 
-    public String getAnswer(String question) {
+    public String getAnswerHtml(String question) {
         return selenium.getEval(String.format(ANSWER_JS, question));
     }
 
     public ClientViewDetailsPage navigateToViewClientDetailsPage() {
-        selenium.click("id=backToDetailsPage");
+        selenium.click("id=_eventId_cancel");
         waitForPageToLoad();
         return new ClientViewDetailsPage(selenium);
     }
