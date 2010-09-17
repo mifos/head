866d17fe88f261b950d65202d0cf397ab398ee2e
Adam Monsen <haircut@gmail.com>
2010-09-17 16:40:22 -0700

    Fix locale_sync.sh and make it will work with any module
    Necessary for MIFOS-3715.
    
    Related discussion:
    http://article.gmane.org/gmane.comp.finance.mifos.devel/9682

--- resources/scripts/pootle/locale_sync.sh
+++ resources/scripts/pootle/locale_sync.sh
@@ -15,22 +15,32 @@ set -o errexit
 #     Should be executed after updating default translations to update .po
 #     files.
 #
+#     Example invocations (from the top-level dir of the Mifos source tree):
+#
+#       resources/scripts/pootle/locale_sync.sh '^[A-Za-z/]+\.properties$' application/src/main/resources/org/mifos/config/localizedResources
+#
+#       resources/scripts/pootle/locale_sync.sh '[A-Za-z]+\.properties$' userInterface/src/main/resources/org/mifos/ui/localizedProperties
+#
 # OPTIONS
 #     None.
 
 naming_convention=$1
 bundle_basedir=$2
 
+if [ -z "$naming_convention" ] || [ -z "$bundle_basedir" ]; then
+    echo "Usage: $0 NAMING_CONVENTION_REGEX BUNDLE_BASEDIR"
+    exit 1
+fi
+
 # TODO: make sure args were passed, die otherwise
 
-defaults=`\ls ${bundle_basedir}*.properties | grep -P "$naming_convention_regex"`
+defaults=`\ls ${bundle_basedir}/*.properties | grep -P "$naming_convention"`
 
-for locale in `find $bundle_basedir -regex '.*/[a-zA-Z_]+$' -type d | xargs basename`
+for locale in `find $bundle_basedir -mindepth 1 -regex '.*/[a-zA-Z_]+$' -type d | xargs -n 1 basename`
 do
     for bundle in $defaults
     do
         bundleBase=`basename $bundle .properties`
-        echo $bundleBase
         potPath=$bundle_basedir/$locale/$bundleBase.pot
         poPath=$bundle_basedir/$locale/$bundleBase.po
         prop2po $bundle --pot $potPath
