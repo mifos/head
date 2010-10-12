d78875925fc46fe55a232618640c04c949b8b815
Kalyan Akella <kalyan.akella@gmail.com>
2010-10-12 11:42:27 +0530

    [MIFOS-3830] Added support for QGs at loan product level - Data model changes

--- application/src/main/sql/latest-schema.sql
+++ application/src/main/sql/latest-schema.sql
@@ -4596,3 +4596,10 @@ create table BATCH_STEP_EXECUTION_SEQ (id bigint not null) engine=myisam;
 create table BATCH_JOB_EXECUTION_SEQ (id bigint not null) engine=myisam;
 
 create table BATCH_JOB_SEQ (id bigint not null) engine=myisam;
+
+create table prd_offering_question_group(
+    prd_offering_id smallint not null,
+    question_group_id integer not null,
+    foreign key (prd_offering_id) references prd_offering(prd_offering_id),
+    foreign key (question_group_id) references question_group(id)
+) engine=innodb character set utf8;
