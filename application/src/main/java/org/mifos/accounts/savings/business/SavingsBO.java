44e6a908495a21644ecbca2777d2546cbf299591
Keith Woodlock <keithwoodlock@gmail.com>
2010-10-21 14:14:22 +0100

    MIFOS-3875: replace next interest calculation and posting date generation code with cleaner implementation.

--- application/src/main/java/org/mifos/accounts/savings/business/SavingsBO.java
+++ application/src/main/java/org/mifos/accounts/savings/business/SavingsBO.java
@@ -55,6 +55,7 @@ import org.mifos.accounts.productdefinition.util.helpers.RecommendedAmountUnit;
 import org.mifos.accounts.productdefinition.util.helpers.SavingsType;
 import org.mifos.accounts.savings.interest.InterestPostingPeriodResult;
 import org.mifos.accounts.savings.interest.schedule.InterestScheduledEvent;
+import org.mifos.accounts.savings.interest.schedule.SavingsInterestScheduledEventFactory;
 import org.mifos.accounts.savings.persistence.SavingsPersistence;
 import org.mifos.accounts.savings.util.helpers.SavingsHelper;
 import org.mifos.accounts.util.helpers.AccountActionTypes;
@@ -226,23 +227,35 @@ public class SavingsBO extends AccountBO {
         }
     }
 
+    /**
+     * create a constructor that doesnt take customFields or delegate to goActiveForFristTimeAndGenerateSavingsSchedule which contains persistence.
+     */
+    @Deprecated
     public SavingsBO(final UserContext userContext, final SavingsOfferingBO savingsOffering, final CustomerBO customer,
             final AccountState accountState, final Money recommendedAmount, final List<CustomFieldDto> customFields)
             throws AccountException {
         super(userContext, customer, AccountTypes.SAVINGS_ACCOUNT, accountState);
-        this.setSavingsOffering(savingsOffering);
 
-        setSavingsPerformance(createSavingsPerformance());
-        setSavingsBalance(new Money(getCurrency()));
-        this.setSavingsType(savingsOffering.getSavingsType());
-        this.setRecommendedAmntUnit(savingsOffering.getRecommendedAmntUnit());
+        this.savingsOffering = savingsOffering;
+        this.savingsPerformance = createSavingsPerformance();
+        this.savingsBalance = Money.zero();
+
+        // inherit details from savings product definition
+        this.savingsType = savingsOffering.getSavingsType();
+        this.recommendedAmntUnit = savingsOffering.getRecommendedAmntUnit();
+        this.minAmntForInt = this.savingsOffering.getMinAmntForInt();
+        this.interestRate = this.savingsOffering.getInterestRate();
+        this.interestCalcType = this.savingsOffering.getInterestCalcType();
+        this.timePerForInstcalc = this.createNewMeetingFrom(this.savingsOffering.getTimePerForInstcalc().getMeeting());
+
+        // FIXME - keithw - create constructor that does not take DTO of customFields
         try {
             addcustomFields(customFields);
         } catch (InvalidDateException e) {
             throw new AccountException(e);
         }
         this.recommendedAmount = recommendedAmount;
-        this.setSavingsOfferingDetails();
+
         // generated the deposit action dates only if savings account is being
         // saved in approved state
         if (isActive()) {
@@ -258,10 +271,6 @@ public class SavingsBO extends AccountBO {
                 .getOfficeId());
         setValuesForActiveState(futureCalendarEventsApplicableToOffice.getWorkingDays(),
                 futureCalendarEventsApplicableToOffice.getHolidays(), new DateTime(new DateTimeService().getCurrentJavaDateTime()));
-    }
-
-    public void populateInstanceForTest(final SavingsOfferingBO savingsOffering) {
-        this.savingsOffering = savingsOffering;
     }
 
     public Money getRecommendedAmount() {
@@ -391,20 +400,8 @@ public class SavingsBO extends AccountBO {
         return timePerForInstcalc;
     }
 
-    private void setInterestCalcType(final InterestCalcTypeEntity interestCalcType) {
-        this.interestCalcType = interestCalcType;
-    }
-
-    private void setInterestRate(final Double interestRate) {
-        this.interestRate = interestRate;
-    }
-
     public void setMinAmntForInt(final Money minAmntForInt) {
         this.minAmntForInt = minAmntForInt;
-    }
-
-    private void setTimePerForInstcalc(final MeetingBO timePerForInstcalc) {
-        this.timePerForInstcalc = timePerForInstcalc;
     }
 
     public Set<SavingsActivityEntity> getSavingsActivityDetails() {
@@ -430,22 +427,18 @@ public class SavingsBO extends AccountBO {
                 .getId().equals(AccountState.SAVINGS_CLOSED.getValue()));
     }
 
-    private void setSavingsOfferingDetails() throws AccountException {
-        setMinAmntForInt(getSavingsOffering().getMinAmntForInt());
-        setInterestRate(getSavingsOffering().getInterestRate());
-        setInterestCalcType(getSavingsOffering().getInterestCalcType());
+    private MeetingBO createNewMeetingFrom(final MeetingBO offeringMeeting) {
         try {
-            setTimePerForInstcalc(getMeeting(getSavingsOffering().getTimePerForInstcalc().getMeeting()));
-        } catch (MeetingException me) {
-            throw new AccountException(me);
+            RecurrenceType recurrenceType = offeringMeeting.getMeetingDetails().getRecurrenceTypeEnum();
+            MeetingType meetingType = MeetingType.fromInt(offeringMeeting.getMeetingType().getMeetingTypeId());
+            return new MeetingBO(recurrenceType, offeringMeeting.getMeetingDetails().getRecurAfter(), startOfFiscalYear(), meetingType);
+        } catch (MeetingException e) {
+            throw new BusinessRuleException(e.getKey(), e);
         }
     }
 
-    private MeetingBO getMeeting(final MeetingBO offeringMeeting) throws MeetingException {
-        RecurrenceType recurrenceType = offeringMeeting.getMeetingDetails().getRecurrenceTypeEnum();
-        MeetingType meetingType = MeetingType.fromInt(offeringMeeting.getMeetingType().getMeetingTypeId());
-        return new MeetingBO(recurrenceType, offeringMeeting.getMeetingDetails().getRecurAfter(), helper
-                .getFiscalStartDate(), meetingType);
+    private Date startOfFiscalYear() {
+        return new LocalDate().withMonthOfYear(1).withDayOfYear(1).toDateMidnight().toDate();
     }
 
     private void setSavingsPerformance(final SavingsPerformanceEntity savingsPerformance) {
@@ -936,17 +929,14 @@ public class SavingsBO extends AccountBO {
 
     private void setValuesForActiveState(final List<Days> workingDays, final List<Holiday> holidays, DateTime activationDate)
             throws AccountException {
-        this.setActivationDate(activationDate.toDate());
+        this.activationDate = activationDate.toDate();
         this.generateDepositAccountActions(workingDays, holidays);
-        try {
-            Date intCalcDate = helper.getNextScheduleDate(getActivationDate(), null, getTimePerForInstcalc());
-            this.setNextIntCalcDate(intCalcDate);
-            Date intPostDate = helper.getNextScheduleDate(getActivationDate(), null, getMeeting(getSavingsOffering()
-                    .getFreqOfPostIntcalc().getMeeting()));
-            this.setNextIntPostDate(intPostDate);
-        } catch (MeetingException me) {
-            throw new AccountException(me);
-        }
+
+        InterestScheduledEvent interestCalculationEvent = new SavingsInterestScheduledEventFactory().createScheduledEventFrom(getTimePerForInstcalc());
+        this.nextIntCalcDate = interestCalculationEvent.nextMatchingDateAfter(new LocalDate(startOfFiscalYear()),new LocalDate(this.activationDate)).toDateMidnight().toDate();
+
+        InterestScheduledEvent interestPostingEvent = new SavingsInterestScheduledEventFactory().createScheduledEventFrom(this.savingsOffering.getFreqOfPostIntcalc().getMeeting());
+        this.nextIntPostDate = interestPostingEvent.nextMatchingDateAfter(new LocalDate(startOfFiscalYear()),new LocalDate(this.activationDate)).toDateMidnight().toDate();
     }
 
     @Override
--- application/src/main/java/org/mifos/accounts/savings/util/helpers/SavingsHelper.java
+++ application/src/main/java/org/mifos/accounts/savings/util/helpers/SavingsHelper.java
@@ -20,137 +20,21 @@
 
 package org.mifos.accounts.savings.util.helpers;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 import org.mifos.accounts.business.AccountActionDateEntity;
 import org.mifos.accounts.business.AccountBO;
 import org.mifos.accounts.business.AccountPaymentEntity;
 import org.mifos.accounts.business.AccountTrxnEntity;
 import org.mifos.accounts.savings.business.SavingsScheduleEntity;
-import org.mifos.accounts.savings.interest.schedule.InterestScheduledEvent;
-import org.mifos.accounts.savings.interest.schedule.SavingsInterestScheduledEventFactory;
 import org.mifos.accounts.util.helpers.AccountActionTypes;
 import org.mifos.accounts.util.helpers.PaymentStatus;
-import org.mifos.application.meeting.business.MeetingBO;
-import org.mifos.application.meeting.exceptions.MeetingException;
-import org.mifos.application.meeting.util.helpers.MeetingType;
-import org.mifos.config.business.Configuration;
 import org.mifos.customers.business.CustomerBO;
-import org.mifos.framework.util.DateTimeService;
-import org.mifos.framework.util.helpers.DateUtils;
 import org.mifos.framework.util.helpers.Money;
 
 public class SavingsHelper {
-    /**
-     * I assume the hardcoding of 1 Jan 2006 is trying to say that the default fiscal year is January 1 to December 31.
-     * Do we use the year? What does this control, versus {@link SavingsConstants#POSTING_DAY}?
-     *
-     * Force a locale that works with pattern parsing.
-     */
-    @Deprecated
-    public Date getFiscalStartDate() {
-        try {
-            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", new Locale("en", "GB"));
-            return format.parse("01/01/2006");
-        } catch (ParseException pe) {
-            throw new RuntimeException(pe);
-        }
-    }
 
     public SavingsHelper() {
-    }
-
-    /**
-     * start from getting a next schedule date after fiscal start date and loop till get a meeting date after account
-     * activation date. This won't move if the date is not working date
-     */
-    private Date getFirstDateForSavingsCalculationAndPosting(MeetingBO meeting, Date accountActivationDate)
-            throws MeetingException {
-        Date date = meeting.getNextScheduleDateAfterRecurrenceWithoutAdjustment(getFiscalStartDate());
-        while (date.compareTo(accountActivationDate) <= 0) {
-            date = meeting.getNextScheduleDateAfterRecurrenceWithoutAdjustment(date);
-        }
-
-        return date;
-    }
-
-    /*
-     * set the day number as end of month for interest calculation and interest posting date and get the meeting date
-     * according to the rules set for meeting
-     */
-    /**
-     * don't use this approach any more. See {@link InterestScheduledEvent} and its implementations and {@link SavingsInterestScheduledEventFactory}.
-     */
-    @Deprecated
-    public Date getNextScheduleDate(Date accountActivationDate, Date currentScheduleDate, MeetingBO meeting)
-            throws MeetingException {
-        Date oldMeetingStartDate = meeting.getStartDate();
-        Short oldDayNumber = meeting.getMeetingDetails().getDayNumber();
-        setDetailsForMeeting(meeting);
-        Date scheduleDate = (currentScheduleDate == null) ? getFirstDateForSavingsCalculationAndPosting(meeting,
-                accountActivationDate) : meeting
-                .getNextScheduleDateAfterRecurrenceWithoutAdjustment(currentScheduleDate);
-        meeting.setStartDate(oldMeetingStartDate);
-        meeting.getMeetingDetails().getMeetingRecurrence().setDayNumber(oldDayNumber);
-        return scheduleDate;
-    }
-
-    /**
-     * don't use this approach any more. See {@link InterestScheduledEvent} and its implementations and {@link SavingsInterestScheduledEventFactory}.
-     */
-    @Deprecated
-    public Date getPrevScheduleDate(Date accountActivationDate, Date currentScheduleDate, MeetingBO meeting)
-            throws MeetingException {
-        Date oldMeetingStartDate = meeting.getStartDate();
-        Short oldDayNumber = meeting.getMeetingDetails().getDayNumber();
-        setDetailsForMeeting(meeting);
-        Date prevScheduleDate = meeting.getPrevScheduleDateAfterRecurrence(currentScheduleDate);
-        meeting.setStartDate(oldMeetingStartDate);
-        meeting.getMeetingDetails().getMeetingRecurrence().setDayNumber(oldDayNumber);
-        return (prevScheduleDate != null && getDate(accountActivationDate).compareTo(prevScheduleDate) > 0) ? null
-                : prevScheduleDate;
-    }
-
-    private Date getDate(Date date) {
-        return new Date(date.getTime());
-    }
-
-    /*
-     * from Rhino release the interest calculation date and posting are the same, which is at the end of month
-     */
-    private void setDetailsForMeeting(MeetingBO meeting) {
-        meeting.setStartDate(getFiscalStartDate());
-        if (meeting.isMonthly()) {
-            if (meeting.getMeetingTypeEnum() == MeetingType.SAVINGS_INTEREST_CALCULATION_TIME_PERIOD) {
-                // interest calculation date is the same date as posting date
-                meeting.setStartDate(DateUtils.addDays(getFiscalStartDate(), -1));
-                meeting.getMeetingDetails().getMeetingRecurrence().setDayNumber(SavingsConstants.POSTING_DAY);
-            } else if (meeting.getMeetingTypeEnum() == MeetingType.SAVINGS_INTEREST_POSTING) {
-                meeting.setStartDate(DateUtils.addDays(getFiscalStartDate(), -1));
-                meeting.getMeetingDetails().getMeetingRecurrence().setDayNumber(SavingsConstants.POSTING_DAY);
-            }
-        }
-    }
-
-    /**
-     * don't use this approach any more. use jodatime library
-     */
-    @Deprecated
-    public int calculateDays(Date fromDate, Date toDate) {
-        long oneDay = 1000 * 60 * 60 * 24;
-        long days = (getMFITime(toDate) / oneDay) - (getMFITime(fromDate) / oneDay);
-        return (int) days;
-    }
-
-    private long getMFITime(Date date) {
-        Calendar cal1 = new DateTimeService().getCurrentDateTime().toGregorianCalendar();
-        cal1.setTimeZone(Configuration.getInstance().getSystemConfig().getMifosTimeZone());
-        cal1.setTime(date);
-        return date.getTime() + cal1.get(Calendar.ZONE_OFFSET) + cal1.get(Calendar.DST_OFFSET);
     }
 
     public AccountActionDateEntity createActionDateObject(AccountBO account, CustomerBO customer, Short installmentId,
