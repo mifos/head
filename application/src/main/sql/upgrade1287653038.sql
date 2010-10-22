4c4e8b9d1e0cd3df63e0cfd80d9f087267e188f2
Vinod John <vinodcjohn@gmail.com>
2010-10-22 15:44:55 +0530

    Revert "[MIFOS-3973] Fixing deployment due to lookup_id duplication"
    This reverts commit 1d66da5e3165cf804047d0f0868a8a2036e0f8e9.

--- application/src/main/sql/upgrade1287653038.sql
+++ application/src/main/sql/upgrade1287653038.sql
@@ -1,5 +1,4 @@
-select max(lookup_id)+1 into @next_lookup_id from lookup_value;
 insert into lookup_value(lookup_id,entity_id,lookup_name)
-    values(@next_lookup_id, 37, 'InterestTypes-DecliningPrincipalBalance');
+    values(635, 37, 'InterestTypes-DecliningPrincipalBalance');
 insert into interest_types (interest_type_id, lookup_id, category_id, descripton)
-    values(5,@next_lookup_id,1,'InterestTypes-DecliningPrincipalBalance');
+    values(5,635,1,'InterestTypes-DecliningPrincipalBalance');
