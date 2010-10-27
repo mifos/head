f510eaa81c71f0479fd4ef76d8620286de5f5591
Adam Monsen <haircut@gmail.com>
2010-10-26 23:41:35 -0700

    Provide clean i18n of messages with links.
    This allows L10n of messages as whole phrases (good for translators),
    while still allowing link target definition in Freemarker templates
    (good for developers).
    
    I only converted two patchwork messages, more may exist. Instead of
    modifying the existing message keys, I created new ones.
    
    Related discussions:
    http://forum.springsource.org/showthread.php?p=325658
    http://translatewiki.net/w/i.php?title=Thread:Translating_talk:Mifos/Inconsistent_capitalisation%3F
    http://thread.gmane.org/gmane.comp.finance.mifos.devel/9987

---
+++ userInterface/src/main/java/org/mifos/ui/ftl/MarkdownLinkFilterWriter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2005-2010 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.ui.ftl;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import com.petebevin.markdown.MarkdownProcessor;
+
+/**
+ * A {@link Writer} that transforms the plain-text Markdown character stream to HTML and forwards it to another
+ * {@link Writer}. Note that the marked-down text is further edited: the paragraph element (automatically and always
+ * added by Markdown) is removed. The intended use of this class is within a directive which creates HTML links from
+ * i18n messages, and it may be undesirable to also wrap a message in a paragraph element.
+ */
+public class MarkdownLinkFilterWriter extends Writer {
+
+    private final Writer out;
+    private final String dest;
+
+    MarkdownLinkFilterWriter(Writer out, String dest) {
+        super();
+        this.out = out;
+        this.dest = dest;
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        StringBuffer slurped = new StringBuffer();
+        for (int i = 0; i < len; i++) {
+            slurped.append(cbuf[i + off]);
+        }
+        String replaced = String.format(slurped.toString(), dest);
+        String markedDown = new MarkdownProcessor().markdown(replaced);
+        markedDown = markedDown.replace("<p>", "");
+        markedDown = markedDown.replace("</p>", "");
+        out.write(markedDown);
+    }
+
+    public void flush() throws IOException {
+        out.flush();
+    }
+
+    public void close() throws IOException {
+        out.close();
+    }
+}
---
+++ userInterface/src/main/java/org/mifos/ui/ftl/MarkdownLinker.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2005-2010 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.ui.ftl;
+
+import java.io.IOException;
+import java.util.Map;
+
+import freemarker.core.Environment;
+import freemarker.template.SimpleScalar;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateDirectiveModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+/**
+ * A Freemarker directive useful for generating HTML from phrases containing links.
+ */
+public class MarkdownLinker implements TemplateDirectiveModel {
+
+    @Override
+    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
+            throws TemplateException, IOException {
+        if (body != null) {
+            // getAsString never returns null (unlike toString)
+            String dest = ((SimpleScalar) params.get("dest")).getAsString();
+            body.render(new MarkdownLinkFilterWriter(env.getOut(), dest));
+        } else {
+            throw new RuntimeException("missing body");
+        }
+    }
+}
