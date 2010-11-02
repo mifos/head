9be393c0ce509952bc5b38f96368b49c0a13bf2a
Kalyan Akella <kalyan.akella@gmail.com>
2010-11-02 19:06:33 +0530

    Fixing the failing acceptance test due to change in message text

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/questionnaire/QuestionTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/questionnaire/QuestionTest.java
@@ -54,7 +54,7 @@ public class QuestionTest extends UiTestCaseBase {
     private InitializeApplicationRemoteTestingService initRemote;
     private static final String START_DATA_SET = "acceptance_small_003_dbunit.xml";
     private String title;
-    private static final String TITLE_MISSING = "must match \"^.*[^\\s]+.*$\"";
+    private static final String TITLE_MISSING = "Please specify the question.";
     private static final String AT_LEAST_2_CHOICES = "Please specify at least 2 choices.";
     private static final String DUPLICATE_TITLE = "Question already exists.";
     private CreateQuestionParameters createQuestionParameters;
