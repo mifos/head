b8e268c8b8b3b27f41de54417bc9c1926c7d631f
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2010-11-11 20:46:26 -0800

    WIP on MIFOSBI-72. Code cleanup.

--- application/src/main/java/org/mifos/framework/util/PPITestDataGenerator.java
+++ application/src/main/java/org/mifos/framework/util/PPITestDataGenerator.java
@@ -20,6 +20,14 @@
 
 package org.mifos.framework.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -28,14 +36,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-import org.dbunit.DatabaseUnitException;
-import org.dbunit.database.DatabaseConfig;
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.operation.DatabaseOperation;
 import org.mifos.accounts.financial.exceptions.FinancialException;
-import org.mifos.accounts.savings.persistence.GenericDaoHibernate;
 import org.mifos.config.exceptions.ConfigurationException;
 import org.mifos.customers.business.CustomerBO;
 import org.mifos.customers.persistence.CustomerDao;
@@ -43,34 +44,14 @@ import org.mifos.customers.persistence.CustomerDaoHibernate;
 import org.mifos.framework.ApplicationInitializer;
 import org.mifos.framework.components.batchjobs.exceptions.TaskSystemException;
 import org.mifos.framework.exceptions.PersistenceException;
-import org.mifos.framework.hibernate.helper.StaticHibernateUtil;
-import org.mifos.framework.persistence.SqlExecutor;
-import org.mifos.framework.persistence.SqlResource;
 import org.mifos.platform.questionnaire.builders.QuestionGroupInstanceDtoBuilder;
 import org.mifos.platform.questionnaire.builders.QuestionGroupResponseDtoBuilder;
-import org.mifos.platform.questionnaire.domain.QuestionnaireService;
-import org.mifos.platform.questionnaire.domain.QuestionnaireServiceImpl;
 import org.mifos.platform.questionnaire.service.QuestionGroupDetail;
-import org.mifos.platform.questionnaire.service.QuestionType;
 import org.mifos.platform.questionnaire.service.QuestionnaireServiceFacade;
-import org.mifos.platform.questionnaire.service.QuestionnaireServiceFacadeImpl;
 import org.mifos.platform.questionnaire.service.SectionQuestionDetail;
-import org.mifos.platform.questionnaire.service.dtos.QuestionGroupInstanceDto;
 import org.mifos.service.test.TestingService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-import java.util.Properties;
 
 
 @SuppressWarnings( { "PMD.SystemPrintln", "PMD.SingularField" })
