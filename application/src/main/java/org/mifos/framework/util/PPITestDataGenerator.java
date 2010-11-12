f584f249615480b6ca4887610f44e1f8f29c9d34
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2010-11-11 17:07:28 -0800

    WIP on MIFOSBI-72.  Added ability to loop over test data scripts from a directory.

--- application/src/main/java/org/mifos/framework/util/PPITestDataGenerator.java
+++ application/src/main/java/org/mifos/framework/util/PPITestDataGenerator.java
@@ -61,6 +61,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.Connection;
@@ -68,64 +70,124 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Properties;
 
 
 @SuppressWarnings( { "PMD.SystemPrintln", "PMD.SingularField" })
 public class PPITestDataGenerator {
     // Command line flag configuration.
     private static final String HELP_OPTION_NAME = "h";
-
+    private static final String TEST_DATA_FILE_OPTION_NAME = "f";
+    private static final String TEST_DATA_DIRECTORY_OPTION_NAME = "a";
 
     // Options
     private final Options options = new Options();
     private Option helpOption;
-
+    private Option allFilesInDirecoryOption;
+    private Option testDataFileOption;
 
     // Variables for data from command line
+    String testDataDirectoryName;
+    String dataSetName;
 
-    public static void main(String[] args) throws URISyntaxException, TaskSystemException, PersistenceException, ConfigurationException, FinancialException {
+    QuestionnaireServiceFacade questionnaireServiceFacade;
+    CustomerDao customerDao;
+
+    public static void main(String[] args) throws URISyntaxException, TaskSystemException, PersistenceException, ConfigurationException, FinancialException, FileNotFoundException, IOException {
         PPITestDataGenerator util = new PPITestDataGenerator();
-        util.parseOptions(args);
 
-        ApplicationContext applicationContext = PPITestDataGenerator.initializeSpring();
-        util.upgrade(applicationContext);
+        util.loadTestDataFiles(args);
     }
 
     public PPITestDataGenerator() {
         defineOptions();
     }
 
-    private void upgrade(ApplicationContext applicationContext) throws TaskSystemException, PersistenceException, ConfigurationException, FinancialException {
+    private QuestionGroupDetail findQuestionGroup(String name) {
+        List<QuestionGroupDetail> list = questionnaireServiceFacade.getAllQuestionGroups();
+        for (QuestionGroupDetail qg: list) {
+            if (qg.getTitle().compareTo(name) == 0) {
+                return qg;
+            }
+        }
+        throw new RuntimeException("Could not find a question group named: " + name);
+    }
+
+
+    public void loadTestDataFiles(String[] args) throws IOException, URISyntaxException, TaskSystemException, PersistenceException, ConfigurationException, FinancialException {
+        parseOptions(args);
+        ApplicationContext applicationContext = initializeSpring();
+        customerDao = applicationContext.getBean(CustomerDaoHibernate.class);
+        questionnaireServiceFacade = applicationContext.getBean(QuestionnaireServiceFacade.class);
+
+        if (testDataDirectoryName != null) {
+            File directory = new File(testDataDirectoryName);
+
+            File[] listOfFiles = directory.listFiles();
+
+            if (listOfFiles != null ) {
+                int totalFilesToLoad = countFilesToUpgrade(listOfFiles);
+                int filesLoaded = 0;
+                for (File listOfFile : listOfFiles) {
+                    String currentFilename = listOfFile.getName();
+                    if (listOfFile.isFile() &&
+                            currentFilename.endsWith("Testing.properties")) {
+                        String dataFileName = testDataDirectoryName + File.separator + currentFilename;
+                        loadData(dataFileName, applicationContext);
+                        filesLoaded++;
+                        System.out.println("Finished " + filesLoaded + "/" + totalFilesToLoad);
+                    }
+                }
+            } else {
+                fail("No files found in directory: " + testDataDirectoryName);
+            }
+        } else {
+            loadData(dataSetName, applicationContext);
+        }
+    }
+
+    private int countFilesToUpgrade(File[] listOfFiles) {
+        int fileCount = 0;
+        for (File listOfFile : listOfFiles) {
+            String currentFilename = listOfFile.getName();
+            if (listOfFile.isFile() &&
+                    currentFilename.endsWith("Testing.properties")) {
+                ++fileCount;
+            }
+        }
+        return fileCount;
+    }
+
+    private void loadData(String filename, ApplicationContext applicationContext) throws TaskSystemException, PersistenceException, ConfigurationException, FinancialException, FileNotFoundException, IOException {
 
         System.setProperty(TestingService.TEST_MODE_SYSTEM_PROPERTY,"acceptance");
         ApplicationInitializer applicationInitializer = new ApplicationInitializer();
         applicationInitializer.dbUpgrade(applicationContext);
         applicationInitializer.setAttributesOnContext(null);
 
-        CustomerDao customerDao = applicationContext.getBean(CustomerDaoHibernate.class);
         CustomerBO customer = customerDao.findClientBySystemId("0006-000000063");
         System.out.println("Found: " + customer.getDisplayName());
 
-        QuestionnaireServiceFacade questionnaireServiceFacade = applicationContext.getBean(QuestionnaireServiceFacade.class);
-        questionnaireServiceFacade.uploadPPIQuestionGroup("BANGLADESH2009");
-        List<QuestionGroupDetail> list = questionnaireServiceFacade.getAllQuestionGroups();
-        assert(list.size() == 1);
+        Properties properties = new Properties();
 
-        QuestionGroupDetail qg = list.get(0);
+        properties.load(new FileInputStream(filename));
+
+        System.out.println("Loading data for: " + properties.getProperty("questionGroup.name"));
+        questionnaireServiceFacade.uploadPPIQuestionGroup(properties.getProperty("questionGroup.xml.filename"));
+
+        QuestionGroupDetail qg = findQuestionGroup(properties.getProperty("questionGroup.name"));
         Integer creatorId = 1;
+
 
         QuestionGroupInstanceDtoBuilder instanceBuilder = new QuestionGroupInstanceDtoBuilder();
         instanceBuilder.withQuestionGroup(qg.getId()).withCompleted(true).withCreator(creatorId).
             withEventSource(1).withEntity(customer.getCustomerId()).withVersion(1);
         List<SectionQuestionDetail> questions = qg.getSectionDetail(0).getQuestions();
         for (SectionQuestionDetail question: questions) {
+            String responseKey = "survey.1.question." + question.getSequenceNumber() + ".response.text";
+            String questionResponse = properties.getProperty(responseKey);
             QuestionGroupResponseDtoBuilder responseBuilder = new QuestionGroupResponseDtoBuilder();
-            if (question.getQuestionType() == QuestionType.DATE) {
-                responseBuilder.withSectionQuestion(question.getQuestionId()).withResponse("11/11/2010");
-            } else {
-                responseBuilder.withSectionQuestion(question.getQuestionId()).withResponse(
-                        question.getAnswerChoices().get(0).getValue());
-            }
+            responseBuilder.withSectionQuestion(question.getQuestionId()).withResponse(questionResponse);
             instanceBuilder.addResponses(responseBuilder.build());
         }
 
@@ -153,9 +215,21 @@ public class PPITestDataGenerator {
         .withDescription( "display help" )
         .create( HELP_OPTION_NAME );
 
+        allFilesInDirecoryOption = OptionBuilder.withArgName( "test data directory" )
+        .withLongOpt("dataDirectory")
+        .hasArg()
+        .withDescription( "Use all test data in this directory-- include the full path (e.g. /home/me/testData )" )
+        .create( TEST_DATA_DIRECTORY_OPTION_NAME );
+
+        testDataFileOption = OptionBuilder.withArgName( "test data file name" )
+        .withLongOpt("dataset")
+        .hasArg()
+        .withDescription( "Use the test data file with this name-- include the full file path (e.g. /home/me/testData/Bangladesh2009Testing.properties" )
+        .create( TEST_DATA_FILE_OPTION_NAME );
 
         options.addOption(helpOption);
-
+        options.addOption(allFilesInDirecoryOption);
+        options.addOption(testDataFileOption);
     }
 
     public void parseOptions(String[] args) throws URISyntaxException {
@@ -167,6 +241,16 @@ public class PPITestDataGenerator {
             if( line.hasOption( HELP_OPTION_NAME ) ) {
                 showHelp(options);
                 System.exit(0);
+            }
+            if( line.hasOption( TEST_DATA_FILE_OPTION_NAME ) ) {
+                if (line.hasOption( TEST_DATA_DIRECTORY_OPTION_NAME)) {
+                    fail("Specify either a data set (-f) or data directory (-a) but not both.");
+                }
+                dataSetName = line.getOptionValue(TEST_DATA_FILE_OPTION_NAME);
+            } else if( line.hasOption( TEST_DATA_DIRECTORY_OPTION_NAME ) ) {
+                testDataDirectoryName = line.getOptionValue(TEST_DATA_DIRECTORY_OPTION_NAME);
+            } else {
+                fail("Specify either a data set (-f) or data directory (-a)");
             }
 
         } catch( ParseException exp ) {
