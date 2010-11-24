e68ccce17e51574a38b00bc36febe3d62ae9d47d
Keith Woodlock <keithwoodlock@gmail.com>
2010-11-24 17:48:25 +0000

    MIFOS-4190: add service facade around viewing recent activity for customer accounts

--- serviceInterfaces/src/main/java/org/mifos/dto/screen/CustomerRecentActivityDto.java
+++ serviceInterfaces/src/main/java/org/mifos/dto/screen/CustomerRecentActivityDto.java
@@ -24,6 +24,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Locale;
 
+@SuppressWarnings("PMD")
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(value={"SE_NO_SERIALVERSIONID", "EI_EXPOSE_REP", "EI_EXPOSE_REP2"}, justification="should disable at filter level and also for pmd - not important for us")
 public class CustomerRecentActivityDto implements Serializable {
 
     public CustomerRecentActivityDto(java.util.Date activityDate, String description, String amount, String postedBy) {
