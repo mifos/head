c1cf0221f6f671b766f1e64ecab22ffa72c63084
Keith Woodlock <keithwoodlock@gmail.com>
2010-11-24 17:21:39 +0000

    MIFOS-4190: add service facade around viewing recent activity for customer accounts

---
+++ serviceInterfaces/src/main/java/org/mifos/dto/screen/CustomerRecentActivityDto.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2005-2010 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.dto.screen;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Locale;
+
+public class CustomerRecentActivityDto implements Serializable {
+
+    public CustomerRecentActivityDto(java.util.Date activityDate, String description, String amount, String postedBy) {
+        this.activityDate = new java.sql.Date(activityDate.getTime());
+        this.description = description;
+        this.amount = amount;
+        this.postedBy = postedBy;
+    }
+
+    public CustomerRecentActivityDto() {
+    }
+
+    private Date activityDate;
+    private String description;
+    private String amount = "-";
+    private String postedBy = "-";
+    private Locale locale = null;
+    private String userPrefferedDate = null;
+
+    public Date getActivityDate() {
+        return activityDate;
+    }
+
+    public void setActivityDate(Date activityDate) {
+        this.activityDate = activityDate;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
+
+    public String getUserPrefferedDate() {
+        return userPrefferedDate;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public void setUserPrefferedDate(String userPrefferedDate) {
+        this.userPrefferedDate = userPrefferedDate;
+    }
+}
