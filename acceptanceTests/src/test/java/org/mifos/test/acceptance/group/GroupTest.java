cb7d20d614e204810752c72fe8050218b1c109b4
Patryk Dereszewski <pdereszewski@soldevelo.com>
2011-01-11 16:46:51 +0100

    MIFOS-4448: MIFOSTEST-300 MIFOSTEST-301

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/group/GroupTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/group/GroupTest.java
@@ -30,6 +30,9 @@ import org.mifos.test.acceptance.framework.group.CenterSearchTransferGroupPage;
 import org.mifos.test.acceptance.framework.group.ConfirmCenterMembershipPage;
 import org.mifos.test.acceptance.framework.group.CreateGroupConfirmationPage;
 import org.mifos.test.acceptance.framework.group.CreateGroupEntryPage;
+import org.mifos.test.acceptance.framework.group.EditGroupStatusConfirmationPage;
+import org.mifos.test.acceptance.framework.group.EditGroupStatusPage;
+import org.mifos.test.acceptance.framework.group.EditGroupStatusParameters;
 import org.mifos.test.acceptance.framework.group.CreateGroupEntryPage.CreateGroupSubmitParameters;
 import org.mifos.test.acceptance.framework.group.CreateGroupSearchPage;
 import org.mifos.test.acceptance.framework.group.GroupViewDetailsPage;
@@ -105,29 +108,54 @@ public class GroupTest extends UiTestCaseBase {
 
     @Test(sequential = true, groups = {"smoke","group","acceptance","ui"})
     @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
+    // http://mifosforge.jira.com/browse/MIFOSTEST-301
     public void createGroupInPendingApprovalStateTest() throws Exception {
-
+        //Given
         initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_001_dbunit.xml", dataSource, selenium);
+        //When
         CreateGroupEntryPage groupEntryPage = loginAndNavigateToNewGroupPage();
         CreateGroupSubmitParameters formParameters = getGenericGroupFormParameters();
         CreateGroupConfirmationPage confirmationPage = groupEntryPage.submitNewGroupForApproval(formParameters);
         confirmationPage.verifyPage();
         GroupViewDetailsPage groupDetailsPage = confirmationPage.navigateToGroupDetailsPage();
         groupDetailsPage.verifyPage();
+        //Then
         groupDetailsPage.verifyStatus("Application Pending*");
+        //When
+        EditGroupStatusPage editGroupStatusPage = groupDetailsPage.navigateToEditGroupStatusPage();
+        EditGroupStatusParameters editGroupStatusParameters = new EditGroupStatusParameters();
+        editGroupStatusParameters.setStatus(EditGroupStatusParameters.ACTIVE);
+        editGroupStatusParameters.setNote("test");
+        EditGroupStatusConfirmationPage confirmationPage2 = editGroupStatusPage.submitAndNavigateToEditStatusConfirmationPage(editGroupStatusParameters);
+        GroupViewDetailsPage detailsPage = confirmationPage2.navigateToGroupDetailsPage();
+        //Then
+        detailsPage.verifyStatus("Active*");
     }
 
     @Test(sequential = true, groups = {"group","acceptance","ui"})
+    // http://mifosforge.jira.com/browse/MIFOSTEST-300
     @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
     public void createGroupInPartialApplicationStateTest() throws Exception {
+        //Given
         initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_001_dbunit.xml", dataSource, selenium);
+        //When
         CreateGroupEntryPage groupEntryPage = loginAndNavigateToNewGroupPage();
         CreateGroupSubmitParameters formParameters = getGenericGroupFormParameters();
         CreateGroupConfirmationPage confirmationPage = groupEntryPage.submitNewGroupForPartialApplication(formParameters);
         confirmationPage.verifyPage();
         GroupViewDetailsPage groupDetailsPage = confirmationPage.navigateToGroupDetailsPage();
         groupDetailsPage.verifyPage();
+        //Then
         groupDetailsPage.verifyStatus("Partial Application*");
+        //When
+        EditGroupStatusPage editGroupStatusPage = groupDetailsPage.navigateToEditGroupStatusPage();
+        EditGroupStatusParameters editGroupStatusParameters = new EditGroupStatusParameters();
+        editGroupStatusParameters.setStatus(EditGroupStatusParameters.PENDING_APPROVAL);
+        editGroupStatusParameters.setNote("test");
+        EditGroupStatusConfirmationPage confirmationPage2 = editGroupStatusPage.submitAndNavigateToEditStatusConfirmationPage(editGroupStatusParameters);
+        GroupViewDetailsPage detailsPage = confirmationPage2.navigateToGroupDetailsPage();
+        //Then
+        detailsPage.verifyStatus("Application Pending Approval*");
     }
 
     @Test(sequential = true, groups = {"group","acceptance","ui"})
@@ -152,7 +180,6 @@ public class GroupTest extends UiTestCaseBase {
     @Test(sequential = true, groups = {"smoke","group","acceptance","ui"})
     @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
     public void createGroupInPendingApprovalStateTestWithSurveys() throws Exception {
-
         String questionGroupTitle = "QG1" + random.nextInt(100);
         String question1 = "Nu_" + random.nextInt(100);
         String question2 = "SS_" + random.nextInt(100);
