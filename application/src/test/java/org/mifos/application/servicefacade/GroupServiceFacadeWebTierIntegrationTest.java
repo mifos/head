0ef0a3c1c4b69c1653d4834b466d967cd18113ef
Keith Woodlock <keithwoodlock@gmail.com>
2011-02-01 12:18:48 +0000

    MIFOS-4507: add integration test to verify api is capable of creating group with activation date in past

--- application/src/test/java/org/mifos/application/servicefacade/GroupServiceFacadeWebTierIntegrationTest.java
+++ application/src/test/java/org/mifos/application/servicefacade/GroupServiceFacadeWebTierIntegrationTest.java
@@ -29,14 +29,16 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mifos.application.meeting.business.MeetingBO;
 import org.mifos.config.ClientRules;
 import org.mifos.customers.group.business.GroupBO;
+import org.mifos.customers.office.business.OfficeBO;
 import org.mifos.customers.persistence.CustomerDao;
+import org.mifos.customers.personnel.business.PersonnelBO;
 import org.mifos.customers.util.helpers.CustomerStatus;
 import org.mifos.domain.builders.MeetingBuilder;
+import org.mifos.domain.builders.OfficeBuilder;
 import org.mifos.dto.domain.AddressDto;
 import org.mifos.dto.domain.ApplicableAccountFeeDto;
 import org.mifos.dto.domain.CustomerDetailsDto;
@@ -56,12 +58,14 @@ public class GroupServiceFacadeWebTierIntegrationTest extends MifosIntegrationTe
     @Autowired private AuthenticationAuthorizationServiceFacade authenticationAuthorizationService;
     @Autowired private CustomerDao customerDao;
 
+    private OfficeBO areaOffice1;
+    private OfficeBO branch1;
+
     @Before
     public void setup () {
         authenticationAuthorizationService.reloadUserDetailsForSecurityContext("mifos");
     }
 
-    @Ignore
     @Test
     public void shouldCreateGroupWithActivationDateInPast() {
 
@@ -74,13 +78,21 @@ public class GroupServiceFacadeWebTierIntegrationTest extends MifosIntegrationTe
         String displayName = "testGroup";
         String externalId = null;
         AddressDto addressDto = null;
-        Short loanOfficerId = null;
+
+        PersonnelBO user = IntegrationTestObjectMother.findPersonnelById(Short.valueOf("1"));
+        Short loanOfficerId = user.getPersonnelId();
         List<ApplicableAccountFeeDto> feesToApply = new ArrayList<ApplicableAccountFeeDto>();
         Short customerStatus = CustomerStatus.GROUP_ACTIVE.getValue();
         boolean trained = false;
         DateTime trainedOn = null;
         String parentSystemId = null;
-        Short officeId = IntegrationTestObjectMother.SAMPLE_BRANCH_OFFICE;
+
+        OfficeBO headOffice = IntegrationTestObjectMother.findOfficeById(Short.valueOf("1"));
+
+        // setup
+        createOfficeHierarchyUnderHeadOffice(headOffice);
+
+        Short officeId = branch1.getOfficeId();
         DateTime mfiJoiningDate = new DateTime().minusWeeks(2);
         DateTime activationDate = new DateTime().minusWeeks(1);
         GroupCreationDetail groupCenterDetail = new GroupCreationDetail(displayName, externalId, addressDto, loanOfficerId,
@@ -94,5 +106,24 @@ public class GroupServiceFacadeWebTierIntegrationTest extends MifosIntegrationTe
         ClientRules.setCenterHierarchyExists(centerHierarchyExistsOriginal);
         GroupBO group = customerDao.findGroupBySystemId(newlyCreatedGroupDetails.getGlobalCustNum());
         Assert.assertThat(new LocalDate(group.getCustomerActivationDate()), is(activationDate.toLocalDate()));
+    }
+
+
+    private void createOfficeHierarchyUnderHeadOffice(OfficeBO headOffice) {
+        areaOffice1 = new OfficeBuilder().areaOffice()
+                                                  .withParentOffice(headOffice)
+                                                  .withName("areaOffice1")
+                                                  .withGlobalOfficeNum("x002")
+                                                  .withSearchId("1.1.2")
+                                                  .build();
+        IntegrationTestObjectMother.createOffice(areaOffice1);
+
+        branch1 = new OfficeBuilder().branchOffice()
+                                              .withParentOffice(areaOffice1)
+                                              .withName("branch1")
+                                              .withGlobalOfficeNum("x005")
+                                              .withSearchId("1.1.2.1")
+                                              .build();
+        IntegrationTestObjectMother.createOffice(branch1);
     }
 }
