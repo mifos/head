4a672f13e7ed2d9a5d62250e1472a3c633c80e85
Vinod John <vinodcjohn@gmail.com>
2011-02-03 17:10:44 +0530

    [MIFOS-4220] Adding unix shell script for liquibase upgrade

--- db/deploy/liquibase.sh
+++ db/deploy/liquibase.sh
@@ -29,20 +29,20 @@ fi
 # build classpath from all jars in lib
 if [ -f /usr/bin/cygpath ]; then
   CP=.
-  for i in "$LIQUIBASE_HOME"/liquibase*.jar; do
+  for i in "$LIQUIBASE_HOME"/../liquibase*.jar; do
     i=`cygpath --windows "$i"`
     CP="$CP;$i"
   done
-  for i in "$LIQUIBASE_HOME"/lib/*.jar; do
+  for i in "$LIQUIBASE_HOME"/../lib/*.jar; do
     i=`cygpath --windows "$i"`
     CP="$CP;$i"
   done
 else
   CP=.
-  for i in "$LIQUIBASE_HOME"/liquibase*.jar; do
+  for i in "$LIQUIBASE_HOME"/../liquibase*.jar; do
     CP="$CP":"$i"
   done
-  for i in "$LIQUIBASE_HOME"/lib/*.jar; do
+  for i in "$LIQUIBASE_HOME"/../lib/*.jar; do
     CP="$CP":"$i"
   done
 fi
@@ -50,4 +50,15 @@ fi
 # add any JVM options here
 JAVA_OPTS=
 
-java -cp "$CP" $JAVA_OPTS liquibase.integration.commandline.Main ${1+"$@"}
+DB_PROPS_FILE=$LIQUIBASE_HOME/../mifos-db.properties
+CHANGE_LOG_FILE=$LIQUIBASE_HOME/../changesets/changelog-master.xml
+
+DRIVER=com.mysql.jdbc.Driver
+DEFAULT_SCHEMA_NAME=`sed '/^\#/d' $DB_PROPS_FILE | grep 'defaultSchemaName'  | tail -n 1 | cut -d "=" -f2-`
+USERNAME=`sed '/^\#/d' $DB_PROPS_FILE | grep 'username'  | tail -n 1 | cut -d "=" -f2-`
+PASSWORD=`sed '/^\#/d' $DB_PROPS_FILE | grep 'password'  | tail -n 1 | cut -d "=" -f2-`
+HOST=`sed '/^\#/d' $DB_PROPS_FILE | grep 'databaseHost'  | tail -n 1 | cut -d "=" -f2-`
+PORT=`sed '/^\#/d' $DB_PROPS_FILE | grep 'databasePort'  | tail -n 1 | cut -d "=" -f2-`
+URL=jdbc:mysql://$HOST:$PORT/$DEFAULT_SCHEMA_NAME
+
+java -cp "$CP" $JAVA_OPTS liquibase.integration.commandline.Main --changeLogFile=$CHANGE_LOG_FILE --driver=$DRIVER --url=$URL --username=$USERNAME --password=$PASSWORD --defaultSchemaName=$DEFAULT_SCHEMA_NAME ${1+"$@"}
