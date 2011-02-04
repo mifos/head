536975182bda90b0b22ff5eb1d536c4aa986af31
Stanley Kwok <skwoka@gmail.com>
2011-02-03 16:14:23 -0800

    MIFOS-4516: added test cases for remaining state transitions

--- application/src/test/java/org/mifos/ui/webflow/CreateSavingsAccountTest.java
+++ application/src/test/java/org/mifos/ui/webflow/CreateSavingsAccountTest.java
@@ -20,9 +20,14 @@
 
 package org.mifos.ui.webflow;
 
-import org.junit.Before;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Test;
 import org.mifos.platform.validation.MifosBeanValidator;
+import org.mifos.ui.core.controller.CreateSavingsAccountController;
+import org.mifos.ui.core.controller.CreateSavingsAccountFormBean;
+import org.mockito.Mock;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.webflow.config.FlowDefinitionResource;
 import org.springframework.webflow.config.FlowDefinitionResourceFactory;
@@ -30,12 +35,13 @@ import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.core.collection.MutableAttributeMap;
 import org.springframework.webflow.test.MockExternalContext;
 import org.springframework.webflow.test.MockFlowBuilderContext;
+import org.springframework.webflow.test.MockParameterMap;
 import org.springframework.webflow.test.execution.AbstractXmlFlowExecutionTests;
 
 /**
  * Tests Create Savings Account webflow.
  * 
- * @see http
+ * @see http 
  *      ://static.springsource.org/spring-webflow/docs/2.0.x/reference/htmlsingle
  *      /spring-webflow-reference.html#testing
  */
@@ -44,11 +50,10 @@ public class CreateSavingsAccountTest extends AbstractXmlFlowExecutionTests {
     MifosBeanValidator validator = new MifosBeanValidator();
     LocalValidatorFactoryBean targetValidator = new LocalValidatorFactoryBean();
 
-    @Before
-    public void setUp() {
-        targetValidator.afterPropertiesSet();
-        validator.setTargetValidator(targetValidator);
-    }
+    @Mock
+    CreateSavingsAccountController controller = mock(CreateSavingsAccountController.class);
+
+    CreateSavingsAccountFormBean formBean = new CreateSavingsAccountFormBean();
 
     @Test
     public void testStartFlow() {
@@ -60,15 +65,142 @@ public class CreateSavingsAccountTest extends AbstractXmlFlowExecutionTests {
         assertCurrentStateEquals("customerSearchStep");
     }
 
+    @Test
+    public void testCustomerSearchStep_SearchTermEntered() {
+
+        setCurrentState("customerSearchStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("searchTermEntered");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("selectCustomerStep");
+    }
+
+    @Test
+    public void testSelectCustomerStep_CustomerSelected() {
+
+        setCurrentState("selectCustomerStep");
+
+        doNothing().when(controller).getProductOfferings(formBean);
+
+        MockExternalContext context = new MockExternalContext();
+        MockParameterMap requestParameterMap = new MockParameterMap();
+        requestParameterMap.put("customerId", "1");
+        context.setRequestParameterMap(requestParameterMap);
+        context.setEventId("customerSelected");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("selectProductOfferingStep");
+    }
+
+    @Test
+    public void testSelectCustomerStep_SearchTermEntered() {
+
+        setCurrentState("selectCustomerStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("searchTermEntered");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("selectCustomerStep");
+    }
+
+    public void testSelectProductOfferingStep_ProductSelected() {
+
+        setCurrentState("selectProductOfferingStep");
+
+        doNothing().when(controller).loadProduct(1, formBean);
+
+        MockExternalContext context = new MockExternalContext();
+        MockParameterMap requestParameterMap = new MockParameterMap();
+        requestParameterMap.put("productId", "1");
+        context.setRequestParameterMap(requestParameterMap);
+        context.setEventId("productSelected");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("enterAccountDetailsStep");
+    }
+
+    @Test
+    public void testEnterAccountDetailsStep_DetailsEntered() {
+
+        setCurrentState("enterAccountDetailsStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("detailsEntered");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("previewStep");
+    }
+
+    @Test
+    public void testEnterAccountDetailsStep_NewProductSelected() {
+
+        setCurrentState("enterAccountDetailsStep");
+
+        MockExternalContext context = new MockExternalContext();
+        MockParameterMap requestParameterMap = new MockParameterMap();
+        requestParameterMap.put("productId", "1");
+        context.setRequestParameterMap(requestParameterMap);
+        context.setEventId("newProductSelected");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("enterAccountDetailsStep");
+    }
+
+    @Test
+    public void testPreviewStep_Edit() {
+
+        setCurrentState("previewStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("edit");
+        resumeFlow(context);
+
+        assertCurrentStateEquals("enterAccountDetailsStep");
+    }
+
+    @Test
+    public void testPreviewStep_SaveForLater() {
+
+        setCurrentState("previewStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("saveForLater");
+        resumeFlow(context);
+
+        assertFlowExecutionEnded();
+        assertFlowExecutionOutcomeEquals("complete");
+    }
+
+    @Test
+    public void testPreviewStep_SaveForApproval() {
+
+        setCurrentState("previewStep");
+
+        MockExternalContext context = new MockExternalContext();
+        context.setEventId("saveForApproval");
+        resumeFlow(context);
+
+        assertFlowExecutionEnded();
+        assertFlowExecutionOutcomeEquals("complete");
+    }
+
     @Override
     protected void configureFlowBuilderContext(
             MockFlowBuilderContext builderContext) {
+
         builderContext.registerBean("validator", validator);
+        builderContext.registerBean("createSavingsAccountController",
+                controller);
+        builderContext.registerBean("savingsAccountFormBean", formBean);
     }
 
     @Override
     protected FlowDefinitionResource getResource(
             FlowDefinitionResourceFactory resourceFactory) {
+
         FlowDefinitionResource resource = resourceFactory
                 .createFileResource("src/main/resources/META-INF/resources/WEB-INF/flows/createSavingsAccount.xml");
         return resource;
