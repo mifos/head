2185609f0adc85dd8fdb25525c7b67068abed7a7
Kalyan Akella <kalyan.akella@gmail.com>
2011-02-09 11:26:54 +0530

    Revert "[MIFOS-4624] Fixing the bug. Avoiding re-computing the principal if it is zero. Mitigates the grace period issue for daily interest calculations."
    This reverts commit 2f15404e27a79770b32beec84ffe5823cac7eeb6. It was instead
    decided to enforce that no PAWDEP loan product will be allowed when
    Grace periods are turned on.

--- appdomain/src/main/java/org/mifos/accounts/loan/business/service/LoanBusinessService.java
+++ appdomain/src/main/java/org/mifos/accounts/loan/business/service/LoanBusinessService.java
@@ -237,7 +237,7 @@ public class LoanBusinessService implements BusinessService {
             Money miscPenality = installment.getMiscPenalty();
             Money total = installment.getTotalValue();
             Money principal = total.subtract(interest.add(fees).add(miscFee).add(miscPenality));
-            if (!installment.isPrincipalZero()) installment.setPrincipalAndInterest(interest, principal);
+            installment.setPrincipalAndInterest(interest, principal);
             initialDueDate = currentDueDate;
             principalOutstanding = principalOutstanding.subtract(principal);
             runningPrincipal = runningPrincipal.add(principal);
