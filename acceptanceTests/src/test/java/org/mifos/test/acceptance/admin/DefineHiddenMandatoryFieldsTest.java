258f4bea2755228f2f0cb009394f65188d232936
Vinod John <vinodcjohn@gmail.com>
2011-02-18 16:55:18 +0530

    [MIFOS-4590] Removing db unit dependency for a few tests

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/admin/DefineHiddenMandatoryFieldsTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/admin/DefineHiddenMandatoryFieldsTest.java
@@ -20,15 +20,11 @@
 
 package org.mifos.test.acceptance.admin;
 
-import org.mifos.framework.util.DbUnitUtilities;
 import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.UiTestCaseBase;
 import org.mifos.test.acceptance.framework.admin.AdminPage;
 import org.mifos.test.acceptance.framework.admin.DefineHiddenMandatoryFieldsPage;
 import org.mifos.test.acceptance.framework.testhelpers.NavigationHelper;
-import org.mifos.test.acceptance.remote.InitializeApplicationRemoteTestingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -36,16 +32,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(locations={"classpath:ui-test-context.xml"})
-@Test(sequential=true, groups={"acceptance","ui","admin"})
+@Test(sequential=true, groups={"acceptance","ui","admin","no_db_unit"})
 public class DefineHiddenMandatoryFieldsTest  extends UiTestCaseBase{
     private NavigationHelper navigationHelper;
-
-    @Autowired
-    private DriverManagerDataSource dataSource;
-    @Autowired
-    private DbUnitUtilities dbUnitUtilities;
-    @Autowired
-    private InitializeApplicationRemoteTestingService initRemote;
 
     @Override
     @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
@@ -53,7 +42,6 @@ public class DefineHiddenMandatoryFieldsTest  extends UiTestCaseBase{
     public void setUp() throws Exception {
         super.setUp();
         navigationHelper = new NavigationHelper(selenium);
-        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_008_dbunit.xml", dataSource, selenium);
     }
 
     @AfterMethod
@@ -79,7 +67,7 @@ public class DefineHiddenMandatoryFieldsTest  extends UiTestCaseBase{
         defineHiddenMandatoryFieldsPage.checkMandatoryEthnicity();
         adminPage = defineHiddenMandatoryFieldsPage.submit();
         adminPage.navigateToClientsAndAccountsPageUsingHeaderTab().navigateToCreateNewClientPage()
-            .navigateToCreateClientWithoutGroupPage().chooseOffice("MyOffice1232993831593");
+            .navigateToCreateClientWithoutGroupPage().chooseOffice("MyOfficeDHMFT");
         //Then
         Assert.assertTrue(selenium.isTextPresent("*Ethnicity:"));
         Assert.assertFalse(selenium.isElementPresent("create_ClientPersonalInfo.input.spouseSecondLastName"));
