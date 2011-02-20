723865daa145f93de5164a965717d1d25524189b
Jakub Slawinski <jeremian@poczta.fm>
2011-02-20 13:39:32 +0100

    Removed unnecessary dbunit verifications and redundant tests.

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/ClientLoanTransactionHistoryTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/ClientLoanTransactionHistoryTest.java
@@ -25,11 +25,8 @@ import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 import org.dbunit.DatabaseUnitException;
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.IDataSet;
 import org.joda.time.DateTime;
 import org.mifos.framework.util.DbUnitUtilities;
-import org.mifos.test.acceptance.collectionsheet.CollectionSheetEntryCustomerAccountTest;
 import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.UiTestCaseBase;
 import org.mifos.test.acceptance.framework.loan.CreateLoanAccountSearchParameters;
@@ -137,68 +134,5 @@ public class ClientLoanTransactionHistoryTest extends UiTestCaseBase {
         loanTestHelper.applyPayment(loanId, paymentParameters);
         //Then
         loanTestHelper.verifyTransactionHistory(loanId, Double.valueOf(paymentParameters.getAmount()));
-    }
-
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-    public void transactionHistoryTest() throws Exception {
-        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_003_dbunit.xml", dataSource, selenium);
-
-        /*
-         * This test consists of:
-         * 1. Make a single payment of the fee, 900
-         * 2. Make a single payment of the principal and interest, 183.
-         * 3. Verify the results (loan_trxn_detail table).
-         *
-         * The data set contains a loan w/ id 000100000000174 that's disbursed and who's loan
-         * product contains a fee.
-         */
-
-        PaymentParameters paymentParameters = new PaymentParameters();
-        paymentParameters.setAmount("990"); // this covers the fee (990)
-        paymentParameters.setTransactionDateDD("06");
-        paymentParameters.setTransactionDateMM("02");
-        paymentParameters.setTransactionDateYYYY("2009");
-        paymentParameters.setPaymentType(PaymentParameters.CASH);
-
-        loanTestHelper.applyPayment("000100000000174", paymentParameters);
-
-        // use the same date and payment type for the principal and the interest
-        paymentParameters.setAmount("183");
-        loanTestHelper.applyPayment("000100000000174", paymentParameters);
-
-        verifyCollectionSheetData("ClientLoanTransactionHistory_001_result_dbunit.xml");
-    }
-
-
-    // the code below is the same that's used in CollectionSheetEntryCustomerAccountTest.
-
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException") // one of the dependent methods throws Exception
-    private void verifyCollectionSheetData(String filename) throws Exception {
-        IDataSet expectedDataSet = dbUnitUtilities.getDataSetFromDataSetDirectoryFile(filename);
-        IDataSet databaseDataSet = dbUnitUtilities.getDataSetForTables(dataSource, new String[] { FEE_TRXN_DETAIL,
-                                   ACCOUNT_TRXN,
-                                   LOAN_TRXN_DETAIL,
-                                   ACCOUNT_PAYMENT,
-                                   LOAN_SCHEDULE,
-                                   LOAN_SUMMARY,
-                                   LOAN_ACTIVITY_DETAILS,
-                                   ACCOUNT_STATUS_CHANGE_HISTORY,
-                                   FINANCIAL_TRXN,
-                                   CUSTOMER_ACCOUNT_ACTIVITY,
-                                   CUSTOMER_TRXN_DETAIL });
-
-
-        verifyTablesWithoutSorting(expectedDataSet, databaseDataSet);
-
-        /* The order that the transactions are entered into the database (and accordingly the
-         * id that they're assigned) is as far as I can tell random. This means that the sorting
-         * is random and not verifyable.
-         */
-        //verifyTransactionsAfterSortingTables(expectedDataSet, databaseDataSet);
-
-    }
-    private void verifyTablesWithoutSorting(IDataSet expectedDataSet, IDataSet databaseDataSet) throws DataSetException,
-    DatabaseUnitException {
-        dbUnitUtilities.verifyTables(new String[] { CollectionSheetEntryCustomerAccountTest.CUSTOMER_ACCOUNT_ACTIVITY }, databaseDataSet, expectedDataSet);
     }
 }
