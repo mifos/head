01374acb55f5fc8727ec97ecaf809ac2d1044d92
Vinod John <vinodcjohn@gmail.com>
2011-02-22 16:26:34 +0530

    [MIFOS-4590] Removing db unit dependency for a few more tests

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/LoanAccountAdjustmentsTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/LoanAccountAdjustmentsTest.java
@@ -21,7 +21,6 @@
 package org.mifos.test.acceptance.loan;
 
 import org.joda.time.DateTime;
-import org.mifos.framework.util.DbUnitUtilities;
 import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.UiTestCaseBase;
 import org.mifos.test.acceptance.framework.loan.LoanAccountPage;
@@ -29,43 +28,24 @@ import org.mifos.test.acceptance.framework.loan.RedoLoanDisbursalParameters;
 import org.mifos.test.acceptance.framework.loan.TransactionHistoryPage;
 import org.mifos.test.acceptance.framework.testhelpers.LoanTestHelper;
 import org.mifos.test.acceptance.remote.DateTimeUpdaterRemoteTestingService;
-import org.mifos.test.acceptance.remote.InitializeApplicationRemoteTestingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-@ContextConfiguration(locations={"classpath:ui-test-context.xml"})
-@Test(sequential=true, groups={"acceptance", "ui", "loan"})
+@ContextConfiguration(locations = {"classpath:ui-test-context.xml"})
+@Test(sequential = true, groups = {"acceptance", "ui", "loan", "no_db_unit"})
 public class LoanAccountAdjustmentsTest extends UiTestCaseBase {
     private LoanTestHelper loanTestHelper;
-
-    @Autowired
-    private DriverManagerDataSource dataSource;
-    @Autowired
-    private DbUnitUtilities dbUnitUtilities;
-    @Autowired
-    private InitializeApplicationRemoteTestingService initRemote;
-
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-    @BeforeClass
-    public void setUpClass() throws Exception {
-        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_003_dbunit.xml", dataSource, selenium);
-    }
 
     @Override
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     @BeforeMethod
     public void setUp() throws Exception {
         super.setUp();
-
         DateTimeUpdaterRemoteTestingService dateTimeUpdaterRemoteTestingService = new DateTimeUpdaterRemoteTestingService(selenium);
-        DateTime targetTime = new DateTime(2011,2,1,15,0,0,0);
+        DateTime targetTime = new DateTime(2011, 3, 23, 15, 0, 0, 0);
         dateTimeUpdaterRemoteTestingService.setDateTime(targetTime);
-
         loanTestHelper = new LoanTestHelper(selenium);
     }
 
@@ -78,21 +58,20 @@ public class LoanAccountAdjustmentsTest extends UiTestCaseBase {
      * Verify loan account status changes when number of days in arrears becomes greater than
      * the lateness definition by performing multiple adjustments when Account status is "Active in Good Standing"
      * http://mifosforge.jira.com/browse/MIFOSTEST-30
+     *
      * @throws Exception
      */
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public void verifyAccountStatusAfterMultipleAdjustments() throws Exception {
-        String client = "Stu1233266134755 Client1233266134755";
+        String client = "Stu1233266063395 Client1233266063395";
         RedoLoanDisbursalParameters redoParams = new RedoLoanDisbursalParameters();
-        redoParams.setDisbursalDateDD("06");
-        redoParams.setDisbursalDateMM("01");
+        redoParams.setDisbursalDateDD("21");
+        redoParams.setDisbursalDateMM("02");
         redoParams.setDisbursalDateYYYY("2011");
-
         LoanAccountPage loanAccountPage = loanTestHelper.redoLoanDisbursal(client, "WeeklyFlatLoanWithOneTimeFees", redoParams, null, 0, false);
         String loanID = loanAccountPage.getAccountId();
         loanAccountPage = loanTestHelper.applyMultipleAdjustments(loanID, 2);
-
-        loanAccountPage.verifyPerformanceHistory("1", "2");
+        loanAccountPage.verifyPerformanceHistory("2", "2");
         loanAccountPage.verifyStatus(LoanAccountPage.ACTIVE_BAD);
     }
 
@@ -100,25 +79,26 @@ public class LoanAccountAdjustmentsTest extends UiTestCaseBase {
      * Verify multiple adjustment changes are logged in transaction history with proper GL codes,
      * account summary changes when the account status is "Active in Bad Standing".
      * http://mifosforge.jira.com/browse/MIFOSTEST-27
+     *
      * @throws Exception
      */
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public void verifyChangesInTransactionHistory() throws Exception {
         RedoLoanDisbursalParameters redoParams = new RedoLoanDisbursalParameters();
-        redoParams.setDisbursalDateDD("07");
-        redoParams.setDisbursalDateMM("01");
-        redoParams.setDisbursalDateYYYY("2010");
+        redoParams.setDisbursalDateDD("04");
+        redoParams.setDisbursalDateMM("03");
+        redoParams.setDisbursalDateYYYY("2011");
 
-        LoanAccountPage loanAccountPage = loanTestHelper.redoLoanDisbursal("MyGroup1233266255641", "WeeklyGroupFlatLoanWithOnetimeFee", redoParams, null, 0, false);
+        LoanAccountPage loanAccountPage = loanTestHelper.redoLoanDisbursal("MyGroup1233266297718", "WeeklyGroupFlatLoanWithOnetimeFee", redoParams, null, 0, false);
 
         String loanID = loanAccountPage.getAccountId();
-        loanAccountPage = loanTestHelper.applyMultipleAdjustments(loanID, 5);
+        loanAccountPage = loanTestHelper.applyMultipleAdjustments(loanID, 2);
 
         loanAccountPage.verifyStatus(LoanAccountPage.ACTIVE_BAD);
-        loanAccountPage.verifyTotalOriginalLoan("4290.0");
-        loanAccountPage.verifyTotalAmountPaid("3951.0");
-        loanAccountPage.verifyAccountSummary("339.0", "03/02/2011", "339.0");
+        loanAccountPage.verifyTotalOriginalLoan("1029.0");
+        loanAccountPage.verifyTotalAmountPaid("0.0");
+        loanAccountPage.verifyAccountSummary("775.0", "25/03/2011", "520.0");
         TransactionHistoryPage transactionHistoryPage = loanAccountPage.navigateToTransactionHistory();
-        transactionHistoryPage.verifyTransactionHistory(3951.0, 52, 238);
+        transactionHistoryPage.verifyTransactionHistory(510.8, 2, 22);
     }
 }
