fff83720d426dc54a20287f3ed22dd3fb1621bdd
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2011-02-25 18:08:19 -0800

    WIP on MIFOS-4633. Added MessageCustomizerDAO to support persisting custom messages.

--- appdomain/src/main/java/org/mifos/application/messagecustomizer/CustomMessage.java
+++ appdomain/src/main/java/org/mifos/application/messagecustomizer/CustomMessage.java
@@ -1,7 +1,32 @@
 package org.mifos.application.messagecustomizer;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@NamedQueries( {
+	  @NamedQuery(
+	    name="allMessages",
+	    query="from CustomMessage "
+	  ),
+	  @NamedQuery(
+	          name = "findCustomMessageByOldMessage",
+	          query = "from CustomMessage message where message.oldMessage=:oldMessage"
+	  )	  
+})
+
+@Entity
+@Table(name = "custom_message")
 public class CustomMessage {
 
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Short id;	
 	private String oldMessage;
 	private String newMessage;
 	
