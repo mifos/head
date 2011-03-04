f437e1a3143db6a4c45bcfb76140d0b97a37b0c5
Vinod John <vinodcjohn@gmail.com>
2011-03-04 13:20:34 +0530

    [MIFOS-4590] Acceptance test improvement

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/ClientLoanStatusHistoryTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/loan/ClientLoanStatusHistoryTest.java
@@ -21,36 +21,23 @@
 package org.mifos.test.acceptance.loan;
 
 import org.joda.time.DateTime;
-import org.mifos.framework.util.DbUnitUtilities;
 import org.mifos.test.acceptance.framework.MifosPage;
 import org.mifos.test.acceptance.framework.UiTestCaseBase;
-import org.mifos.test.acceptance.framework.loan.CreateLoanAccountSearchParameters;
-import org.mifos.test.acceptance.framework.loan.CreateLoanAccountSubmitParameters;
 import org.mifos.test.acceptance.framework.loan.EditLoanAccountStatusParameters;
 import org.mifos.test.acceptance.framework.testhelpers.LoanTestHelper;
 import org.mifos.test.acceptance.remote.DateTimeUpdaterRemoteTestingService;
-import org.mifos.test.acceptance.remote.InitializeApplicationRemoteTestingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-@ContextConfiguration(locations={"classpath:ui-test-context.xml"})
-@Test(sequential=true, groups={"acceptance","ui", "loan"})
+@ContextConfiguration(locations = {"classpath:ui-test-context.xml"})
+@Test(sequential = true, groups = {"acceptance", "ui", "loan", "no_db_unit"})
 public class ClientLoanStatusHistoryTest extends UiTestCaseBase {
     private LoanTestHelper loanTestHelper;
 
     // acceptance_small_007 account id's:
-    private static final String ACCOUNT_APPROVED_ID = "000100000000005";
-
-    @Autowired
-    private DriverManagerDataSource dataSource;
-    @Autowired
-    private DbUnitUtilities dbUnitUtilities;
-    @Autowired
-    private InitializeApplicationRemoteTestingService initRemote;
+    private static final String ACCOUNT_APPROVED_ID = "000100000000052";
 
     @Override
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
@@ -60,7 +47,7 @@ public class ClientLoanStatusHistoryTest extends UiTestCaseBase {
         super.setUp();
 
         DateTimeUpdaterRemoteTestingService dateTimeUpdaterRemoteTestingService = new DateTimeUpdaterRemoteTestingService(selenium);
-        DateTime targetTime = new DateTime(2009,7,4,12,0,0,0);
+        DateTime targetTime = new DateTime(2009, 7, 4, 12, 0, 0, 0);
         dateTimeUpdaterRemoteTestingService.setDateTime(targetTime);
 
         loanTestHelper = new LoanTestHelper(selenium);
@@ -73,7 +60,6 @@ public class ClientLoanStatusHistoryTest extends UiTestCaseBase {
 
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public void approvedToRejected() throws Exception {
-        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_007_dbunit.xml", dataSource, selenium);
 
         EditLoanAccountStatusParameters params = new EditLoanAccountStatusParameters();
         params.setStatus(EditLoanAccountStatusParameters.CANCEL);
@@ -87,15 +73,7 @@ public class ClientLoanStatusHistoryTest extends UiTestCaseBase {
 
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public void newLoanToPartialApplicationToPendingApprovalToApproved() throws Exception {
-        initRemote.dataLoadAndCacheRefresh(dbUnitUtilities, "acceptance_small_007_dbunit.xml", dataSource, selenium);
-        CreateLoanAccountSearchParameters searchParameters = new CreateLoanAccountSearchParameters();
-        CreateLoanAccountSubmitParameters submitAccountParameters = new CreateLoanAccountSubmitParameters();
-
-        searchParameters.setLoanProduct("Cattle Loan");
-        searchParameters.setSearchString("First Tester");
-        submitAccountParameters.setAmount("1000");
-
-        String loanId = loanTestHelper.createLoanAccount(searchParameters, submitAccountParameters).getAccountId();
+        String loanId = "000100000000053";
         loanTestHelper.verifyLastEntryInStatusHistory(loanId, EditLoanAccountStatusParameters.NEW, EditLoanAccountStatusParameters.PENDING_APPROVAL);
 
         EditLoanAccountStatusParameters params = new EditLoanAccountStatusParameters();
