b790e87282be8e065acc457679882956252ffeb1
Van Mittal-Henkle <van.mittalhenkle@gmail.com>
2011-03-17 16:53:20 -0700

    WIP on MIFOS-4831.  Removed unused imports.

--- userInterface/src/main/java/org/mifos/ui/core/controller/CustomizeTextController.java
+++ userInterface/src/main/java/org/mifos/ui/core/controller/CustomizeTextController.java
@@ -28,7 +28,6 @@ import org.mifos.application.admin.servicefacade.CustomizedTextDto;
 import org.mifos.application.admin.servicefacade.CustomizedTextServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
