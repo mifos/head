faf030a1984da1cb1a12c0fc61056e359644d3f9
Keith Woodlock <keithwoodlock@gmail.com>
2011-03-29 18:49:28 +0100

    MIFOS-4684: fix pmd warnings

--- serviceInterfaces/src/main/java/org/mifos/dto/screen/LoanCreationProductDetailsDto.java
+++ serviceInterfaces/src/main/java/org/mifos/dto/screen/LoanCreationProductDetailsDto.java
@@ -22,7 +22,6 @@ package org.mifos.dto.screen;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
