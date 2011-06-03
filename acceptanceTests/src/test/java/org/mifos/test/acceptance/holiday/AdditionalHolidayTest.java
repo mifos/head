99ac60890e7dd8100b8d2964551edd2db93ac4d2
Łukasz Domżalski <ldomzalski@soldevelo.com>
2011-06-03 17:43:49 +0200

    [MIFOS-5004] PMD fix

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/holiday/AdditionalHolidayTest.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/holiday/AdditionalHolidayTest.java
@@ -97,7 +97,7 @@ public class AdditionalHolidayTest extends UiTestCaseBase {
     /*
      * loan creation for setup is stepping into questionnaire page when it shouldnt
      */
-    @Test(enabled=false)
+    @Test(enabled=true)
     public void createTwoWeeklyLoansInDifferentOffices() throws Exception {
         DateTime targetTime = new DateTime(2011, 3, 9, 0, 0, 0, 0);
         dateTimeUpdaterRemoteTestingService.setDateTime(targetTime);
