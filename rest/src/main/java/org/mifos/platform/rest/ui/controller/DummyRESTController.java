e4dc1f581fc4ddc866b80aee9d7a88530c846806
Udai Gupta <mailtoud@gmail.com>
2011-09-09 01:08:22 +0530

    MIFOS-5115 : REST/JSON service API module initial cut

---
+++ rest/src/main/java/org/mifos/platform/rest/ui/controller/DummyRESTController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2005-2011 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+package org.mifos.platform.rest.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * This is a dummy rest controller which is used to make sure if the rest
+ * services are available
+ *
+ * /mifos/status.json
+ *
+ */
+@Controller
+public class DummyRESTController {
+
+	@RequestMapping("status")
+	public final @ResponseBody StatusJSON status() {
+		StatusJSON json = new StatusJSON();
+		json.setStatus("Running");
+		return json;
+	}
+}
+
+class StatusJSON {
+
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
