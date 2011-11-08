f7f24599e72452838c7669f19b2e8398836c9449
Jakub Slawinski <jeremian@poczta.fm>
2011-11-08 21:23:33 +0100

    FIXED MIFOS-5179: Added new REST Controller with getGroupByNumber method.

---
+++ rest/src/main/java/org/mifos/platform/rest/ui/controller/GroupRESTController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2005-2011 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+package org.mifos.platform.rest.ui.controller;
+
+import org.mifos.application.servicefacade.ClientServiceFacade;
+import org.mifos.application.servicefacade.GroupServiceFacade;
+import org.mifos.dto.screen.ClientInformationDto;
+import org.mifos.dto.screen.GroupInformationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GroupRESTController {
+
+    @Autowired
+    private GroupServiceFacade groupServiceFacade;
+
+    @RequestMapping(value = "group/num-{globalCustNum}", method = RequestMethod.GET)
+    public final @ResponseBody
+    GroupInformationDto getGroupByNumber(@PathVariable String globalCustNum) {
+        return groupServiceFacade.getGroupInformationDto(globalCustNum);
+    }
+}
