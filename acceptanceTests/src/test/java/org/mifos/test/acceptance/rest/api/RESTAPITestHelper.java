8b6aa8c42f41648bb6c319c65884e035192c497c
Michal Dudzinski <mdudzinski@soldevelo.com>
2011-12-01 13:02:50 +0100

    MIFOS-5181 acceptance test

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/rest/api/RESTAPITestHelper.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/rest/api/RESTAPITestHelper.java
@@ -40,6 +40,9 @@ public class RESTAPITestHelper {
 
     private final Selenium selenium;
 
+	private static final String LOANOFFICER_PASSWORD = "testmifos";
+    private static final String LOANOFFICER_USERNAME = "loanofficer"; 
+    
     public static class Type {
         public static final String CLIENT = "client";
         public static final String CLIENT_CHARGES = "client/charges";
@@ -63,6 +66,7 @@ public class RESTAPITestHelper {
     public static class By {
         public static final String GLOBAL_NUMBER = "num";
         public static final String ID = "id";
+        public static final String MEETINGS = "meetings";
     }
 
     public RESTAPITestHelper(Selenium selenium) {
@@ -73,6 +77,12 @@ public class RESTAPITestHelper {
         LoginPage loginPage = new AppLauncher(selenium).launchMifos();
         loginPage.loginSuccessfullyUsingDefaultCredentials();
         selenium.open("jsonAjax.ftl");
+    }
+    
+    public void navigateToJsonAjaxPageAsLoanOfficer() {
+        LoginPage loginPage = new AppLauncher(selenium).launchMifos();
+        loginPage.loginSuccessfulAs(LOANOFFICER_USERNAME, LOANOFFICER_PASSWORD);
+        selenium.open("jsonAjax.ftl"); 
     }
 
     public String getJSONFromUI(String type, String by, String value) throws InterruptedException {
