a047c879c8aeca173eda8a62b5c89d7e9a795e23
Keith Woodlock <keithwoodlock@gmail.com>
2011-12-06 17:54:11 +0000

    remove commas from string representation of number.

--- acceptanceTests/src/test/java/org/mifos/test/acceptance/framework/loanproduct/EditLoanProductPage.java
+++ acceptanceTests/src/test/java/org/mifos/test/acceptance/framework/loanproduct/EditLoanProductPage.java
@@ -164,7 +164,7 @@ public class EditLoanProductPage extends MifosPage {
         if (!selenium.isChecked(configureVariableInstalmentsCheckbox)){
             selenium.click(configureVariableInstalmentsCheckbox);
         }
-        selenium.waitForCondition("selenium.isVisible('minimumInstallmentAmount')","10,000");
+        selenium.waitForCondition("selenium.isVisible('minimumInstallmentAmount')","10000");
         selenium.type(maxInstalmentGapTextBox, maxGap);
         selenium.type(minInstalmentGapTextBox, minGap);
         selenium.type(minInstalmentAmountTextBox, minInstalmentAmount.replace(",", ""));
