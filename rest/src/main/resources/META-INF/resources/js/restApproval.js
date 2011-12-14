e81519704b64f9dbcc2d13a2e87dbbfa6fe16e1e
Udai Gupta <mailtoud@gmail.com>
2011-12-14 18:20:47 +0530

    MIFOS-5319 : Disable Update funtion in Details pop-up for approved/rejected operations

--- rest/src/main/resources/META-INF/resources/js/restApproval.js
+++ rest/src/main/resources/META-INF/resources/js/restApproval.js
@@ -52,6 +52,6 @@ function updateArgs(id) {
 		  data: JSON.stringify(content),
 		  dataType: 'json', 
           contentType: "application/json; charset=utf-8",
-		  success: function() { alert("Update successful!"); }
+		  success: function() { alert("Update successful!"); dialog.dialog("close"); }
 		});
 }
