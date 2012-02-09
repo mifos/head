114022c76912a9fc2ad7a389412fb370ed773cc1
Jakub Slawinski <jeremian@poczta.fm>
2012-02-09 15:55:35 +0100

    MIFOS-5370: Added initial version of the controller.

---
+++ serviceInterfaces/src/main/java/org/mifos/application/admin/servicefacade/MonthClosingServiceFacade.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2005-2011 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.application.admin.servicefacade;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Date;
+
+public interface MonthClosingServiceFacade {
+    @PreAuthorize("isFullyAuthenticated()") // TODO: add similar role as: and hasRole('ROLE_CAN_SHUT_DOWN_MIFOS')
+    void setMonthClosingDate(Date day);
+
+    Date getMonthClosingDate();
+}
---
+++ userInterface/src/main/java/org/mifos/ui/core/controller/MonthClosingFormBean.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2005-2011 Grameen Foundation USA
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * See also http://www.apache.org/licenses/LICENSE-2.0.html for an
+ * explanation of the license and how it is applied.
+ */
+
+package org.mifos.ui.core.controller;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Locale;
+
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="EQ_UNUSUAL", justification="using commons equals builder")
+public class MonthClosingFormBean {
+
+    @DateTimeFormat(style = "S-")
+    private DateTime date;
+
+    private Locale locale;
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public String getFormattedDate() {
+        DateTimeFormatter formatter = org.joda.time.format.DateTimeFormat.forStyle("S-").withLocale(Locale.getDefault());
+        String printedDate = "";
+        if (date != null) {
+            printedDate = formatter.print(date);
+        }
+        return printedDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
