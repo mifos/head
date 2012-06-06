71f7363aee6a27169c81aa65ee402e5205564368
Michal Dudzinski <mdudzinski@soldevelo.com>
2012-06-06 15:40:56 +0200

    MIFOS-5408: configured spring security 'hasPermission' expression evaluator. Custom PermissionEvaluator and Permission implementation.

---
+++ appdomain/src/main/java/org/mifos/security/rolesandpermission/authorization/MifosPermissionEvaluator.java
@@ -0,0 +1,51 @@
+package org.mifos.security.rolesandpermission.authorization;
+
+import java.io.Serializable;
+
+import org.mifos.core.MifosRuntimeException;
+import org.mifos.framework.exceptions.ApplicationException;
+import org.mifos.framework.exceptions.SystemException;
+import org.mifos.security.rolesandpermission.authorization.permissions.MaxLoanAmountForApprovePermission;
+import org.mifos.security.rolesandpermission.authorization.permissions.MifosPermission;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Custom Mifos PermissionEvaluator.
+ * See http://static.springsource.org/spring-security/site/docs/3.1.x/reference/el-access.html#el-permission-evaluator
+ */
+public class MifosPermissionEvaluator implements PermissionEvaluator {
+
+    public MifosPermissionEvaluator() {
+        super();
+    }
+
+    @Override
+    @Transactional
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        try {
+            return checkPermission(authentication, targetDomainObject, permission);
+        } catch (SystemException e) {
+            throw new MifosRuntimeException(e);
+        } catch (ApplicationException e) {
+            throw new MifosRuntimeException(e);
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
+            Object permission) {
+        return false;
+    }
+
+    public boolean checkPermission(Authentication authentication, Object targetDomainObject, Object permissionName) throws SystemException, ApplicationException{
+        /** TODO: when Mifos will contain more permissions than one, MifosPermissionEvaluator should
+         *  use factory class to get appriopriate MifosPermission implementation for given permissionName.
+         */
+        MifosPermission permission = new MaxLoanAmountForApprovePermission();
+        return permission.isAllowed(authentication, targetDomainObject);
+    }
+    
+}
---
+++ appdomain/src/main/java/org/mifos/security/rolesandpermission/authorization/permissions/MaxLoanAmountForApprovePermission.java
@@ -0,0 +1,102 @@
+package org.mifos.security.rolesandpermission.authorization.permissions;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+import org.mifos.accounts.loan.business.LoanBO;
+import org.mifos.accounts.loan.persistance.LoanDao;
+import org.mifos.accounts.loan.persistance.LoanDaoHibernate;
+import org.mifos.accounts.productdefinition.persistence.LoanProductDao;
+import org.mifos.accounts.productdefinition.persistence.LoanProductDaoHibernate;
+import org.mifos.accounts.util.helpers.AccountState;
+import org.mifos.application.servicefacade.ApplicationContextProvider;
+import org.mifos.dto.domain.AccountUpdateStatus;
+import org.mifos.framework.exceptions.ApplicationException;
+import org.mifos.framework.exceptions.ServiceException;
+import org.mifos.framework.exceptions.SystemException;
+import org.mifos.framework.util.helpers.Money;
+import org.mifos.security.MifosUser;
+import org.mifos.security.rolesandpermission.business.ActivityRestrictionTypeEntity;
+import org.mifos.security.rolesandpermission.business.RoleActivityRestrictionBO;
+import org.mifos.security.rolesandpermission.business.RoleBO;
+import org.mifos.security.rolesandpermission.business.service.RolesPermissionsBusinessService;
+import org.mifos.security.rolesandpermission.persistence.LegacyRolesPermissionsDao;
+import org.mifos.security.util.helpers.ActivityRestrictionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+
+/**
+ * If new loan account status is "Approved" then checks if user role has max loan amount
+ * for this activity;
+ * {@link http://mifosforge.jira.com/browse/MIFOS-5408}
+ */
+public class MaxLoanAmountForApprovePermission implements MifosPermission {
+
+    @Autowired
+    private LoanDao loanDao;
+
+    private RolesPermissionsBusinessService rolesPermissionsBusinessService;
+
+    public MaxLoanAmountForApprovePermission() {
+        // FIXME : somehow autowiring is not working
+        if (loanDao == null) {
+            loanDao = ApplicationContextProvider.getBean(LoanDaoHibernate.class);
+        }
+        rolesPermissionsBusinessService = new RolesPermissionsBusinessService();
+    }
+
+    @Override
+    public boolean isAllowed(Authentication authentication, Object targetDomainObject) throws ServiceException {
+        if (targetDomainObject instanceof AccountUpdateStatus) {
+
+            AccountUpdateStatus accountUpdateStatus = (AccountUpdateStatus) targetDomainObject;
+            MifosUser user = (MifosUser) authentication.getPrincipal();
+            return checkPermissionByAccountUpdateStatus(user, accountUpdateStatus);
+
+        } else if (targetDomainObject instanceof List) {
+
+            boolean isAllowed = true;
+            List<AccountUpdateStatus> accountsUpdateStatus = (List<AccountUpdateStatus>) targetDomainObject;
+            MifosUser user = (MifosUser) authentication.getPrincipal();
+            for (AccountUpdateStatus accountUpdateStatus : accountsUpdateStatus) {
+                isAllowed = checkPermissionByAccountUpdateStatus(user, accountUpdateStatus);
+                if (!isAllowed) {
+                    return false;
+                }
+            }
+            return isAllowed;
+
+        }
+
+        return true;
+    }
+
+    private boolean checkPermissionByAccountUpdateStatus(MifosUser user, AccountUpdateStatus accountUpdateStatus)
+            throws ServiceException {
+        boolean isAllowed = true;
+
+        if (!accountUpdateStatus.getNewStatusId().equals(AccountState.LOAN_APPROVED.getValue())) {
+            return true;
+        }
+        LoanBO loanBO = loanDao.findById(accountUpdateStatus.getSavingsId().intValue());
+        Money totalPrincipalAmount = loanBO.getTotalPrincipalAmount();
+
+        List<Short> roleIds = user.getRoleIds();
+        for (Short roleId : roleIds) {
+            BigDecimal amountRestriction = rolesPermissionsBusinessService
+                    .getRoleActivityRestrictionAmountValueByRestrictionTypeId(roleId,
+                            ActivityRestrictionType.MAX_LOAN_AMOUNT_FOR_APPROVE.getValue());
+
+            if (amountRestriction == null
+                    || totalPrincipalAmount.isLessThan(new Money(loanBO.getCurrency(), amountRestriction))) {
+                return true;
+            } else {
+                isAllowed = false;
+            }
+        }
+
+        return isAllowed;
+    }
+
+}
