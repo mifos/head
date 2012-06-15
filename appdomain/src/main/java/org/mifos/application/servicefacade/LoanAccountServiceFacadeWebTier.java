de3ecd60d53b5583b96b38bd9fd5574a24ba5e84
Jakub Slawinski <jeremian@poczta.fm>
2012-06-15 16:36:15 +0200

    Revert "FIXED MIFOS-5650 Stack trace appears during creation of multiple Loan Accounts when LSIM is enabled."
    This reverts commit 6effb469ff1173b0343148b4a436b39f6b401bda.

--- appdomain/src/main/java/org/mifos/application/servicefacade/LoanAccountServiceFacadeWebTier.java
+++ appdomain/src/main/java/org/mifos/application/servicefacade/LoanAccountServiceFacadeWebTier.java
@@ -997,11 +997,16 @@ public class LoanAccountServiceFacadeWebTier implements LoanAccountServiceFacade
         boolean loanScheduleIndependentOfCustomerMeetingEnabled = loanAccountInfo.isRepaymentScheduleIndependentOfCustomerMeeting();
         LoanScheduleConfiguration configuration = new LoanScheduleConfiguration(loanScheduleIndependentOfCustomerMeetingEnabled, interestDays);
 
-        MeetingDto customerMeetingDto = customer.getCustomerMeetingValue().toDto();
-        MeetingBO repaymentDayMeeting = new MeetingFactory().create(customerMeetingDto);
-        
-        Short recurAfter = loanAccountDetail.getLoanProduct().getLoanOfferingMeeting().getMeeting().getRecurAfter();
-        repaymentDayMeeting.getMeetingDetails().setRecurAfter(recurAfter);
+        MeetingBO repaymentDayMeeting = null;
+        if (loanScheduleIndependentOfCustomerMeetingEnabled) {
+            repaymentDayMeeting = this.createNewMeetingForRepaymentDay(loanAccountInfo.getDisbursementDate(), loanAccountInfo, loanAccountDetail.getCustomer());
+        } else {
+            MeetingDto customerMeetingDto = customer.getCustomerMeetingValue().toDto();
+            repaymentDayMeeting = new MeetingFactory().create(customerMeetingDto);
+            
+            Short recurAfter = loanAccountDetail.getLoanProduct().getLoanOfferingMeeting().getMeeting().getRecurAfter();
+            repaymentDayMeeting.getMeetingDetails().setRecurAfter(recurAfter);
+        }
 
         List<DateTime> loanScheduleDates = new ArrayList<DateTime>(loanScheduleInstallmentDates);
 
