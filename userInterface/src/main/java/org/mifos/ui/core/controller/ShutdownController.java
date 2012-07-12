a76b9f82b522a887bef94e7d2776eaa4a9541481
Kamil Kalfas <kkalfas@soldevelo.com>
2012-07-12 17:36:20 +0200

    Revert "Merge pull request #34 from Robson309/MIOS-4132"
    This reverts commit 8f7474e6adc3deb993c7542307270923e1b91b13, reversing
    changes made to 76f4d710cbcd1eff45a82aa6765afe938fc25644.

--- userInterface/src/main/java/org/mifos/ui/core/controller/ShutdownController.java
+++ userInterface/src/main/java/org/mifos/ui/core/controller/ShutdownController.java
@@ -107,9 +107,6 @@ public class ShutdownController {
             }
             status.setComplete();
         }
-        else if(StringUtils.isNotBlank(cancel)) {
-        	this.shutdownServiceFacade.cancelShutdown(request);
-        }
 
         return loadShutdownInfo(request);
     }
