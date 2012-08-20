4ef10d0638e4bb8c3e6ffdfa285a9110c58a816b
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2012-08-20 14:39:07 +0200

    MIFOS-5727: added to create a dynamic jdbc file for for kettle enviroment

--- application/src/main/java/org/mifos/framework/components/batchjobs/helpers/ETLReportDWHelper.java
+++ application/src/main/java/org/mifos/framework/components/batchjobs/helpers/ETLReportDWHelper.java
@@ -1,9 +1,9 @@
 package org.mifos.framework.components.batchjobs.helpers;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.InputStreamReader;
-import java.util.Properties;
+import java.io.PrintWriter;
 
 import org.apache.log4j.Logger;
 import org.mifos.application.servicefacade.ApplicationContextHolder;
@@ -25,12 +25,7 @@ public class ETLReportDWHelper extends TaskHelper {
         String jarPath = this.getClass().getResource("/mifos-etl-plugin-1.0-SNAPSHOT.one-jar.jar").toString().replace("file:", "");
         try {
             String cmd = System.getProperty("java.home") + "/bin/java -jar " + jarPath + " " + path + " false";
-            Process p = Runtime.getRuntime().exec(cmd);
-            p.waitFor();
-            if (p.exitValue()!=0) {
-                throw new MifosRepositoryException("Error in run pentaho ETL batch job");
-            }
-            
+            Process p = Runtime.getRuntime().exec(cmd);    
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -39,27 +34,23 @@ public class ETLReportDWHelper extends TaskHelper {
 
     private void createPropertiesFileForPentahoDWReports() {
         try {
+            File file = new File(System.getProperty("user.dir")+"/Simple-JNDI/jdbc.properties");
             new ApplicationContextHolder();
             ApplicationContext ach = ApplicationContextHolder.getApplicationContext();
-
             DriverManagerDataSource ds = (DriverManagerDataSource) ach.getBean("dataSource");
-
-            Properties prop = new Properties();
-            prop.setProperty("SourceDB/type", "javax.sql.DataSource");
-            prop.setProperty("SourceDB/driver", "com.mysql.jdbc.Driver");
-            prop.setProperty("SourceDB/url", ds.getUrl());
-            prop.setProperty("SourceDB/user", ds.getUsername());
-            prop.setProperty("SourceDB/password", ds.getPassword());
-
             DriverManagerDataSource dsDW = (DriverManagerDataSource) ach.getBean("dataSourcePentahoDW");
-
-            prop.setProperty("DestinationDB/type", "javax.sql.DataSource");
-            prop.setProperty("DestinationDB/driver", "com.mysql.jdbc.Driver");
-            prop.setProperty("DestinationDB/url", dsDW.getUrl());
-            prop.setProperty("DestinationDB/user", dsDW.getUsername());
-            prop.setProperty("DestinationDB/password", dsDW.getUsername());
-        
-            prop.store(new FileOutputStream(System.getProperty("user.dir")+"/Simple-JNDI/jdbc.properties"), "create");
+            PrintWriter fw = new PrintWriter(file);
+            fw.println("SourceDB/type=javax.sql.DataSource");
+            fw.println("SourceDB/driver=com.mysql.jdbc.Driver");
+            fw.println("SourceDB/url="+ds.getUrl());
+            fw.println("SourceDB/user="+ds.getUsername());
+            fw.println("SourceDB/password="+ds.getPassword());
+            fw.println("DestinationDB/type=javax.sql.DataSource");
+            fw.println("DestinationDB/driver=com.mysql.jdbc.Driver");
+            fw.println("DestinationDB/url="+dsDW.getUrl());
+            fw.println("DestinationDB/user="+dsDW.getUsername());
+            fw.println("DestinationDB/password="+dsDW.getUsername());
+            fw.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
