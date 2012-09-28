6d2d8a352c35a0d08185b792e129eb95fe23a424
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2012-09-28 14:03:46 +0200

    MIFOS-5827: change hardcored path for ETL

--- application/src/main/java/org/mifos/framework/components/batchjobs/helpers/ETLReportDWHelper.java
+++ application/src/main/java/org/mifos/framework/components/batchjobs/helpers/ETLReportDWHelper.java
@@ -2,6 +2,7 @@ package org.mifos.framework.components.batchjobs.helpers;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.regex.Matcher;
@@ -11,6 +12,7 @@ import org.apache.log4j.Logger;
 import org.mifos.application.servicefacade.ApplicationContextHolder;
 import org.mifos.framework.components.batchjobs.TaskHelper;
 import org.mifos.framework.components.batchjobs.exceptions.BatchJobException;
+import org.mifos.framework.util.ConfigurationLocator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -32,38 +34,40 @@ public class ETLReportDWHelper extends TaskHelper {
             nameOfDataBase = m.group(6);
         }
         if (!nameOfDataBase.equals("")) {
+        	
+        	ConfigurationLocator configurationLocator = new ConfigurationLocator();
+        	String configPath = configurationLocator.getConfigurationDirectory();
             createPropertiesFileForPentahoDWReports(ds, dsDW);
-            String path = System.getProperty("user.home") + "/.mifos/ETL/MifosDataWarehouseETL/" + FILENAME;
-            String jarPath = System.getProperty("user.home") + "/.mifos/ETL/mifos-etl-plugin-1.0-SNAPSHOT.one-jar.jar";
+            String path = configPath + "/ETL/MifosDataWarehouseETL/" + FILENAME;
+            String jarPath = configPath + "/ETL/mifos-etl-plugin-1.0-SNAPSHOT.one-jar.jar";
             String javaHome = System.getProperty("java.home") + "/bin/java"; 
-            String cmd = "-jar " + jarPath + " " + path + " false";
-            String pathToLog = System.getProperty("user.home") + "/.mifos/ETL/log";     
+            String pathToLog = configPath + "/ETL/log"; 
+            String cmd = "-jar " + jarPath + " " + path + " false";   
             if (File.separatorChar == '\\') { // windows platform
                 javaHome=javaHome.replaceAll("/", "\\\\");
                 javaHome='"'+javaHome+'"';
                 cmd = cmd.replaceAll("/", "\\\\");
-                pathToLog = pathToLog.replaceAll("/", "\\\\");
             }
             try {
-            	
-                cmd = javaHome +" "+ cmd +" "+ dsDW.getUsername() + " " + dsDW.getPassword() + " " + dsDW.getUrl();
+            	cmd = javaHome +" "+ cmd +" "+ dsDW.getUsername() + " " + dsDW.getPassword() + " " + dsDW.getUrl();
                 Process p = Runtime.getRuntime().exec(cmd);
                 BufferedReader reader = new BufferedReader (new InputStreamReader(p.getInputStream()));
                 String line =null;
-                if(new File(pathToLog).exists()){
-                	new File(pathToLog).delete();
-                }
-                File file = new File(pathToLog);
-                PrintWriter fw = new PrintWriter(file);
+				if (new File(pathToLog).exists()) {
+					new File(pathToLog).delete();
+				}
+				File file = new File(pathToLog);
+				PrintWriter fw = new PrintWriter(file);
                 while ((line = reader.readLine ()) != null) {
                 	fw.println(line);
+                	
                 }
                 fw.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+               
+            } catch (IOException ex) {
+            	throw new BatchJobException(ex.getCause());
             }
-        }
-
+        } 
     }
 
     private void createPropertiesFileForPentahoDWReports(DriverManagerDataSource ds, DriverManagerDataSource dsDW) {
