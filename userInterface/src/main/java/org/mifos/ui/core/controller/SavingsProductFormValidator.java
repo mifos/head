72d57ad64ff2c0f4c48716584adc6a0a377dcb7c
Michal Dudzinski <mdudzinski@soldevelo.com>
2012-10-09 14:07:24 +0200

    Revert "Merge pull request #138 from wojtek-szarmacher/MIFOS-5788"
    This reverts commit 647116a6a8d5ab97fb76a601a292a5fb21ae1284, reversing
    changes made to a6236b8f176677d60049adfb1fa839e747d8eda2.

--- userInterface/src/main/java/org/mifos/ui/core/controller/SavingsProductFormValidator.java
+++ userInterface/src/main/java/org/mifos/ui/core/controller/SavingsProductFormValidator.java
@@ -192,44 +192,24 @@ public class SavingsProductFormValidator implements Validator {
         }
         Short digsAfterDec = configurationServiceFacade.getAccountingConfiguration().getDigitsAfterDecimal();
         Short digsBeforeDec = configurationServiceFacade.getAccountingConfiguration().getDigitsBeforeDecimal();
-        Double maxWithdrawalObj = formBean.getMaxWithdrawalAmount();
-        String maxWithdrawalString;
-
-        if (maxWithdrawalObj == null) {
-            maxWithdrawalString = "";
-        } else {
-            maxWithdrawalString = maxWithdrawalObj.toString();
-        }
-        
-        int dot = maxWithdrawalString.lastIndexOf(".");
+        int dot = formBean.getMaxWithdrawalAmount().toString().lastIndexOf(".");
         int max = digsAfterDec + digsBeforeDec + dot;
-        int withdrawalLength = maxWithdrawalString.length();
-        
-        if (maxWithdrawalString.lastIndexOf(0, dot) > digsBeforeDec) {
+        int withdrawalLength = formBean.getMaxWithdrawalAmount().toString().length();
+        int depositLength = formBean.getAmountForDeposit().toString().length();
+        if (formBean.getMaxWithdrawalAmount().toString().lastIndexOf(0, dot) > digsBeforeDec) {
 			errors.reject("MaxDigitsBefore.savingProduct.withdrawal" , new String [] {digsBeforeDec.toString()} , null);       	
         }
-        if (maxWithdrawalString.lastIndexOf(dot, withdrawalLength) > digsAfterDec) {
+        if (formBean.getMaxWithdrawalAmount().toString().lastIndexOf(dot, withdrawalLength) > digsAfterDec) {
     		errors.reject("MaxDigitsAfter.savingProduct.withdrawal" , new String [] {digsAfterDec.toString()} , null);
         }
         if (withdrawalLength > max) {
         	errors.reject("MaxDigitsNumber.savingProduct.withdrawal" , new String [] {String.valueOf(max)} , null);
         }
         
-        Double amountForDepositObj = formBean.getAmountForDeposit();
-        String amountForDepositString;
-        
-        if (amountForDepositObj == null) {
-            amountForDepositString = "";
-        } else {
-            amountForDepositString = amountForDepositObj.toString();
-        }
-        
-        int depositLength = amountForDepositString.length();
-        
-        if (amountForDepositString.lastIndexOf(0, dot) > digsBeforeDec) {
+        if (formBean.getAmountForDeposit().toString().lastIndexOf(0, dot) > digsBeforeDec) {
 			errors.reject("MaxDigitsBefore.savingProduct.deposit" , new String [] {digsBeforeDec.toString()} , null);       	
         }
-        if (amountForDepositString.lastIndexOf(dot, depositLength) > digsAfterDec) {
+        if (formBean.getAmountForDeposit().toString().lastIndexOf(dot, depositLength) > digsAfterDec) {
     		errors.reject("MaxDigitsAfter.savingProduct.deposit" , new String [] {digsAfterDec.toString()} , null);
         }
         if (depositLength > max) {
