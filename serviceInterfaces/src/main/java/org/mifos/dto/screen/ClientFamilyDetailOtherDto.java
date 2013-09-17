622e5f57e36479e785dfdbe44b7269fa263e0448
Lech Rozanski <lrozanski@soldevelo.com>
2013-09-17 15:28:05 +0200

    MIFOS-6104: Error while applying guaranty to a loan account

--- serviceInterfaces/src/main/java/org/mifos/dto/screen/ClientFamilyDetailOtherDto.java
+++ serviceInterfaces/src/main/java/org/mifos/dto/screen/ClientFamilyDetailOtherDto.java
@@ -20,11 +20,12 @@
 
 package org.mifos.dto.screen;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @SuppressWarnings("PMD")
 @edu.umd.cs.findbugs.annotations.SuppressWarnings(value={"SE_NO_SERIALVERSIONID", "EI_EXPOSE_REP", "EI_EXPOSE_REP2"}, justification="should disable at filter level and also for pmd - not important for us")
-public class ClientFamilyDetailOtherDto {
+public class ClientFamilyDetailOtherDto implements Serializable {
 
     private final String relationship;
     private final String displayName;
